e296b3e96f6ea2ba460c5ebc7a0a9158e39031bf
yupeng74@huawei.com <yupeng74@huawei.com>
2023-09-23 11:43:56 +0800

    根据设备类型来区分线程优先级
    Signed-off-by: yupeng74@huawei.com <yupeng74@huawei.com>

--- base/threading/platform_thread_linux.cc
+++ base/threading/platform_thread_linux.cc
@@ -294,8 +294,13 @@ const struct sched_param kRealTimePrio = {8};
 const ThreadPriorityToNiceValuePair kThreadPriorityToNiceValueMap[4] = {
     {ThreadPriority::BACKGROUND, 10},
     {ThreadPriority::NORMAL, 0},
+#if BUILDFLAG(IS_OHOS)
+    {ThreadPriority::DISPLAY, -20},
+    {ThreadPriority::REALTIME_AUDIO, -20},
+#else
     {ThreadPriority::DISPLAY, -8},
     {ThreadPriority::REALTIME_AUDIO, -10},
+#endif
 };
 
 bool CanSetThreadPriorityToRealtimeAudio() {
