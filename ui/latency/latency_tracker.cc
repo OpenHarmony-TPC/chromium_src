d7ec3cb57257dacb2cfe52fe1956a3b6f92042e1
hukaijie <hukaijie@huawei.com>
2023-10-17 07:36:31 +0000

    添加滑动响应耗时
    Change-Id: If9ef023ff25554ae5a36150b2dbd5179788c4b78
    Signed-off-by: hukaijie <hukaijie@huawei.com>

--- ui/latency/latency_tracker.cc
+++ ui/latency/latency_tracker.cc
@@ -104,6 +104,22 @@ void LatencyTracker::OnGpuSwapBuffersCompleted(
       continue;
     }
 
+    base::TimeTicks first_scroll_start_timestamp;
+    base::TimeTicks original_scroll_start_timestamp;
+    if (latency.FindLatency(
+            INPUT_EVENT_LATENCY_FIRST_SCROLL_UPDATE_ORIGINAL_COMPONENT,
+            &first_scroll_start_timestamp)) {
+      TRACE_EVENT1("input","OnGpuSwapBuffersCompleted::FirstScrollUpdate",
+                    "time",
+                    (gpu_swap_begin_timestamp - first_scroll_start_timestamp).InMillisecondsF());
+    } else if (latency.FindLatency(
+            INPUT_EVENT_LATENCY_SCROLL_UPDATE_ORIGINAL_COMPONENT,
+            &original_scroll_start_timestamp)) {
+      TRACE_EVENT1("input","OnGpuSwapBuffersCompleted::OriginScrollUpdate",
+                    "time",
+                    (gpu_swap_begin_timestamp - original_scroll_start_timestamp).InMillisecondsF());
+    }
+
     ui::SourceEventType source_event_type = latency.source_event_type();
     if (source_event_type == ui::SourceEventType::WHEEL ||
         source_event_type == ui::SourceEventType::MOUSE ||
