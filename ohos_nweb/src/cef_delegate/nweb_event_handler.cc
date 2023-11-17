e0c6170928d50c0e12407d32aab5246cbcc08c43
jcg_814 <jinchanggang@huawei.com>
2023-11-17 16:41:45 +0800

    增加判断鼠标抬起时是否已经离开窗口
    Signed-off-by: jcg_814 <jinchanggang@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_event_handler.cc
+++ ohos_nweb/src/cef_delegate/nweb_event_handler.cc
@@ -220,7 +220,11 @@ void NWebEventHandler::SendMouseEvent(int x,
   cef_mouse_button_type_t buttonType = static_cast<cef_mouse_button_type_t>(
       NWebInputDelegate::CefConverter("mousebutton", button));
   mouseEvent.modifiers = input_delegate_.GetModifiers(buttonType);
-
+  if (NWebInputDelegate::IsMouseLeave(action)) {
+    in_web = false;
+  } else if (NWebInputDelegate::IsMouseEnter(action)) {
+    in_web = true;
+  }
   if (browser_ && browser_->GetHost()) {
     if (NWebInputDelegate::IsMouseDown(action)) {
       previous_action = action;
@@ -232,6 +236,9 @@ void NWebEventHandler::SendMouseEvent(int x,
     } else if (NWebInputDelegate::IsMouseUp(action)) {
       previous_action = action;
       browser_->GetHost()->SendMouseClickEvent(mouseEvent, buttonType, true, 1);
+      if (!in_web) {
+        browser_->GetHost()->SendMouseMoveEvent(mouseEvent, true);
+      }
     } else if (NWebInputDelegate::IsMouseMove(action)) {
       if (last_mouse_x_ == x && last_mouse_y_ == y) {
         LOG(INFO) << "no change in coordinates, cancel mouse move event";
