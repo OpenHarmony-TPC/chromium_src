71acfd26de127851fcd0fa9a7a77e5a83dda4ef2
w00848065 <wangyanhan3@huawei.com>
2023-11-25 02:42:35 +0000

    [fling]add a debug log for testing Signed-off-by: w00848065 <wangyanhan3@huawei.com>
    Signed-off-by: w00848065 <wangyanhan3@huawei.com>

--- ui/events/gestures/blink/web_gesture_curve_impl.cc
+++ ui/events/gestures/blink/web_gesture_curve_impl.cc
@@ -47,6 +47,7 @@ std::unique_ptr<GestureCurve> CreateDefaultPlatformCurve(
   use_native_fling_curve = true;
 #endif
   if (use_native_fling_curve) {
+    LOG(DEBUG) << "WebGestureCurveImpl DUMP_FLING_CURVE initial_velocity: " << initial_velocity.y();
     auto scroller = std::make_unique<NativeScrollerOhos>();
     scroller->Fling(0, 0, initial_velocity.x(), initial_velocity.y(), INT_MIN,
                     static_cast<float>(INT_MAX), INT_MIN,
