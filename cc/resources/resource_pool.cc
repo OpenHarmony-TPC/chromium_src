8ed0cb0f27ddbad44e699786551f6fe8ebf51cb5
lizhengkun <lizhengkun3@huawei.com>
2023-12-04 20:35:22 -0500

    Signed-off-by: lizhengkun <lizhengkun3@huawei.com> Change-Id: I1cb7553e5b23a5008952f0a7feaf2cbbd8ad026b
    Change-Id: Ibdf0bcfc857fafdab0ed8c7cdc28cf190df4c8f6

--- cc/resources/resource_pool.cc
+++ cc/resources/resource_pool.cc
@@ -563,7 +563,7 @@ void ResourcePool::EvictResourcesNotUsedSince(base::TimeTicks time_limit) {
     if (unused_resources_.back()->last_usage() > time_limit)
       return;
 
-    #ifdef OHOS_NWED_EX
+#ifdef OHOS_NWED_EX
     if (delete_unused_resources_delay_enabled_ &&
         unused_resources_.size() <= kUnusedResourcesToKeep &&
         unused_resources_.back()->last_usage() + kDefaultExpirationDelay >
