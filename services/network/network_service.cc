3a889f17d4908294673ca113b467427a3452b965
fitvival <gaojianhao@huawei.com>
2023-12-13 17:44:35 +0800

    FIX setConnectionTimeout
    Signed-off-by: fitvival <gaojianhao@huawei.com>

--- services/network/network_service.cc
+++ services/network/network_service.cc
@@ -536,6 +536,15 @@ std::unique_ptr<NetworkService> NetworkService::CreateForTesting() {
 
 void NetworkService::RegisterNetworkContext(NetworkContext* network_context) {
   DCHECK_EQ(0u, network_contexts_.count(network_context));
+#if defined(OHOS_EX_NETWORK_CONNECTION)
+  net::URLRequestContext* url_request_context =
+      network_context->url_request_context();
+  if (url_request_context) {
+    LOG(INFO) << "Register network context and set network timeout "
+              << timeout_override_ << " second(s)";
+    url_request_context->SetConnectTimeout(timeout_override_);
+  }
+#endif
   network_contexts_.insert(network_context);
   if (quic_disabled_)
     network_context->DisableQuic();
@@ -982,6 +991,7 @@ NetworkService* NetworkService::GetNetworkServiceForTesting() {
 void NetworkService::SetConnectTimeout(int seconds) {
   LOG(INFO) << "Network service set network timeout " << seconds
             << " second(s)";
+  timeout_override_ = seconds;
   for (auto* network_context : network_contexts_) {
     net::URLRequestContext* url_request_context =
         network_context->url_request_context();
