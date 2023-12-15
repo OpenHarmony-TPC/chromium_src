087f62bfc82f15402c38dbcf3a850a6c96fb4ce0
fitvival <gaojianhao@huawei.com>
2023-12-15 11:16:35 +0800

    init
    Signed-off-by: fitvival <gaojianhao@huawei.com>

--- ohos_nweb/include/nweb.h
+++ ohos_nweb/include/nweb.h
@@ -924,6 +924,12 @@ class OHOS_NWEB_EXPORT NWeb : public std::enable_shared_from_this<NWeb> {
     virtual bool Restore() = 0;
 
     /**
+     * Get the security level of current page.
+     * @return security level for current page.
+     */
+    virtual int GetSecurityLevel() = 0;
+
+    /**
      * CallH5Function
      *
      * @param routing_id       int32_t: the h5 frmae routing id
@@ -936,12 +942,6 @@ class OHOS_NWEB_EXPORT NWeb : public std::enable_shared_from_this<NWeb> {
         int32_t h5_object_id,
         const std::string h5_method_name,
         const std::vector<std::shared_ptr<NWebValue>>& args) = 0;
-
-    /**
-     * Get the security level of current page.
-     * @return security level for current page.
-     */
-    virtual int GetSecurityLevel() = 0;
 };
 }  // namespace OHOS::NWeb
 
--- ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -29,6 +29,7 @@
 #include "cef/include/cef_base.h"
 #include "cef/include/cef_request_context.h"
 #include "content/public/common/content_switches.h"
+#include "components/security_state/core/security_state.h"
 #include "nweb_find_delegate.h"
 #include "nweb_preference_delegate.h"
 #include "url/gurl.h"
@@ -2282,11 +2283,9 @@ double NWebDelegate::GetBrowserZoomLevel() {
 int NWebDelegate::GetSecurityLevel() {
   if (GetBrowser() == nullptr) {
     LOG(ERROR) << "NWebDelegate::GetSecurityLevel failed.";
-    return static_cast<int>(security_state::SecurityLevel::DANGEROUS); 
+    return static_cast<int>(security_state::SecurityLevel::DANGEROUS);
   }
 
-  if (GetBrowser().get()) {
-    return GetBrowser()->GetSecurityLevel();
-  }
+  return GetBrowser()->GetSecurityLevel();
 }
 }  // namespace OHOS::NWeb
