69b8e16769f998e9e0437d223530a798bb0b8df3
jeffreyliu <liutianyi20@huawei.com>
2024-01-02 15:57:28 +0800

    RM003 v99, UT adaptation. indentation fixed
    Signed-off-by: jeffreyliu <liutianyi20@huawei.com>
    Change-Id: Iada24872db76fe1192330fd3d9db2917105cbf6f

--- base/base_paths_ohos.cc
+++ base/base_paths_ohos.cc
@@ -21,18 +21,18 @@
 namespace base {
 
 bool ParseAssetsOHOS(FilePath* result) {
-  if(!base::CommandLine::ForCurrentProcess()) {
+  if (!base::CommandLine::ForCurrentProcess()) {
       LOG(ERROR)<< "CommandLine not init ";
-        return false; 
+      return false;
   }
   auto bundle_path = base::CommandLine::ForCurrentProcess()->
-    GetSwitchValueASCII(switches::kBundleInstallationDir);
+  GetSwitchValueASCII(switches::kBundleInstallationDir);
   if (bundle_path.empty()) {
-	FilePath bin_dir;
-    if(!ReadSymbolicLink(FilePath(kProcSelfExe), &bin_dir)){
-      LOG(ERROR)<< "Unable to resolve " << kProcSelfExe << ".";
-        return false; 
-    }
+      FilePath bin_dir;
+      if (!ReadSymbolicLink(FilePath(kProcSelfExe), &bin_dir)){
+          LOG(ERROR)<< "Unable to resolve " << kProcSelfExe << ".";
+          return false; 
+      }
       *result = bin_dir.DirName();
       return true;
   }
