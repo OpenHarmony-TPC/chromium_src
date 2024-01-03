9d1a28da995cd013c9732afd2dc6a6ad977daec0
Lin <penglin22@huawei.com>
2024-01-03 11:29:59 +0800

    更新同层渲染部分特性
    1.Fix some build warnings.
    2.Enable 'SetNativeEmbedModeEnabled' interface.
    3.Use 'px' for native embed size report.
    4.Native type change should activate full life cycle.
    
    Signed-off-by: Lin <penglin22@huawei.com>
    Change-Id: Ie9bdf042d8fb5cc18eeb3afb9dd47e72c06840e3
    Signed-off-by: Lin <penglin22@huawei.com>

--- content/public/renderer/render_frame.h
+++ content/public/renderer/render_frame.h
@@ -99,9 +99,8 @@ class CONTENT_EXPORT RenderFrame : public IPC::Listener,
   static void ForEach(RenderFrameVisitor* visitor);
 
 #if defined(OHOS_INPUT_EVENTS)
-  virtual void SetZoomLevel(float magnify_delta, const gfx::Point& anchor) {};
-  virtual void SetOverscrollMode(int mode) {};
-  virtual void SetNativeEmbedModeEnabled(bool mode) {};
+  virtual void SetZoomLevel(float magnify_delta, const gfx::Point& anchor) {}
+  virtual void SetOverscrollMode(int mode) {}
 #endif  // defined(OHOS_INPUT_EVENTS)
 
   // Returns the RenderFrame associated with the main frame of the WebView.
