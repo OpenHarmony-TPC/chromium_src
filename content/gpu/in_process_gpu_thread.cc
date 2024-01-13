242e4f261295746dc053227af3f493d9a22882a5
Zhao-PengFei35 <zhaopengfei35@huawei.com>
2024-01-13 22:23:12 +0800

    fix bug
    Signed-off-by: Zhao-PengFei35 <zhaopengfei35@huawei.com>

--- content/gpu/in_process_gpu_thread.cc
+++ content/gpu/in_process_gpu_thread.cc
@@ -20,7 +20,7 @@
 #include <fstream>
 #include <dirent.h>
 #include "res_sched_client_adapter.h"
-#endif  // BUILDFLAG(IS_OHOS)
+#endif
 
 #if BUILDFLAG(USE_VAAPI)
 #include "media/gpu/vaapi/vaapi_wrapper.h"
@@ -32,7 +32,7 @@
 
 #if BUILDFLAG(IS_OHOS)
 const int MAX_FILE_LENGTH = 32* 1024 * 1024;
-#endif  // BUILDFLAG(IS_OHOS)
+#endif
 
 namespace content {
 
@@ -52,10 +52,10 @@ InProcessGpuThread::~InProcessGpuThread() {
   auto tid = GetGpuThreadId(base::GetCurrentProcId());
   if (tid > 0) {
     ResSchedClientAdapter::ReportKeyThread(
-      ResSchedStatusAdapter::THREAD_DESTROYED, base::GetCurrentRealPid(),
+      ResSchedStatusAdapter::THREAD_DESTROYED, base::GetCurrentProcId(),
       tid, ResSchedRoleAdapter::IMPORTANT_DISPLAY);
   }
-#endif // BUILDFLAG(IS_OHOS)
+#endif
 }
 
 void InProcessGpuThread::Init() {
@@ -105,10 +105,10 @@ void InProcessGpuThread::Init() {
   auto tid = GetGpuThreadId(base::GetCurrentProcId());
   if(tid > 0) {
     ResSchedClientAdapter::ReportKeyThread(
-      ResSchedStatusAdapter::THREAD_CREATED, base::GetCurrentRealPid(),
+      ResSchedStatusAdapter::THREAD_CREATED, base::GetCurrentProcId(),
       tid, ResSchedRoleAdapter::IMPORTANT_DISPLAY);
   }
-#endif // BUILDFLAG(IS_OHOS)
+#endif
 }
 
 void InProcessGpuThread::CleanUp() {
@@ -125,9 +125,9 @@ base::Thread* CreateInProcessGpuThread(
 #if BUILDFLAG(IS_OHOS)
 int32_t InProcessGpuThread::GetGpuThreadId(int32_t pid)
 {
-  int32_t tid = GetTidListByName(pid, "gpu-worker-server");
+  int32_t tid = GetTidListByName(pid, "gpu-work-server");
   if (tid < 0) {
-    tid = GetTidListByName(pid, "gpu-worker-backe");
+    tid = GetTidListByName(pid, "mali-cmar-backe");
   }
   return tid;
 }
@@ -142,7 +142,7 @@ int32_t InProcessGpuThread::GetTidListByName(int32_t pid, const std::string& thr
   std::string path_name = std::string("/proc/").append(std::to_string(pid)).append("/task");
   DIR *dir = opendir(path_name.c_str());
   if (!dir) {
-    LOG(ERROR) << "opendir " << path_name <<" failed, errno: " << errono;
+    LOG(ERROR) << "opendir " << path_name <<" failed, errno: " << errno;
     return tid;
   }
 
@@ -186,5 +186,5 @@ bool InProcessGpuThread::LoadStringFromFile(const std::string& file_path, std::s
   std::copy(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), std::back_inserter(content));
   return true;
 }
-#endif // BUILDFLAG(IS_OHOS)
+#endif
 }  // namespace content
