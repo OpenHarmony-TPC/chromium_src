8750ede75a88c44fdf2e78c75146a0e06035bcd6
yushihao4 <yushihao4@huawei.com>
2024-01-18 17:24:25 +0800

    [webview]media player video format conversion
    Signed-off-by: yushihao4 <yushihao4@huawei.com>

--- media/base/ohos/ohos_media_player_bridge.cc
+++ media/base/ohos/ohos_media_player_bridge.cc
@@ -356,18 +356,26 @@ void OHOSMediaPlayerBridge::OnBufferAvailable(
     return;
   }
 
-  int32_t coded_height = buffer->GetHeight();
-  int32_t coded_width = buffer->GetStride();
+  int32_t coded_height;
+  int32_t coded_width;
 
-  // argb format video frame stride should divided by 4
+  // video frame height must be 32*N
+  const int step_height = 32;
+  // argb format video frame should divided by 4
   const int argb_stride_step = 4;
-
+  if (buffer->GetHeight() % step_height == 0) {
+    coded_height = buffer->GetHeight();
+  } else {
+    coded_height = (buffer->GetHeight() / step_height + 1) * step_height;
+  }
   if (buffer->GetFormat() ==
       OHOS::NWeb::PixelFormatAdapter::PIXEL_FMT_RGBA_8888) {
-    coded_width /= argb_stride_step;
+    coded_width = buffer->GetStride() / argb_stride_step;
     coded_height = buffer->GetHeight();
+  } else {
+    coded_width = buffer->GetStride();
   }
-  
+
 #if defined(RK3568)
   if (!is_hls_) {
     coded_width = buffer->GetWidth();
