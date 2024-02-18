a3a6a75c7d9a24a85186d91438a2073113fe6230
xuefu zhang <xuefu.sh@gmail.com>
2024-02-18 21:16:36 +0800

    fix: 手机横屏切换过程中,竖屏时保持页面旋转状态和是否双页为初始状态
    Signed-off-by: xuefu zhang <ryze.xuefu@huawei.com>
    Change-Id: I909ad293826a8011e11441c03a3bc469f74e581d
    Signed-off-by: xuefu zhang <ryze.xuefu@huawei.com>

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -2160,6 +2160,13 @@ void PDFiumEngine::RotateCounterclockwise() {
   ProposeNextDocumentLayout();
 }
 
+#if defined(OHOS_PDF)
+void PDFiumEngine::ResetDesiredLayoutOptions() {
+  DocumentLayout::Options default_options;
+  desired_layout_options_ = default_options;
+}
+#endif
+
 bool PDFiumEngine::IsReadOnly() const {
   return read_only_;
 }
