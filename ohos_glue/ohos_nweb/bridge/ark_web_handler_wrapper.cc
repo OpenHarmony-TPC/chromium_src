2f9bcfdaec8152b49c5898991ca7066a94039c43
Lei Gao <leigao@huawei.com>
2024-03-02 09:30:29 +0800

    [Media] Supports to get video natural size when enter fullscreen.
    Signed-off-by: Lei Gao <leigao@huawei.com>
    Change-Id: Id2e0e1e7c88f9d2f1b819b09406cd1c50dc9f0bf

--- ohos_glue/ohos_nweb/bridge/ark_web_handler_wrapper.cc
+++ ohos_glue/ohos_nweb/bridge/ark_web_handler_wrapper.cc
@@ -761,4 +761,19 @@ void ArkWebHandlerWrapper::OnSafeBrowsingCheckResult(int threat_type) {
   ark_web_handler_->OnSafeBrowsingCheckResult(threat_type);
 }
 
+void ArkWebHandlerWrapper::OnFullScreenEnterWithVideoSize(
+    std::shared_ptr<OHOS::NWeb::NWebFullScreenExitHandler> handler,
+    int video_natural_width,
+    int video_natural_height) {
+  if (CHECK_SHARED_PTR_IS_NULL(handler)) {
+    ark_web_handler_->OnFullScreenEnterWithVideoSize(
+        nullptr, video_natural_width, video_natural_height);
+    return;
+  }
+
+  ark_web_handler_->OnFullScreenEnterWithVideoSize(
+      new ArkWebFullScreenExitHandlerImpl(handler), video_natural_width,
+      video_natural_height);
+}
+
 } // namespace OHOS::ArkWeb
