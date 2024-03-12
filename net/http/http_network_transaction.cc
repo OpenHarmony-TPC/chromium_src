bb0b7dde73c6c94c690ca45f4de858a951a6ed78
juwenjing <juwenjing@huawei.com>
2024-03-12 14:30:35 +0800

    code
    Signed-off-by: juwenjing <juwenjing@huawei.com>

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -21,6 +21,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "net/base/auth.h"
