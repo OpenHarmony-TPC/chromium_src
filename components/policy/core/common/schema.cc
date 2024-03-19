c7c2d0960a23053e688700682fd8dd25004c57bd
lizhengkun <lizhengkun3@huawei.com>
2024-03-19 07:05:38 -0400

    Signed-off-by: lizhengkun <lizhengkun3@huawei.com> Change-Id: Ib8bdcc8bc418175ad7f9ac7f4468da8bc88f6260

--- components/policy/core/common/schema.cc
+++ components/policy/core/common/schema.cc
@@ -1530,8 +1530,10 @@ bool CompareKeys(const PropertyNode& node, const std::string& key) {
 }  // namespace
 
 Schema Schema::GetKnownProperty(const std::string& key) const {
-  LOG(INFO) << "Schema::GetKnownProperty current-type is :" << type();
   CHECK(valid());
+  if (base::Value::Type::DICT != type()) {
+    LOG(INFO) << "Schema::GetKnownProperty current-type is :" << type();
+  }
   CHECK_EQ(base::Value::Type::DICT, type());
   const PropertiesNode* node = storage_->properties(node_->extra);
   if (node->begin == kInvalid || node->end == kInvalid)
