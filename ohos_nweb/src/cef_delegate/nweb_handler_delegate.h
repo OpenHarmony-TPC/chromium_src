27e6e2a9e2406298e1914b02de4e4e149242de10
linyunxuan <linyunxuan1@huawei.com>
2024-03-29 16:32:12 +0800

    fix codecheck
    Signed-off-by: linyunxuan <linyunxuan1@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_handler_delegate.h
+++ ohos_nweb/src/cef_delegate/nweb_handler_delegate.h
@@ -177,12 +177,12 @@ class NWebHandlerDelegate : public CefClient,
                              int32_t routing_id,
                              int32_t object_id) override;
   int NotifyJavaScriptResultFlowbuf(CefRefPtr<CefListValue> args,
-                             const CefString& method,
-                             const CefString& object_name,
-                             int fd,
-                             CefRefPtr<CefListValue> result,
-                             int32_t routing_id,
-                             int32_t object_id) override;
+                                    const CefString& method,
+                                    const CefString& object_name,
+                                    int fd,
+                                    CefRefPtr<CefListValue> result,
+                                    int32_t routing_id,
+                                    int32_t object_id) override;
   bool HasJavaScriptObjectMethods(int32_t object_id,
                                   const CefString& method_name) override;
   void GetJavaScriptObjectMethods(
