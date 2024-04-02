798da78c21d20380d0cae4571faddc025149b2a6
gaopengzhen <gaopengzhen@huawei.com>
2024-04-02 14:29:02 +0800

    video_take_over_by_app
    Change-Id: If3e0a5b3afec2ff19c71cd2f9fae060df140fab9
    Signed-off-by: gaopengzhen <gaopengzhen@huawei.com>

--- media/mojo/services/mojo_renderer_service.h
+++ media/mojo/services/mojo_renderer_service.h
@@ -71,6 +71,18 @@ class MEDIA_MOJO_EXPORT MojoRendererService final : public mojom::Renderer,
   void SetCdm(const absl::optional<base::UnguessableToken>& cdm_id,
               SetCdmCallback callback) final;
 
+#if defined(OHOS_CUSTOM_VIDEO_PLAYER)
+  void SetSurfaceId(int surface_id) override;
+  void SetMediaSourceList(
+      std::vector<mojom::MediaSourceInfoPtr> source_infos) override;
+  void SetMediaControls(bool show_media_controls,
+      const std::vector<std::string>& controls_list) override;
+  void SetPoster(const std::string& poster_url) override;
+  void SetAttributes(
+      const base::flat_map<std::string, std::string>& attributes) override;
+  void SetIsAudio(bool is_audio) override;
+#endif // OHOS_CUSTOM_VIDEO_PLAYER
+
  private:
   enum State {
     STATE_UNINITIALIZED,
