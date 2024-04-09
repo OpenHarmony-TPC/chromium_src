deb2929c282ccc45ea9451ba288f0bc214135779
hwchenhongyu <chenhongyu15@huawei.com>
2024-04-09 07:02:41 +0000

    修改指令数开关名称
    Signed-off-by: hwchenhongyu <chenhongyu15@huawei.com>

--- ui/events/gestures/blink/web_gesture_curve_impl.cc
+++ ui/events/gestures/blink/web_gesture_curve_impl.cc
@@ -52,7 +52,7 @@ std::unique_ptr<GestureCurve> CreateDefaultPlatformCurve(
   bool use_native_fling_curve = false;
 #ifdef USE_NATIVE_FLING_CURVE
   if (!OHOS::NWeb::OhosAdapterHelper::GetInstance().GetSystemPropertiesInstance().GetBoolParameter(
-      "web.instruction", 0)) {
+      "web.instructionOptimize.enable", 0)) {
     use_native_fling_curve = true;
   }
 #endif
