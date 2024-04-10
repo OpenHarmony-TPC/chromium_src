f1550be181f98db6e2089c5f2025f97d6d5ea213
lichenwei <lichenwei2@huawei.com>
2024-04-10 10:44:39 +0800

    修改评审意见
    Signed-off-by: lichenwei <lichenwei2@huawei.com>

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -2653,6 +2653,9 @@ void RenderWidgetHostImpl::UpdateBrowserControlsState(
 void RenderWidgetHostImpl::GetWordSelection(const std::string& text,
                                             int8_t offset,
                                             GetWordSelectionCallback callback) {
+  if (!view_) {
+    return;
+  }
   std::vector<int8_t> select = view_->GetWordSelection(text, offset);
   std::move(callback).Run(select);
 }
