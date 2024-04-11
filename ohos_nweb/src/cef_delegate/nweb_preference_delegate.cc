464338218ac7d78e5c5c9430af6bf1271011cf50
yuhaoge <yuhaoge1@huawei.com>
2024-04-11 18:19:27 +0800

    mediaOptions attribute bugfix
    Signed-off-by: yuhaoge <yuhaoge1@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_preference_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_preference_delegate.cc
@@ -761,4 +761,21 @@ void NWebPreferenceDelegate::SetNativeVideoPlayerConfig(bool enable,
   WebPreferencesChanged();
 }
 
+#if defined(OHOS_MEDIA_POLICY)
+void NWebPreferenceDelegate::PutAudioExclusive(bool audioExclusive) {
+  audio_exclusive_ = audioExclusive;
+}
+
+bool NWebPreferenceDelegate::GetAudioExclusive() {
+  return audio_exclusive_;
+}
+
+void NWebPreferenceDelegate::PutAudioResumeInterval(int32_t resumeInterval) {
+  resume_interval_ = resumeInterval;
+}
+
+int32_t NWebPreferenceDelegate::GetAudioResumeInterval() {
+  return resume_interval_;
+}
+#endif
 }  // namespace OHOS::NWeb
