5fd1a1063ecdfb7921c2b6039ac98732152ec0d7
LXinJie <liuxinjie6@huawei.com>
2024-04-19 17:39:56 +0800

    liuxinjie6@huawei.com
    Signed-off-by: LXinJie <liuxinjie6@huawei.com>

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -351,6 +351,7 @@ void Compositor::SetLayerTreeFrameSink(
     display_private_->SetDisplayColorMatrix(
         gfx::SkM44ToTransform(display_color_matrix_));
     display_private_->SetOutputIsSecure(output_is_secure_);
+    display_private_->SetDrawMode(drawMode_);
     if (has_vsync_params_) {
       display_private_->SetDisplayVSyncParameters(vsync_timebase_,
                                                   vsync_interval_);
@@ -474,10 +475,13 @@ void Compositor::SetDrawRect(const gfx::Rect& new_rect) {
 }
 
 void Compositor::SetDrawMode(const int32_t& mode) {
-  if (display_private_) {
-    TRACE_EVENT0("viz", "Compositor::SetDrawMode");
-    mojo::SyncCallRestrictions::ScopedAllowSyncCall scoped_allow_sync_call;
-    display_private_->SetDrawMode(mode);
+  if (drawMode_ != mode) {
+    drawMode_ = mode;
+    if (display_private_) {
+      TRACE_EVENT0("viz", "Compositor::SetDrawMode");
+      mojo::SyncCallRestrictions::ScopedAllowSyncCall scoped_allow_sync_call;
+      display_private_->SetDrawMode(drawMode_);
+    }
   }
 }
 #endif  // defined(OHOS_COMPOSITE_RENDER)
