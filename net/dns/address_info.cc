96aa1d63203e743347d280f6d57f81edbe506a40
wu_zhende <wuzhende2@huawei.com>
2024-04-21 10:48:22 +0800

    web加载解析dns失败时增加错误日志
    Signed-off-by: wu_zhende <wuzhende2@huawei.com>

--- net/dns/address_info.cc
+++ net/dns/address_info.cc
@@ -208,6 +208,9 @@ std::unique_ptr<addrinfo, FreeAddrInfoFunc> AddrInfoGetter::getaddrinfo(
 #if BUILDFLAG(IS_WIN)
     *out_os_error = WSAGetLastError();
 #endif
+#ifdef OHOS_NETWORK_LOAD
+    LOG(ERROR) << "get address info failed, out_os_error is: " << *out_os_error;
+#endif
     return rv;
   }
 
