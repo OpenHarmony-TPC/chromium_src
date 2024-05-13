0dd6e2701407933433ad7286ca2100806fc03c71
yuhaoge <yuhaoge1@huawei.com>
2024-05-13 12:07:31 +0800

    web screen info bugfix
    Signed-off-by: yuhaoge <yuhaoge1@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_render_handler.cc
+++ ohos_nweb/src/cef_delegate/nweb_render_handler.cc
@@ -372,7 +372,9 @@ bool NWebRenderHandler::IsNeedCefNotifyScreenInfoChanged() {
   if (last_screen_info_.display_ratio <= 0 || screen_info_.display_ratio <= 0) {
     return false;
   }
-  return last_screen_info_.display_ratio != screen_info_.display_ratio;
+  return last_screen_info_.display_ratio != screen_info_.display_ratio ||
+         last_screen_info_.width != screen_info_.width ||
+         last_screen_info_.height != screen_info_.height;
 }
 // #endif  // #ifdef OHOS_SCREEN_ROTATION
 
