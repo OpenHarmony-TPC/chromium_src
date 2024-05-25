def51719d8f936528438f52c6b2666ad1b84454d
gaopengzhen <gaopengzhen@huawei.com>
2024-05-25 15:58:54 +0800

    [视频托管] 解决某些场景托管画面模糊问题
    Change-Id: If9c4777c7e74e4d081ec55b918078d3080294cd9
    Signed-off-by: gaopengzhen <gaopengzhen@huawei.com>

--- media/base/renderer.h
+++ media/base/renderer.h
@@ -167,7 +167,7 @@ class MEDIA_EXPORT Renderer {
   virtual void SetPoster(const std::string& poster_url) {}
   virtual void SetAttributes(base::flat_map<std::string, std::string> attributes) {}
   virtual void SetReferrer(const std::string& referrer) {}
-  using OnGetRectCallback = base::OnceCallback<void(const gfx::Rect&)>;
+  using OnGetRectCallback = base::RepeatingCallback<void(const gfx::Rect&)>;
   using SurfaceCreatedCallback = base::OnceCallback<void(int, OnGetRectCallback)>;
   virtual void SetSurfaceCreatedCallback(SurfaceCreatedCallback cb) {}
   using UpdatePlaybackStatusCallback = base::RepeatingCallback<void(uint32_t)>;
