edb6de1e7309832538a6887534ee5913fcec9ba7
chenxin128 <chenxin128@huawei.com>
2024-05-29 16:43:29 +0800

    add log
    Signed-off-by: chenxin128 <chenxin128@huawei.com>

--- base/datashare_uri_utils.cc
+++ base/datashare_uri_utils.cc
@@ -13,8 +13,7 @@ namespace base {
 
 File OpenDatashareUriForRead(const FilePath& datashare_uri) {
   std::string real_path = GetRealPath(datashare_uri);
-  LOG(INFO) << "open datashare for read, uri: " <<  datashare_uri.value()
-            << ", realpath: " << real_path;
+  LOG(INFO) << "open datashare for read invoked";
   return File(FilePath(real_path), File::FLAG_OPEN | File::FLAG_READ);
 }
 std::u16string GetFileDisplayName(const FilePath& datashare_uri) {
