d11340d7ddaf30b1afd00c8013610e6362d2ca2e
zzhangkai <zhangkai284@huawei.com>
2024-06-03 21:27:48 +0800

    解决光标从web网页的input 标签到ArkUI的input控件切换时 输入法键盘出现隐藏再拉起的问题
    Signed-off-by: zzhangkai <zhangkai284@huawei.com>

--- ohos_nweb/src/nweb_impl.cc
+++ ohos_nweb/src/nweb_impl.cc
@@ -1602,7 +1602,7 @@ void NWebImpl::OnBlur(const BlurReason& blurReason) {
     return;
   }
 #endif  // #ifdef OHOS_FOCUS
-  if (blurReason != OHOS::NWEB::BlurReason::FOCUS_SWITCH) {
+  if (blurReason != OHOS::NWeb::BlurReason::FOCUS_SWITCH) {
     inputmethod_handler_->HideTextInput(
         nweb_id_, NWebInputMethodClient::HideTextinputType::FROM_ONBLUR);
   }
