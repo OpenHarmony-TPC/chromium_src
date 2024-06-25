2d458e2f52a1f7d94a602f5409cb54c502ec39e8
tengfan <tengfan3@huawei.com>
2024-06-25 15:42:57 +0800

    revert web播控前后台切换crash
    Signed-off-by: tengfan <tengfan3@huawei.com>

--- ohos_nweb/browser/performance_manager/policies/background_task_policy.cc
+++ ohos_nweb/browser/performance_manager/policies/background_task_policy.cc
@@ -89,13 +89,6 @@ void BackgroundTaskPolicy::OnIsVisibleChanged(const PageNode* page_node) {
             << (visible_num > 0 ? "true" : "false")
             << ", visible_page_num: " << visible_page_num_;
   MaybeChangeBackgroundTask(page_node);
-  auto webcontents = page_node->GetContentsProxy().Get();
-  content::MediaSessionImpl* mediaSession =
-      content::MediaSessionImpl::Get(webcontents);
-  if (!mediaSession || !webcontents) {
-    return;
-  }
-  mediaSession->SetWebviewShow(visible_num > 0);
 }
 
 void BackgroundTaskPolicy::OnIsMediaPlayingChanged(const PageNode* page_node) {
