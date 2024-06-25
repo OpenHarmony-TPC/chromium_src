894910289ce55d95c0299f3df307c7d8dcc79293
杨文雪 <yangwenxue1@huawei.com>
2024-06-25 13:28:13 +0000

    deps
    Signed-off-by: 杨文雪 <yangwenxue1@huawei.com>

--- net/prp_preload/src/res_preload_scheduler.cc
+++ net/prp_preload/src/res_preload_scheduler.cc
@@ -5,7 +5,12 @@
 #include "res_preload_scheduler.h"
 
 #include "base/logging.h"
+#include "net/base/http_user_agent_settings.h"
 #include "net/base/network_anonymization_key.h"
+#include "net/http/http_network_session.h"
+#include "net/http/http_transaction_factory.h"
+#include "net/http/transport_security_state.h"
+#include "net/url_request/url_request_context.h"
 #include "url/origin.h"
 
 namespace {
@@ -19,8 +24,8 @@ namespace ohos_prp_preload {
 
 ResPreloadScheduler::ResPreloadScheduler(const scoped_refptr<base::SingleThreadTaskRunner>& sth_task_runner,
   const scoped_refptr<base::SingleThreadTaskRunner>& net_task_runner,
-  const base::WeakPtr<network::NetworkContext>& network_context) :
-  sth_task_runner_(sth_task_runner), net_task_runner_(net_task_runner), network_context_(network_context) {}
+  net::URLRequestContext* url_request_context) :
+  sth_task_runner_(sth_task_runner), net_task_runner_(net_task_runner), url_request_context_(url_request_context) {}
 
 void ResPreloadScheduler::PreloadSchedule(const std::list<std::shared_ptr<PRRequestInfo>>& res_req_info_list) {
   LOG(DEBUG) << "PRPPreload.ResPreloadScheduler::PreloadSchedule " << res_req_info_list.size();
@@ -44,14 +49,13 @@ void ResPreloadScheduler::PreloadSchedule(const std::list<std::shared_ptr<PRRequ
           ++socket_connected_;
           LOG(DEBUG) << "PRPPreload.ResPreloadScheduler::PreloadSchedule preconnect " << url;
           net_task_runner_->PostTask(FROM_HERE, base::BindOnce(&network::NetworkContext::PreconnectSockets,
-                                     network_context_, 1, url.GetURL(), info->allow_credentials(),
-                                     net::NetworkAnonymizationKey::CreateSameSite(net::SchemefulSite(url))));
+                                     weak_factory_.GetWeakPtr(), url.GetURL(), info->allow_credentials()));
         }
       } else {
         if (sth_task_runner_ != nullptr) {
           sth_task_runner_->PostDelayedTask(FROM_HERE,
             base::BindOnce(&ResPreloadScheduler::PreconnectBeyondLimit, weak_factory_.GetWeakPtr(),
-            iter, network_context_, info_list_version_), base::Milliseconds(DELAYED_TIME));
+            iter, info_list_version_), base::Milliseconds(DELAYED_TIME));
         }
         return;
       }
@@ -80,9 +84,9 @@ bool ResPreloadScheduler::NeedToPreconnect(const GURL& url, bool allow_credentia
 }
 
 void ResPreloadScheduler::PreconnectBeyondLimit(InfoIter info_iter,
-  const base::WeakPtr<network::NetworkContext>& network_context, const int info_list_version) {
+                                                const int info_list_version) {
   if (info_list_version != info_list_version_ || info_iter == info_list_.end() ||
-      network_context == nullptr || !preload_triggered_) {
+      url_request_context_ == nullptr || !preload_triggered_) {
     return;
   }
 
@@ -93,17 +97,62 @@ void ResPreloadScheduler::PreconnectBeyondLimit(InfoIter info_iter,
     if (NeedToPreconnect(url.GetURL(), info->allow_credentials()) && net_task_runner_ != nullptr) {
       ++socket_connected_;
       LOG(DEBUG) << "PRPPreload.ResPreloadScheduler::PreconnectBeyondLimit preconnect " << url;
-      net_task_runner_->PostTask(FROM_HERE, base::BindOnce(&network::NetworkContext::PreconnectSockets,
-                                 network_context_, 1, url.GetURL(), info->allow_credentials(),
-                                 net::NetworkAnonymizationKey::CreateSameSite(net::SchemefulSite(url))));
+      net_task_runner_->PostTask(FROM_HERE, base::BindOnce(&ResPreloadScheduler::PreconnectSocket,
+                                 weak_factory_.GetWeakPtr(), url.GetURL(), info->allow_credentials()));
     }
   }
   ++info_iter;
   if (sth_task_runner_ != nullptr && info_iter != info_list_.end()) {
     sth_task_runner_->PostDelayedTask(FROM_HERE,
       base::BindOnce(&ResPreloadScheduler::PreconnectBeyondLimit, weak_factory_.GetWeakPtr(),
-      info_iter, network_context_, info_list_version), base::Milliseconds(DELAYED_TIME));
+      info_iter, info_list_version), base::Milliseconds(DELAYED_TIME));
   }
+}
+
+void ResPreloadScheduler::PreconnectSocket(const GURL& original_url, bool allow_credentials) {
+  net::NetworkAnonymizationKey key =
+    net::NetworkAnonymizationKey::CreateSameSite(net::SchemefulSite(original_url));
+
+  GURL url = GetHSTSRedirect(original_url);
+
+  std::string user_agent;
+  if (url_request_context_->http_user_agent_settings()) {
+    user_agent =
+        url_request_context_->http_user_agent_settings()->GetUserAgent();
+  }
+  net::HttpRequestInfo request_info;
+  request_info.url = url;
+  request_info.method = net::HttpRequestHeaders::kGetMethod;
+  request_info.extra_headers.SetHeader(net::HttpRequestHeaders::kUserAgent,
+                                       user_agent);
+
+  if (allow_credentials) {
+    request_info.load_flags = net::LOAD_NORMAL;
+    request_info.privacy_mode = net::PRIVACY_MODE_DISABLED;
+  } else {
+    request_info.load_flags = net::LOAD_DO_NOT_SAVE_COOKIES;
+    request_info.privacy_mode = net::PRIVACY_MODE_ENABLED;
+  }
+  request_info.network_anonymization_key = key;
+
+  net::HttpTransactionFactory* factory =
+      url_request_context_->http_transaction_factory();
+  net::HttpNetworkSession* session = factory->GetSession();
+  net::HttpStreamFactory* http_stream_factory = session->http_stream_factory();
+  http_stream_factory->PreconnectStreams(1, request_info);
+}
+
+GURL ResPreloadScheduler::GetHSTSRedirect(const GURL& original_url) {
+  if (!url_request_context_->transport_security_state() ||
+      !original_url.SchemeIs("http") ||
+      !url_request_context_->transport_security_state()->ShouldUpgradeToSSL(
+          original_url.host())) {
+    return original_url;
+  }
+
+  GURL::Replacements replacements;
+  replacements.SetSchemeStr("https");
+  return original_url.ReplaceComponents(replacements);
 }
 
 }  // namespace ohos_prp_preload
