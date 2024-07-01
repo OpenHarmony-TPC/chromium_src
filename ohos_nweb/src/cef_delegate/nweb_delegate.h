cd03e4a30b1472429ecd1db2deec9dc81e9d4f0d
yushihao4 <yushihao4@huawei.com>
2024-07-01 10:26:46 +0800

    [webview]The slider and mouse wheel carry keyboard information
    Signed-off-by: yushihao4 <yushihao4@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_delegate.h
+++ ohos_nweb/src/cef_delegate/nweb_delegate.h
@@ -357,6 +357,16 @@ bool HitNativeArea(double x, double y);
   void SlideScroll(float vx, float vy) override;
   bool WebSendKeyEvent(int32_t keyCode, int32_t keyAction,
                        const std::vector<int32_t>& pressedCodes) override;
+  void WebSendMouseWheelEvent(double x,
+                              double y,
+                              double deltaX,
+                              double deltaY,
+                              const std::vector<int32_t>& pressedCodes) override;
+  void WebSendTouchpadFlingEvent(double x,
+                                 double y,
+                                 double vx,
+                                 double vy,
+                                 const std::vector<int32_t>& pressedCodes) override;
 #endif  // defined(OHOS_INPUT_EVENTS)
 
 #ifdef OHOS_ARKWEB_ADBLOCK
--- ohos_nweb/src/cef_delegate/nweb_input_delegate.h
+++ ohos_nweb/src/cef_delegate/nweb_input_delegate.h
@@ -34,6 +34,7 @@ class NWebInputDelegate {
   static bool IsMMIKeyEvent(int32_t keyCode);
   static uint32_t GetWebModifiers(int32_t keyCode, int32_t keyAction,
                                   const std::vector<int32_t>& pressedCodes);
+  static uint32_t GetWebModifiersByPressedCode(const std::vector<int32_t>& pressedCodes);
   void SetModifiers(int keyCode, int keyAction);
   uint32_t GetModifiers();
   uint32_t GetModifiers(cef_mouse_button_type_t button);
