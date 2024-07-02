baab4ed03df0bdd920395cbf45f247d80ac525e1
hwchenhongyu <chenhongyu15@huawei.com>
2024-07-02 12:56:29 +0000

    增加脏区传递
    Signed-off-by: hwchenhongyu <chenhongyu15@huawei.com>

--- ui/gl/gl_surface.cc
+++ ui/gl/gl_surface.cc
@@ -103,6 +103,14 @@ gfx::SwapResult GLSurface::PostSubBuffer(int x,
   return gfx::SwapResult::SWAP_FAILED;
 }
 
+#if BUILDFLAG(IS_OHOS)
+gfx::SwapResult GLSurface::SwapBuffersWithDamage(const std::vector<int>& rects,
+                                                 PresentationCallback callback,
+                                                 gfx::FrameData data) {
+  return gfx::SwapResult::SWAP_FAILED;
+}
+#endif
+
 void GLSurface::PostSubBufferAsync(int x,
                                    int y,
                                    int width,
@@ -326,6 +334,14 @@ gfx::SwapResult GLSurfaceAdapter::SwapBuffersWithBounds(
     gfx::FrameData data) {
   return surface_->SwapBuffersWithBounds(rects, std::move(callback), data);
 }
+
+#if BUILDFLAG(IS_OHOS)
+gfx::SwapResult GLSurfaceAdapter::SwapBuffersWithDamage(const std::vector<int>& rects,
+                                                        PresentationCallback callback,
+                                                        gfx::FrameData data) {
+  return surface_->SwapBuffersWithDamage(rects, std::move(callback), data);
+}
+#endif
 
 gfx::SwapResult GLSurfaceAdapter::PostSubBuffer(int x,
                                                 int y,
