8d2d27fb627c0a5837c42425fcafb250d8245207
gaojianhao1 <gaojianhao@huawei.com>
2024-07-07 03:08:40 +0800

    test together.
    Signed-off-by: gaojianhao1 <gaojianhao@huawei.com>

--- ohos_nweb/browser/performance_manager/nweb_performance_manager.cc
+++ ohos_nweb/browser/performance_manager/nweb_performance_manager.cc
@@ -18,6 +18,8 @@
 #include "ohos_nweb/browser/performance_manager/policies/background_task_policy.h"
 #include "ohos_nweb/browser/performance_manager/policies/ohos_bfcache_policy.h"
 
+#include "base/logging.h"
+
 namespace OHOS::NWeb {
 // static
 void NwebPerformanceManager::CreatePoliciesAndDecorators(
