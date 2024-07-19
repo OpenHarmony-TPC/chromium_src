cc106dee7667dc2100376a806a0427712fe31cb7
xiongjun_gitee <xiongjun28@huawei.com>
2024-07-19 16:09:03 +0800

    解决有些网页避让不了软键盘
    Signed-off-by: xiongjun_gitee <xiongjun28@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -731,11 +731,10 @@ void NWebDelegate::Resize(uint32_t width, uint32_t height, bool isKeyboard) {
 
   auto browser = GetBrowser();
   if (browser != nullptr && browser->GetHost() != nullptr) {
-    if (isKeyboard) {
-      browser->GetHost()->WasKeyboardResized();
-    } else {
-      browser->GetHost()->WasResized();
+    if (isKeyboard && render_handler_) {
+      render_handler_->SetNeedFocusViewport(true);
     }
+    browser->GetHost()->WasResized();
     browser->GetHost()->OnTextSelected(false);
   }
 }
@@ -757,11 +756,10 @@ void NWebDelegate::ResizeVisibleViewport(uint32_t width, uint32_t height, bool i
 
   auto browser = GetBrowser();
   if (browser != nullptr && browser->GetHost() != nullptr) {
-    if (isKeyboard) {
-      browser->GetHost()->WasKeyboardResized();
-    } else {
-      browser->GetHost()->WasResized();
+    if (isKeyboard && render_handler_) {
+      render_handler_->SetNeedFocusViewport(true);
     }
+    browser->GetHost()->WasResized();
     browser->GetHost()->OnTextSelected(false);
   }
 }
