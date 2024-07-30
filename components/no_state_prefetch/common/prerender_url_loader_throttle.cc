72b9df39ac1fdb88e6882290f745d9716bd32a52
xushawly <xushawly.xu@huawei.com>
2024-07-30 14:54:16 +0800

    ArkWeb的Prerender的请求头部中不再携带Purpose字段
    Signed-off-by: xushawly <xushawly.xu@huawei.com>
    Change-Id: I279fc129ffea61dede64adee704a001e3494f0fe
    Signed-off-by: xushawly <xushawly.xu@huawei.com>

--- components/no_state_prefetch/common/prerender_url_loader_throttle.cc
+++ components/no_state_prefetch/common/prerender_url_loader_throttle.cc
@@ -19,8 +19,10 @@ namespace prerender {
 
 namespace {
 
+#if !defined(OHOS_NO_STATE_PREFETCH)
 const char kPurposeHeaderName[] = "Purpose";
 const char kPurposeHeaderValue[] = "prefetch";
+#endif  // !defined(OHOS_NO_STATE_PREFETCH)
 
 void CallCancelPrerenderForUnsupportedScheme(
     mojo::PendingRemote<prerender::mojom::PrerenderCanceler> canceler) {
@@ -65,8 +67,10 @@ void PrerenderURLLoaderThrottle::WillStartRequest(
     network::ResourceRequest* request,
     bool* defer) {
   request->load_flags |= net::LOAD_PREFETCH;
+#if !defined(OHOS_NO_STATE_PREFETCH)
   request->cors_exempt_headers.SetHeader(kPurposeHeaderName,
                                          kPurposeHeaderValue);
+#endif  // !defined(OHOS_NO_STATE_PREFETCH)
 
   request_destination_ = request->destination;
   // Abort any prerenders that spawn requests that use unsupported HTTP
