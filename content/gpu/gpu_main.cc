12d6378d083a95ce07d8833d55a9c8aba292a640
ZhaoPengfei <zhaopengfei35@huawei.com>
2024-08-06 20:20:32 +0800

    fix bug of gpu-work-server
    Signed-off-by: ZhaoPengfei <zhaopengfei35@huawei.com>

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -110,6 +110,10 @@
 
 #if BUILDFLAG(IS_OHOS)
 #include "base/trace_event/trace_event_ohos.h"
+#include "gpu/ipc/common/nweb_native_window_tracker.h"
+#include <fstream>
+#include <dirent.h>
+#include "res_sched_client_adapter.h"
 #endif
 
 namespace content {
@@ -122,6 +126,12 @@ bool StartSandboxLinux(gpu::GpuWatchdogThread*,
                        const gpu::GpuPreferences&);
 #elif BUILDFLAG(IS_WIN)
 bool StartSandboxWindows(const sandbox::SandboxInterfaceInfo*);
+#endif
+
+#if BUILDFLAG(IS_OHOS)
+int32_t GetTidListByName(int32_t pid, const std::string& thread_name);
+bool LoadStringFromFile(const std::string& file_path, std::string& content);
+const int MAX_FILE_LENGTH = 32* 1024 * 1024;
 #endif
 
 class ContentSandboxHelper : public gpu::GpuSandboxHelper {
@@ -415,6 +425,28 @@ int GpuMain(MainFunctionParams parameters) {
   base::allocator::PartitionAllocSupport::Get()->ReconfigureAfterTaskRunnerInit(
       switches::kGpuProcess);
 
+#if BUILDFLAG(IS_OHOS)
+  using namespace OHOS::NWeb;
+
+  auto pid = base::GetCurrentProcId();
+  int32_t tid = GetTidListByName(pid, "gpu-work-server");
+  if (tid < 0) {
+    tid = GetTidListByName(pid, "mali-cmar-backe");
+  }
+  if(tid > 0) {
+    auto type = base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+      switches::kProcessType);
+    if (type == switches::kGpuProcess) {
+      NWebNativeWindowTracker::Get()->g_browser_client_->ReportThread(
+        ResSchedStatusAdapter::THREAD_CREATED, base::GetCurrentProcId(),
+        tid, ResSchedRoleAdapter::IMPORTANT_DISPLAY);
+    } else {
+      ResSchedClientAdapter::ReportKeyThread(
+        ResSchedStatusAdapter::THREAD_CREATED, base::GetCurrentProcId(),
+        tid, ResSchedRoleAdapter::IMPORTANT_DISPLAY);
+    }
+  }
+#endif
   base::HighResolutionTimerManager hi_res_timer_manager;
 
   {
@@ -510,6 +542,64 @@ bool StartSandboxWindows(const sandbox::SandboxInterfaceInfo* sandbox_info) {
   return false;
 }
 #endif  // BUILDFLAG(IS_WIN)
+
+#if BUILDFLAG(IS_OHOS)
+int32_t GetTidListByName(int32_t pid, const std::string& thread_name)
+{
+  int32_t tid = -1;
+  if (pid <= 0) {
+    return tid;
+  }
+
+  std::string path_name = std::string("/proc/").append(std::to_string(pid)).append("/task");
+  DIR *dir = opendir(path_name.c_str());
+  if (!dir) {
+    LOG(ERROR) << "opendir " << path_name <<" failed, errno: " << errno;
+    return tid;
+  }
+
+  struct dirent *de = nullptr;
+  while ((de = readdir(dir))) {
+    if (!(de->d_type & DT_DIR) || !isdigit(de->d_name[0])) {
+        continue;
+    }
+    std::string comm_path = path_name + std::string("/").append(de->d_name).append("/comm");
+    std::string comm;
+    if (!LoadStringFromFile(comm_path, comm)) {
+        continue;
+    }
+    if (tid < 0 && comm.find(thread_name) != std::string::npos) {
+        tid = atoi(de->d_name);
+        if (tid >= 0) {
+            break;
+        }
+    }
+  }
+  closedir(dir);
+  return tid;
+}
+
+bool LoadStringFromFile(const std::string& file_path, std::string& content)
+{
+  std::ifstream file(file_path.c_str());
+  if (!file.is_open()) {
+    LOG(ERROR) << "open file failed! file path: " << file_path;
+    return false;
+  }
+
+  file.seekg(0, std::ios::end);
+  int file_length = file.tellg();
+  if (file_length > MAX_FILE_LENGTH) {
+    LOG(ERROR) << "invalid file length: " << file_length;
+    return false;
+  }
+  content.clear();
+  file.seekg(0, std::ios::beg);
+  std::copy(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>(), std::back_inserter(content));
+  return true;
+}
+#endif
+
 
 }  // namespace.
 
