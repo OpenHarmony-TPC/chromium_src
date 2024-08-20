5db4a9fac5a620faec6c119a0a19a0884b9abcaa
yushihao4 <yushihao4@huawei.com>
2024-08-20 10:03:47 +0800

    [webview]clear codecheck
    Signed-off-by: yushihao4 <yushihao4@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_event_handler.h
+++ ohos_nweb/src/cef_delegate/nweb_event_handler.h
@@ -73,6 +73,14 @@ class NWebEventHandler {
   bool SendKeyEvent(int32_t keyCode, int32_t keyAction);
   bool WebSendKeyEvent(int32_t keyCode, int32_t keyAction,
                        const std::vector<int32_t>& pressedCodes);
+  bool CreateCefKeyEvent(CefKeyEvent& keyEvent,
+                         int32_t keyCode, int32_t keyAction, int32_t modifiers);
+  void SendCefKeyEvent(CefKeyEvent& keyEvent);
+  void SendCefMouseWheelEvent(double x,
+                              double y,
+                              double deltaX,
+                              double deltaY,
+                              int32_t modifiers);
 #endif  // defined(OHOS_INPUT_EVENTS)
 
   CefRefPtr<CefBrowser> browser_ = nullptr;
--- ui/touch_selection/touch_selection_controller.cc
+++ ui/touch_selection/touch_selection_controller.cc
@@ -397,7 +397,7 @@ void TouchSelectionController::ResetResponsePendingInputEvent() {
 #ifdef OHOS_CLIPBOARD
 void  TouchSelectionController::UpdateSelectionChanged(
     const TouchSelectionDraggable& draggable) {
-  if(&draggable != insertion_handle_.get()) {
+  if (&draggable != insertion_handle_.get()) {
     client_->OnSelectionEvent(SELECTION_HANDLES_UPDATEMENU);
   }
 }
