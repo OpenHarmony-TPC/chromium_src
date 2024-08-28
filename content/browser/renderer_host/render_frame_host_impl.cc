161b347999ff84a86f0467913ecd07e0e3b76317
chendawei8@huawei.com <chendawei8@huawei.com>
2024-08-28 16:14:52 +0800

    [TicketNo:]DTS2024082406385 [Description:]增加非空验证 [Binary Source:]NA
    Signed-off-by: chendawei8@huawei.com <chendawei8@huawei.com>

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -5076,9 +5076,15 @@ void RenderFrameHostImpl::DetachFromProxy() {
   if (IsPendingDeletion())
     return;
 
+
   // Start pending deletion on this frame and its children.
   DeleteRenderFrame(mojom::FrameDeleteIntention::kNotMainFrame);
   StartPendingDeletionOnSubtree(PendingDeletionReason::kFrameDetach);
+#if BUILDFLAG(IS_OHOS)
+  if (!frame_tree()) {
+    return;
+  }
+#endif
   frame_tree()->FrameUnloading(GetFrameTreeNodeForUnload());
 
   // Some children with no unload handler may be eligible for immediate
