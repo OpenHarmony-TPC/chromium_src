76a305ecf0c3c2674b240082d0cc80b61959f1f8
huangjunjie <huangjunjie31@huawei.com>
2024-09-03 14:44:56 +0800

    增加HEIF格式图片的开关
    Signed-off-by: huangjunjie <huangjunjie31@huawei.com>

--- components/viz/service/gl/gpu_service_impl.cc
+++ components/viz/service/gl/gpu_service_impl.cc
@@ -136,11 +136,11 @@
 #if BUILDFLAG(IS_OHOS)
 #include "base/ohos/dynamic_frame_loss_monitor.h"
 #include "gpu/ipc/common/gpu_surface_id_tracker.h"
-#if BUILDFLAG(ENABLE_HEIF_DECODER)
 #include "base/ohos/ltpo/include/sliding_observer.h"
-#endif // BUILDFLAG(ENABLE_HEIF_DECODER)
 #include "base/ohos/ltpo/include/dynamic_frame_rate_decision.h"
+#if BUILDFLAG(ENABLE_HEIF_DECODER)
 #include "media/gpu/ohos/ohos_image_decode_accelerator_worker.h"
+#endif // BUILDFLAG(ENABLE_HEIF_DECODER)
 #endif
 
 namespace viz {
