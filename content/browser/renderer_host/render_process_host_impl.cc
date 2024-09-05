906fb385be5e3c8c226cd760174cd78bcfdf6984
zhushengle <zhushengle@huawei.com>
2024-09-05 17:48:37 +0800

    fix: 优化启动日志
    Signed-off-by: zhushengle <zhushengle@huawei.com>
    Change-Id: I257fd3fb7cac825aa3150802bbeb09ebcecf6af4

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -3849,7 +3849,7 @@ bool RenderProcessHostImpl::FastShutdownIfPossible(size_t page_count,
   // died due to fast shutdown versus another cause.
   fast_shutdown_started_ = true;
 
-  LOG(INFO) << "Rended process: " << GetID() << " died due to fast shutdown versus another cause";
+  LOG(INFO) << "Rended process: " << GetProcess().Handle() << " died due to fast shutdown versus another cause";
   ChildProcessTerminationInfo info;
   info.status = base::TERMINATION_STATUS_PROCESS_WAS_KILLED;
   info.exit_code = 0;
@@ -4942,7 +4942,8 @@ RenderProcessHost* RenderProcessHostImpl::GetProcessHostForSiteInstance(
     if (render_process_host) {
       site_instance->set_process_assignment(
           SiteInstanceProcessAssignment::REUSED_EXISTING_PROCESS);
-      LOG(INFO) << "Use an existing rendering process, render_process: " << render_process_host->GetID();
+      LOG(INFO) << "Use an existing rendering process, render_process: "
+                << render_process_host->GetProcess().Handle();
     }
   }
 
@@ -4999,6 +5000,8 @@ RenderProcessHost* RenderProcessHostImpl::GetProcessHostForSiteInstance(
               RenderProcessHostImpl::GetExistingBackgroundProcessHost(
                   site_instance));
       if (render_host) {
+        LOG(INFO) << "Try fast shutdown idle render process: "
+                  << render_host->GetProcess().Handle() << " in the background.";
         render_host->FastShutdownIfPossible(1u, true);
       }
     }
