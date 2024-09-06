522835ac62deafa86c164d2041eb903ecb3ef668
tengfan66 <tengfan3@huawei.com>
2024-09-06 18:31:51 +0800

    update new cases
    Signed-off-by: tengfan66 <tengfan3@huawei.com>

--- ohos_nweb/src/nweb_impl.cc
+++ ohos_nweb/src/nweb_impl.cc
@@ -3510,7 +3510,6 @@ void NWebImpl::TrimMemoryByPressureLevel(int32_t memoryLevel) {
 void NWebImpl::SetPopupSurface(void* popupSurface) {
 
   uint32_t width, height;
-  output_handler_->GetWindowInfo(width, height);
   if (nweb_delegate_ == nullptr) {
     WVLOG_E(
         "SetPopupSurface failed,nweb_delegate is nullptr.");
@@ -3521,6 +3520,7 @@ void NWebImpl::SetPopupSurface(void* popupSurface) {
     WVLOG_E("SetPopupSurface failed, NWeb output handler is not ready");
     return;
   }
+  output_handler_->GetWindowInfo(width, height);
   void* popup_window = nullptr;
   popup_window = output_handler_->GetNativeWindowFromSurface(popupSurface);
 
