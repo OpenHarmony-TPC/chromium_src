8ba382dc261c66d2fbeeb23ff79bf27e95cf0205
杨文雪 <yangwenxue1@huawei.com>
2024-09-06 08:48:19 +0000

    trace整改
    Signed-off-by: 杨文雪 <yangwenxue1@huawei.com>

--- net/prp_preload/src/preconnect_runner.cc
+++ net/prp_preload/src/preconnect_runner.cc
@@ -6,7 +6,6 @@
 
 #include "net/base/http_user_agent_settings.h"
 #include "net/base/network_anonymization_key.h"
-#include "net/base/tracing.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_transaction_factory.h"
 #include "net/http/transport_security_state.h"
@@ -21,7 +20,6 @@ void PreconnectRunner::PreconnectSocket(
   if (url_request_context.get() == nullptr) {
     return;
   }
-  TRACE_EVENT1("net", "PreconnectRunner::PreconnectSocket", "url", original_url.spec());
   net::NetworkAnonymizationKey key =
       net::NetworkAnonymizationKey::CreateSameSite(
           net::SchemefulSite(original_url));
