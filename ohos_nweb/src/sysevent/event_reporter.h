e0261892c8b07d7d51039fd5f72342c6d77b496e
庞宇成 <pangyucheng@huawei.com>
2024-09-11 21:23:12 +0800

    ArkWeb内核对dmp文件改造到指定目录
    Signed-off-by: 庞宇成 <pangyucheng@huawei.com>

--- ohos_nweb/src/sysevent/event_reporter.h
+++ ohos_nweb/src/sysevent/event_reporter.h
@@ -19,6 +19,13 @@
 #include <string>
 #include <vector>
 #include "oh_web_performance_timing.h"
+#include "base/files/file_util.h"
+#include "base/files/file_enumerator.h"
+#include "base/files/file_path.h"
+#include "base/logging.h"
+#include "base/command_line.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/browser/browser_thread.h"
 
 void ReportPageLoadStats(int instanceId,
                          int accessSumCount,
@@ -66,4 +73,10 @@ void ReportAudioFrameDropStats(int frameCount);
 void ReportVideoFrameDropStats(int64_t frameCount, int64_t frameDuration);
 
 void ReportSiteIsolationMode(const std::string site_isolation_status);
+
+#if defined(OHOS_CRASHPAD)
+void SetTargetCrashpadLogPath(std::string crashpadLogPath);
+void ReportTransferToBaseStorage(void);
+#endif
+
 #endif
