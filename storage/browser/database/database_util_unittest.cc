74d8a8e6af6c954dd5555a5c4f9425a1cee2233d
wangweiyuan2 <wangweiyuan2@huawei.com>
2024-09-21 17:57:24 +0800

    storage/browser/database/database_util_unittest.cc的case测试
    Signed-off-by: wangweiyuan2 <wangweiyuan2@huawei.com>

--- storage/browser/database/database_util_unittest.cc
+++ storage/browser/database/database_util_unittest.cc
@@ -63,4 +63,34 @@ TEST(DatabaseUtilTest, CrackVfsFilePathTest) {
 #endif
 }
 
+TEST(DatabaseUtilTest, CrackVfsFilePathTest001) {
+  std::string origin_identifier;
+  std::u16string database_name;
+  std::u16string sqlite_suffix;
+  std::u16string vfs_file_name = u"example.com/db_name!@#.db";
+  auto result = DatabaseUtil::CrackVfsFileName(vfs_file_name,
+                                                     &origin_identifier, &database_name,
+                                                     &sqlite_suffix);
+  EXPECT_EQ(false, result);
+}
+
+TEST(DatabaseUtilTest, CrackVfsFilePathTest002) {
+  std::string origin_identifier;
+  std::u16string database_name;
+  std::u16string sqlite_suffix;
+  std::u16string vfs_file_name = u"example.ÖÐÎÄ/db_name.db";
+  auto result = DatabaseUtil::CrackVfsFileName(vfs_file_name,
+                                                     &origin_identifier, &database_name,
+                                                     &sqlite_suffix);
+  EXPECT_EQ(false, result);
+}
+
+TEST(DatabaseUtilTest, CrackVfsFilePathTest003) {
+  std::u16string vfs_file_name = u"example.com/db_name.db";
+  auto result = DatabaseUtil::CrackVfsFileName(vfs_file_name,
+                                                     nullptr, nullptr,
+                                                     nullptr);
+  EXPECT_EQ(true, result);
+}
+
 }  // namespace storage
