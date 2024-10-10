e28c316e084da1c620be6ffbeceefbf33f577810
hayand <xuhanyang2@huawei.com>
2024-10-10 14:35:20 +0800

    update
    Signed-off-by: hayand <xuhanyang2@huawei.com>

--- components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -607,7 +607,6 @@ SubmitResult CompositorFrameSinkSupport::MaybeSubmitCompositorFrame(
   pending_received_frame_times_.emplace(frame.metadata.frame_token, now_time);
 
   // Override the has_damage flag (ignoring invalid data from clients).
-  TRACE_EVENT1("viz", "MaybeSubmitCompositorFrame", "has_damage", frame.metadata.begin_frame_ack.has_damage);
   frame.metadata.begin_frame_ack.has_damage = true;
   DCHECK(frame.metadata.begin_frame_ack.frame_id.IsSequenceValid());
 
