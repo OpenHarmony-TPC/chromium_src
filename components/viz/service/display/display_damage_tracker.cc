d245f60bd4190ed6224817f5a27956760f396785
hayand <xuhanyang2@huawei.com>
2024-10-11 09:36:08 +0800

    update
    Signed-off-by: hayand <xuhanyang2@huawei.com>

--- components/viz/service/display/display_damage_tracker.cc
+++ components/viz/service/display/display_damage_tracker.cc
@@ -158,8 +158,8 @@ void DisplayDamageTracker::OnSurfaceMarkedForDestruction(
 bool DisplayDamageTracker::OnSurfaceDamaged(const SurfaceId& surface_id,
                                             const BeginFrameAck& ack) {
   bool display_damaged = false;
-  TRACE_EVENT_INSTANT1("viz", "DisplayDamageTracker::OnSurfaceDamaged",
-                       TRACE_EVENT_SCOPE_THREAD, "ack.has_damage", ack.has_damage);
+  TRACE_EVENT1("viz", "DisplayDamageTracker::OnSurfaceDamaged",
+               TRACE_EVENT_SCOPE_THREAD, "ack.has_damage", ack.has_damage);
   if (ack.has_damage) {
     display_damaged =
         aggregator_->NotifySurfaceDamageAndCheckForDisplayDamage(surface_id);
@@ -169,9 +169,9 @@ bool DisplayDamageTracker::OnSurfaceDamaged(const SurfaceId& surface_id,
     if (display_damaged)
       surfaces_to_ack_on_next_draw_.push_back(surface_id);
   }
-  TRACE_EVENT_INSTANT2("viz", "DisplayDamageTracker::OnSurfaceDamaged",
-                       TRACE_EVENT_SCOPE_THREAD, "adisplay_damaged", display_damaged,
-                       "surface_id ?= root_surface_id_", (surface_id == root_surface_id_));
+  TRACE_EVENT2("viz", "DisplayDamageTracker::OnSurfaceDamaged",
+               "adisplay_damaged", display_damaged,
+               "surface_id ?= root_surface_id_", (surface_id == root_surface_id_));
   if (surface_id == root_surface_id_)
     UpdateRootFrameMissing();
 
