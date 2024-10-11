3bb31e8f31de6a70cabd946923c97e5d592a3931
雪地画猫 <yangjiawei24@huawei.com>
2024-10-11 10:10:53 +0000

    update content/browser/accessibility/browser_accessibility_manager.cc.
    Signed-off-by: 雪地画猫 <yangjiawei24@huawei.com>

--- content/browser/accessibility/browser_accessibility_manager.cc
+++ content/browser/accessibility/browser_accessibility_manager.cc
@@ -883,6 +883,12 @@ void BrowserAccessibilityManager::DoDefaultAction(
   ui::AXActionData action_data;
   action_data.action = ax::mojom::Action::kDoDefault;
   action_data.target_node_id = node.GetId();
+
+#if BUILDFLAG(IS_OHOS)
+  ui::AXActionData load_text_data = data;
+  load_text_data.action = ax::mojom::Action::kLoadInlineTextBoxes;
+  delegate_->AccessibilityPerformAction(load_text_data);
+#endif
   delegate_->AccessibilityPerformAction(action_data);
   BrowserAccessibilityStateImpl::GetInstance()->OnAccessibilityApiUsage();
 }
