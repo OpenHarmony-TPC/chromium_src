f3030ec6b9cb6ea94528212de741ede2a052ab21
vcnmj <wujiewen1@huawei.com>
2024-10-12 17:34:48 +0800

    Signed-off-by: vcnmj <wujiewen1@huawei.com> 安全函数整改

--- ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
@@ -289,7 +289,7 @@ char* CopyCefStringToChar(const CefString& str) {
   }
   int strLen = str.size() + 1;
   char* result = new char[strLen]{0};
-  if (strcpy(result, strLen, str.ToString().c_str()) != EOF) {
+  if (strcpy_s(result, strLen, str.ToString().c_str()) != EOF) {
     return nullptr;
   }
   return result;
