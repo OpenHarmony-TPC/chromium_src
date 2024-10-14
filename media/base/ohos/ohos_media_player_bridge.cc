7717d404d137daf34ca9f448afd0fb0fe93ed7f4
gzweioh <gaozhiwei6@huawei.com>
2024-10-14 20:01:57 +0800

    解决直播加载转圈问题
    Signed-off-by: gzweioh <gaozhiwei6@huawei.com>

--- media/base/ohos/ohos_media_player_bridge.cc
+++ media/base/ohos/ohos_media_player_bridge.cc
@@ -223,7 +223,12 @@ base::TimeDelta OHOSMediaPlayerBridge::GetMediaTime() {
   }
 
   int32_t time = -1;
-  (void)player_->GetCurrentTime(time);
+  int32_t ret = player_->GetCurrentTime(time);
+  if (ret == 0 && time == -1) {
+    //if is livestream, return system time
+    auto system_time = base::Time::Now().ToInternalValue() / base::Time::kMicrosecondsPerMillisecond;
+    return base::Milliseconds(system_time);
+  }
   return base::Milliseconds(time);
 }
 
