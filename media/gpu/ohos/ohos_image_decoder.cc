518965884ca0af474b3a2a92c434190ab0888b22
whuqincheng <liweiyang1@huawei.com>
2024-10-17 19:32:12 +0800

    add important log
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- media/gpu/ohos/ohos_image_decoder.cc
+++ media/gpu/ohos/ohos_image_decoder.cc
@@ -47,7 +47,7 @@ OhosImageDecodeStatus OhosImageDecoder::Decode(
   if (!OhosImageDecoderAdapter_->Decode(
           encoded_image.data(), encoded_image.size(), GetDecodeAllocatorType(),
           IsYuvFormat())) {
-    LOG(ERROR) << "[HeifSupport] Decode image failed."
+    LOG(ERROR) << "[HeifSupport] Decode image failed.";
     return OhosImageDecodeStatus::kExecuteDecodeFailed;
   }
   LOG(INFO) << "[HeifSupport] Decode native window buffer "
