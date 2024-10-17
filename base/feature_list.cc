12f3937e14d3738458c6d60c38082c8e2bff2593
tengfan66 <tengfan3@huawei.com>
2024-10-17 18:19:32 +0800

    crash
    Signed-off-by: tengfan66 <tengfan3@huawei.com>
    Change-Id: I8cf0e83d5d3af53800814ad490a4e6e39c092aa8

--- base/feature_list.cc
+++ base/feature_list.cc
@@ -334,7 +334,7 @@ void FeatureList::InitializeFromSharedMemory(
 }
 
 bool FeatureList::IsFeatureOverridden(const std::string& feature_name) const {
-#ifdef OHOS_SCROLLAR
+#ifdef OHOS_SCROLLBAR
   AutoLock lock(overrides_lock_);
 #endif
   return overrides_.count(feature_name);
@@ -342,7 +342,7 @@ bool FeatureList::IsFeatureOverridden(const std::string& feature_name) const {
 
 bool FeatureList::IsFeatureOverriddenFromCommandLine(
     const std::string& feature_name) const {
-#ifdef OHOS_SCROLLAR
+#ifdef OHOS_SCROLLBAR
   AutoLock lock(overrides_lock_);
 #endif
   auto it = overrides_.find(feature_name);
@@ -352,7 +352,7 @@ bool FeatureList::IsFeatureOverriddenFromCommandLine(
 bool FeatureList::IsFeatureOverriddenFromCommandLine(
     const std::string& feature_name,
     OverrideState state) const {
-#ifdef OHOS_SCROLLAR
+#ifdef OHOS_SCROLLBAR
   AutoLock lock(overrides_lock_);
 #endif
   auto it = overrides_.find(feature_name);
@@ -785,7 +785,7 @@ FeatureList::OverrideState FeatureList::GetOverrideStateByFeatureName(
   DCHECK(initialized_);
   DCHECK(IsValidFeatureOrFieldTrialName(feature_name)) << feature_name;
 
-#ifdef OHOS_SCROLLAR
+#ifdef OHOS_SCROLLBAR
   AutoLock lock(overrides_lock_);
 #endif
   auto it = overrides_.find(feature_name);
