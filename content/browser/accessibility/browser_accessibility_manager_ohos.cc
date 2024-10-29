f1759828ac8be7e9eb35ed6b6822e9ddb8fbe870
hoo334 <liminghu1@huawei.com>
2024-10-29 11:31:37 +0000

    【无障碍】修复浏览器关注推荐页面切换绿框不跟随问题
    Signed-off-by: hoo334 <liminghu1@huawei.com>

--- content/browser/accessibility/browser_accessibility_manager_ohos.cc
+++ content/browser/accessibility/browser_accessibility_manager_ohos.cc
@@ -277,8 +277,10 @@ void BrowserAccessibilityManagerOHOS::FireGeneratedEvent(
       }
       break;
     case ui::AXEventGenerator::Event::SELECTED_CHANGED:
-      SendAccessibilityEvent(accessibilityId, 
-          OHOS::NWeb::AccessibilityEventType::SELECTED);
+      if (nodeOHOS->IsSelected()) {
+        MoveAccessibilityFocusToId(accessibilityId);
+        SendAccessibilityEvent(accessibilityId, OHOS::NWeb::AccessibilityEventType::SELECTED);
+      }
       break;
     case ui::AXEventGenerator::Event::DOCUMENT_SELECTION_CHANGED: {
       if (ax_tree() == nullptr) {
