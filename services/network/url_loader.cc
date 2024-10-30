a051293ae6d967e672b0d1b46f6f2d4fd1be0f44
wanglei <wanglei647@huawei.com>
2024-10-30 03:43:14 +0000

    解决代码门禁告警
    Signed-off-by: wanglei <wanglei647@huawei.com>

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -2189,7 +2189,7 @@ void URLLoader::NotifyCompleted(int error_code) {
             ? raw_response_headers_.get()
             : url_request_->response_headers();
         TRACE_EVENT2("net", "URLLoader::NotifyCompleted",
-                     "response_code", response_headers()->response_code(),
+                     "response_code", response_headers->response_code(),
                      "id", request_id_);
       }
       PrintNetworkTimingInfo();
