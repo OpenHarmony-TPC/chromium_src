ed40a1674304376be2d71fda91700999c28ea427
马宗富 <mazongfu1@h-partners.com>
2024-11-04 17:30:11 +0800

    修复CVE-340822365-40058879
    Signed-off-by: 马宗富 <mazongfu1@h-partners.com>

--- gpu/command_buffer/client/raster_implementation.cc
+++ gpu/command_buffer/client/raster_implementation.cc
@@ -91,9 +91,11 @@ namespace raster {
 
 namespace {
 
+// TODO(crbug.com/40058879): Disable this work-around, once call-sites are
+// handling failures correctly.
 BASE_FEATURE(kDisableErrorHandlingForReadback,
              "kDisableErrorHandlingForReadback",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 const uint32_t kMaxTransferCacheEntrySizeForTransferBuffer = 1024;
 const size_t kMaxImmediateDeletedPaintCachePaths = 1024;
