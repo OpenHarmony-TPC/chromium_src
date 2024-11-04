1707811bd20aeabacf57e50bb3feefe9bc0c7658
wuqiwen <wuqiwen7@huawei.com>
2024-11-04 13:15:16 +0000

    update media/gpu/ohos/video_frame_factory_impl.cc.
    Signed-off-by: wuqiwen <wuqiwen7@huawei.com>

--- media/gpu/ohos/video_frame_factory_impl.cc
+++ media/gpu/ohos/video_frame_factory_impl.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Based on video_frame_factory_impl.cc originally written by
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "media/gpu/ohos/video_frame_factory_impl.h"
 
 #include <memory>
