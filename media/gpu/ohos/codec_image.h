4f139e05f3d1b3d347d2cfcf48a2c11d40ad7891
wuqiwen <wuqiwen7@huawei.com>
2024-11-04 02:10:51 +0000

    update media/gpu/ohos/codec_image.h.
    Signed-off-by: wuqiwen <wuqiwen7@huawei.com>

--- media/gpu/ohos/codec_image.h
+++ media/gpu/ohos/codec_image.h
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Based on codec_image.h originally written by
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef MEDIA_GPU_OHOS_CODEC_IMAGE_H_
 #define MEDIA_GPU_OHOS_CODEC_IMAGE_H_
 
