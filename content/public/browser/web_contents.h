9c5e71063bf3e3e668586d8fcc5703f256c8a988
xiongjun_gitee <xiongjun28@huawei.com>
2024-11-14 16:15:44 +0800

    解决单手柄菜单有时没有[全选]项
    Signed-off-by: xiongjun_gitee <xiongjun28@huawei.com>

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -1548,6 +1548,9 @@ class WebContents : public PageNavigator,
 #ifdef OHOS_DRAG_DROP
   virtual void ClearContextMenu() = 0;
 #endif //OHOS_DRAG_DROP
+#ifdef OHOS_CLIPBOARD
+  virtual int32_t GetEditFlags() { return 0; }
+#endif
  private:
   // This interface should only be implemented inside content.
   friend class WebContentsImpl;
