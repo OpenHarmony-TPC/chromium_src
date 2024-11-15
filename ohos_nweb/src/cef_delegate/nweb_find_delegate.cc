12b9ef4ff3f6c64a2b7499eb4a8fcb7421e208ca
vcnmj <wujiewen1@huawei.com>
2024-11-15 17:38:58 +0800

    i
    Signed-off-by: vcnmj <wujiewen1@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_find_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_find_delegate.cc
@@ -11,6 +11,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Based on find_helper.cc originally written by
+ * Copyright 2012 The Chromium Authors
+ * Use of this source code is governed by a BSD->
+ * found in the LICENSE file.
  */
 
 #include "nweb_find_delegate.h"
