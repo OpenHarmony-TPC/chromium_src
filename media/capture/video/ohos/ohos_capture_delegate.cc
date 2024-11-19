4a61d3742d884ba0edb63c39f334fcaf51a23fd1
AIUZz <yanxinghui@huawei.com>
2024-11-19 19:08:29 +0800

    delete blank
    Signed-off-by: AIUZz <yanxinghui@huawei.com>

--- media/capture/video/ohos/ohos_capture_delegate.cc
+++ media/capture/video/ohos/ohos_capture_delegate.cc
@@ -148,7 +148,7 @@ void OHOSCaptureDelegate::OnBufferAvailable(
     return;
   }
   int32_t rotation = roration_info->GetRotation();
-  if (client_ != nullptr) {    
+  if (client_ != nullptr) {
     client_->OnIncomingCapturedData(
         buffer->GetBufferAddr(), buffer->GetSize(), capture_format_,
         gfx::ColorSpace(), rotation, roration_info->GetIsFlipY() /* flip_y */,
