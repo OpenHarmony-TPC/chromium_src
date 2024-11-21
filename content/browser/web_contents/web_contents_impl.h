b1fcd3d86d44013c5f14f0bffc7a4076eda258f0
w00848065 <wangyanhan3@huawei.com>
2024-11-21 02:31:04 +0000

    断网时媒体播放状态新增 Signed-off-by: w00848065 <wangyanhan3@huawei.com>
    Signed-off-by: w00848065 <wangyanhan3@huawei.com>

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1259,6 +1259,11 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
       const WebContentsObserver::MediaPlayerInfo& media_info,
       const MediaPlayerId& id,
       WebContentsObserver::MediaStoppedReason reason);
+#if BUILDFLAG(IS_OHOS)
+  void MediaPlayerGone(
+      const WebContentsObserver::MediaPlayerInfo& media_info,
+      const MediaPlayerId& id);
+#endif
   // This will be called before playback is started, check
   // GetCurrentlyPlayingVideoCount if you need this when playback starts.
   void MediaResized(const gfx::Size& size, const MediaPlayerId& id);
