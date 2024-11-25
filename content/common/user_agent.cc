4cff40d8ccb956bf037e216d99c194db78cc2929
zhaodan_123 <zhaodan97@h-partners.com>
2024-11-25 20:44:08 +0800

    fix:codex DIR:SR..
    Signed-off-by: zhaodan_123 <zhaodan97@h-partners.com>

--- content/common/user_agent.cc
+++ content/common/user_agent.cc
@@ -230,7 +230,6 @@ std::string GetDistVersion() {
   int versionPartTwo;
   int versionPartthree;
   std::string os_version = base::ohos::OsVersion();
-<<<<<<< HEAD
   if (os_version.find('.') != std::string::npos) {
     int ret = sscanf_s(os_version.c_str(), "%d.%d.%d",
         &versionPartOne, &versionPartTwo, &versionPartthree);
@@ -248,16 +247,6 @@ std::string GetDistVersion() {
                         versionPartOne, versionPartTwo);
     return dist_version;
   }
-=======
-  int ret = sscanf_s(os_version.c_str(), "%d.%d.%d",
-      &versionPartOne, &versionPartTwo, &versionPartthree);
-  if (ret <= 0){
-    return dist_version;
-  }
-  base::StringAppendF(&dist_version, "%d.%d",
-                      versionPartOne, versionPartTwo);
-  return dist_version;
->>>>>>> d8f2a05f436a152125f5113430295362a354bee7
 }
 #endif
 
