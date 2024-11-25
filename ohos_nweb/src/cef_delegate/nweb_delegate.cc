34035383fa915545358bb4faf2824ead8e8f0c82
yushihao4 <yushihao4@huawei.com>
2024-11-25 15:29:42 +0800

    [webview]web焦点优化
    Signed-off-by: yushihao4 <yushihao4@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -1464,6 +1464,7 @@ void NWebDelegate::OnPause() {
   }
 
   // Remove focus from the browser.
+  LOG(INFO) << "NWebDelegate::OnPause set web blur, nweb_id = " << nweb_id_;
   GetBrowser()->GetHost()->SetFocus(false);
 
 #if defined(OHOS_INPUT_EVENTS)
@@ -1584,13 +1585,18 @@ void NWebDelegate::OnContinue() {
   }
 
   // Give focus to the browser.
+#if defined(OHOS_INPUT_EVENTS)
   if (handler_delegate_ && handler_delegate_->GetContinueNeedFocus()) {
     if (is_onPause_) {
       GetBrowser()->GetHost()->SetFocus(true);
       handler_delegate_->SetContinueNeedFocus(false);
     }
+  } else if (handler_delegate_ && handler_delegate_->IsCurrentFocus()) {
+    LOG(INFO) << "NWebDelegate::OnContinue set web core focus, nweb_id = " << nweb_id_;
+    GetBrowser()->GetHost()->SetFocus(true);
   }
   is_onPause_ = false;
+#endif
 }
 
 void NWebDelegate::WebComponentsBlur() {
@@ -2328,6 +2334,7 @@ bool NWebDelegate::OnFocus(const FocusReason& focusReason) const {
 
 #ifdef OHOS_FOCUS
   if (handler_delegate_ && !handler_delegate_->GetFocusState()) {
+    LOG(INFO) << "NWebDelegate::OnFocus set web focus, nweb_id = " << nweb_id_;
     GetBrowser()->GetHost()->SetFocus(true);
   }
 #endif  // #ifdef OHOS_FOCUS
@@ -2344,6 +2351,7 @@ void NWebDelegate::OnBlur() const {
 
 #ifdef OHOS_FOCUS
   if (handler_delegate_ && handler_delegate_->GetFocusState()) {
+    LOG(INFO) << "NWebDelegate::OnBlur set web blur, nweb_id = " << nweb_id_;
     handler_delegate_->SetFocusState(false);
     GetBrowser()->GetHost()->SetFocus(false);
 #if defined(OHOS_INPUT_EVENTS)
