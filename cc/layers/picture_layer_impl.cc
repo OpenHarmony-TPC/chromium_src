c93f10c763384d079b73e4fcdb89dee1c15812a2
juwenjing <juwenjing@huawei.com>
2024-11-27 11:34:42 +0800

    code
    Signed-off-by: juwenjing <juwenjing@huawei.com>

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -1346,8 +1346,17 @@ void PictureLayerImpl::UpdateTilingsForRasterScaleAndTranslation(
   }
   high_res->set_resolution(HIGH_RESOLUTION);
 
+#if BUILDFLAG(IS_OHOS)
   if (layer_tree_impl()->settings().commit_to_active_tree &&
       IsDirectlyCompositedImage()) {
+#else
+  if (layer_tree_impl()->IsPendingTree() ||
+      (layer_tree_impl()->settings().commit_to_active_tree &&
+       IsDirectlyCompositedImage())) {
+#endif
+    // On the pending tree, drop any tilings that are non-ideal since we don't
+    // need them to activate anyway.
+
     // For DirectlyCompositedImages, if we recomputed a new raster scale, we
     // should drop the non-ideal ones if we're committing to the active tree.
     // Otherwise a non-ideal scale that is _larger_ than the HIGH_RESOLUTION
