7531786427c2378f9fe5d2c688f1498bc288dccb
YJone <zhouyongqiang8@huawei.com>
2024-11-29 03:24:07 +0000

    update components/viz/common/quads/compositor_frame_metadata_unittest.cc.
    Signed-off-by: YJone <zhouyongqiang8@huawei.com>

--- components/viz/common/quads/compositor_frame_metadata_unittest.cc
+++ components/viz/common/quads/compositor_frame_metadata_unittest.cc
@@ -63,6 +63,7 @@ TEST(CompositorFrameMetadata, Clone) {
   metadata.content_color_usage = gfx::ContentColorUsage::kHDR;
   metadata.may_contain_video = true;
   metadata.is_resourceless_software_draw_with_scroll_or_animation = true;
+  metadata.is_actively_scrolling = true;
   metadata.root_background_color = SkColors::kBlue;
   metadata.latency_info.emplace_back(ui::SourceEventType::KEY_PRESS);
   metadata.referenced_surfaces.emplace_back(
