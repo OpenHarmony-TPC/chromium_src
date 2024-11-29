3d88a5910f0108155fc6f1c6904cb49544cff8f8
YJone <zhouyongqiang8@huawei.com>
2024-11-29 11:37:45 +0000

    update components/viz/service/display/display_scheduler.cc.
    Signed-off-by: YJone <zhouyongqiang8@huawei.com>

--- components/viz/service/display/display_scheduler.cc
+++ components/viz/service/display/display_scheduler.cc
@@ -13,9 +13,6 @@
 #include "base/task/delay_policy.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
-#if BUILDFLAG(IS_OHOS)
-#include "base/ohos/sys_info_utils.h"
-#endif
 #include "components/viz/common/features.h"
 #include "components/viz/service/performance_hint/hint_session.h"
 
