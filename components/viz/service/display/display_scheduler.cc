1c663846c0defe5a0b53379e2895b76c0f566217
YJone <zhouyongqiang8@huawei.com>
2024-11-29 12:02:51 +0000

    update components/viz/service/display/display_scheduler.cc. 产品隔离
    Signed-off-by: YJone <zhouyongqiang8@huawei.com>

--- components/viz/service/display/display_scheduler.cc
+++ components/viz/service/display/display_scheduler.cc
@@ -13,6 +13,9 @@
 #include "base/task/delay_policy.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
+#if BUILDFLAG(IS_OHOS)
+#include "base/ohos/sys_info_utils.h"
+#endif
 #include "components/viz/common/features.h"
 #include "components/viz/service/performance_hint/hint_session.h"
 
@@ -30,7 +33,7 @@ base::TimeDelta ComputeAdpfTarget(const BeginFrameArgs& args) {
 }
 
 bool DrawImmediatelyWhenInteractive() {
-  return features::ShouldDrawImmediatelyWhenInteractive();
+  return base::ohos::IsPcDevice() && features::ShouldDrawImmediatelyWhenInteractive();
 }
 
 }  // namespace
