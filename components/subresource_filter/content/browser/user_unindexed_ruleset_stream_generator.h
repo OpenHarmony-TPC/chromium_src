c590adc9d49c850ae4e80d6b6e53590701c5f00c
liyongzhuang <liyongzhuang1@huawei.com>
2024-12-06 16:29:20 +0800

    版权信息整改
    Signed-off-by: liyongzhuang <liyongzhuang1@huawei.com>

--- components/subresource_filter/content/browser/user_unindexed_ruleset_stream_generator.h
+++ components/subresource_filter/content/browser/user_unindexed_ruleset_stream_generator.h
@@ -1,17 +1,11 @@
-/*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Based on unindexed_ruleset_stream_generator.h originally written by
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_USER_UNINDEXED_RULESET_STREAM_GENERATOR_H_
 #define COMPONENTS_SUBRESOURCE_FILTER_CONTENT_BROWSER_USER_UNINDEXED_RULESET_STREAM_GENERATOR_H_
