f5a096be356eb1546d3c32e6190311d3488f6363
j00466033 <jianglinyang@huawei.com>
2024-12-16 15:38:54 +0800

    javascriptstart order
    Signed-off-by: j00466033 <jianglinyang@huawei.com>
    Change-Id: I77e6af1263e4c4de86c729bc13acee183826eec7
    Signed-off-by: j00466033 <jianglinyang@huawei.com>

--- ohos_nweb/src/nweb_input_handler_unittest.cc
+++ ohos_nweb/src/nweb_input_handler_unittest.cc
@@ -767,6 +767,7 @@ class MockNWebDelegate : public NWebDelegateInterface {
 
   MOCK_METHOD(void, SendAccessibilityHoverEvent, (int x, int y), (override));
   MOCK_METHOD(void, RefreshAccessibilityManagerClickEvent, (), (override));
+
 #ifdef OHOS_EX_REFRESH_IFRAME
   MOCK_METHOD(void, WebExtensionContextMenuIsIframe, (), (override));
   MOCK_METHOD(bool, WebExtensionContextMenuReloadFocusedFrame, (), (override));
@@ -776,6 +777,16 @@ class MockNWebDelegate : public NWebDelegateInterface {
   MOCK_METHOD(bool, NeedToFireBeforeUnloadOrUnloadEvents, (), (override));
   MOCK_METHOD(void, DispatchBeforeUnload, (), (override));
 #endif
+
+  MOCK_METHOD(void,
+              JavaScriptOnDocumentStartByOrder,
+              (const ScriptItems& ScriptItems, const ScriptItemsByOrder& ScriptItemsByOrder),
+              (override));
+
+  MOCK_METHOD(void,
+              JavaScriptOnDocumentEndByOrder,
+              (const ScriptItems& ScriptItems, const ScriptItemsByOrder& ScriptItemsByOrder),
+              (override));
 };
 
 class MockNWebTouchPointInfo : public NWebTouchPointInfo {
