023e75b21da9325c5d886fef56998569aba73f79
AIUZz <yanxinghui@huawei.com>
2024-12-18 18:52:25 +0800

    modify
    Signed-off-by: AIUZz <yanxinghui@huawei.com>

--- media/gpu/ohos/codec_wrapper.cc
+++ media/gpu/ohos/codec_wrapper.cc
@@ -203,7 +203,7 @@ CodecWrapperImpl::QueueStatus CodecWrapperImpl::QueueInputBuffer(
       elided_eos_pending_ = true;
     } else {
       auto res = codec_->QueueInputBufferEOS();
-      TRACE_EVENT1("media", "CodecWrapperImpl::QueueInputBufferEOS End", "result", status);
+      TRACE_EVENT1("media", "QueueInputBufferEOS End", "result", status);
       if (res == DecoderAdapterCode::DECODER_RETRY) {
         return QueueStatus::kTryAgainLater;
       }
