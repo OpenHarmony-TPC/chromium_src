cb435abd50c23faa79331efe7a7ea0fc39dcabe8
yueyang <lingyue2@huawei.com>
2024-12-25 22:22:52 +0800

    新增自动恢复流程根据错误码判断是否自动恢复
    Signed-off-by: yueyang <lingyue2@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_download_handler_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_download_handler_delegate.cc
@@ -134,6 +134,8 @@ std::string NWebDownloadHandlerDelegate::GenerateSuggestedFilename(
       l10n_util::GetStringUTF8(IDS_DEFAULT_DOWNLOAD_FILENAME));
   std::string default_charset =
       preference_delegate_ ? preference_delegate_->DefaultTextEncodingFormat() : "utf-8";
+  LOG(INFO) << "GenerateSuggestedFilename mime_type: " << sniffed_mime_type
+            << ", default_charset: " << default_charset;
   GURL gurl(download_item->GetURL().ToString());
   base::FilePath generated_filename = net::GenerateFileName(
       gurl, download_item->GetContentDisposition().ToString(), default_charset,
