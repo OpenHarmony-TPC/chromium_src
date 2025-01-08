712781566c974fd535e2fbe8f0f736c32a3b1918
lileiAJ <lilei433@h-partners.com>
2025-01-08 14:43:21 +0800

    update: 更新文件 platform_shared_memory_region_ohos.cc
    Signed-off-by: lileiAJ <lilei433@h-partners.com>

--- base/memory/platform_shared_memory_region_ohos.cc
+++ base/memory/platform_shared_memory_region_ohos.cc
@@ -4,7 +4,7 @@
 // found in the LICENSE file.
 
 // Based on platform_shared_memory_region.cc originally written by
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
