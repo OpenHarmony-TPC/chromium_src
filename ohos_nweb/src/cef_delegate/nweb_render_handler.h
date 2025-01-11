210c42406ce47507b9ac668821c89e5665690f85
coolfeng <zhangshengfeng6@huawei.com>
2025-01-11 19:44:36 +0800

    web新增OnScrollStart接口传入初始偏移量
    Signed-off-by: coolfeng <zhangshengfeng6@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_render_handler.h
+++ ohos_nweb/src/cef_delegate/nweb_render_handler.h
@@ -169,6 +169,9 @@ class NWebRenderHandler : public CefRenderHandler {
   void OnOverScrollFlingEnd(CefRefPtr<CefBrowser> browser) override;
   void OnScrollState(CefRefPtr<CefBrowser> browser,
                      bool scroll_state) override;
+  void OnScrollStart(CefRefPtr<CefBrowser> browser,
+                         const float x,
+                         const float y) override;
   void OnNativeEmbedGestureEvent(CefRefPtr<CefBrowser> browser,
                     const CefEmbedTouchEvent& event,
                     CefRefPtr<CefGestureEventCallback> callback) override;
