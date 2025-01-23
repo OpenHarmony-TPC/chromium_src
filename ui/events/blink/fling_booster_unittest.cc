c3fd329c9ba156efb40466a1c5cb5d6512e6d15c
wangziyao <wangziyao13@huawei.com>
2025-01-23 22:05:01 +0800

    update
    Signed-off-by: wangziyao <wangziyao13@huawei.com>

--- ui/events/blink/fling_booster_unittest.cc
+++ ui/events/blink/fling_booster_unittest.cc
@@ -65,6 +65,7 @@ class MockSystemPropertiesAdapter : public OHOS::NWeb::SystemPropertiesAdapter {
   MOCK_METHOD(std::string, GetUserAgentOSVersion, (), (override));
   MOCK_METHOD(std::string, GetDeviceInfoApiVersion, (), (override));
   MOCK_METHOD(std::string, GetPRPPreloadMode, (), (override));
+  MOCK_METHOD(std::string, GetScrollVelocityScale, (), (override));
 };
 }  // namespace OHOS::NWeb
 #endif // OHOS_UNITTESTS
