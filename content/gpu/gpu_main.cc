43a5d37aabee2b88a5eb4d4707353445cbad7e8e
胡文韬 <huwentao21@huawei.com>
2025-02-04 16:05:41 +0800

    解决gpu_work_server拉起延迟导致报点失效问题
    Signed-off-by: 胡文韬 <huwentao21@huawei.com>

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -135,6 +135,7 @@ bool LoadStringFromFile(const std::string& file_path, std::string& content);
 const int MAX_FILE_LENGTH = 32* 1024 * 1024;
 static int retry_times = 0;
 const int retry_delay_ms = 100;
+const int retry_max_times = 4;
 #endif
 
 class ContentSandboxHelper : public gpu::GpuSandboxHelper {
@@ -550,7 +551,7 @@ void TryForReportThread() {
     }
     return;
   }
-  if (retry_times < 4) {
+  if (retry_times < retry_max_times) {
     retry_times = retry_times + 1;
     base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(FROM_HERE, base::BindOnce(&TryForReportThread),
       base::Milliseconds(retry_delay_ms));
