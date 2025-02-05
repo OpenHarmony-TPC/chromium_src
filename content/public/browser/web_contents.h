239b5d2a46e8c0d35bf5bdcd5be5bd3d6e830df4
liyou88 <liyou44@huawei.com>
2025-02-05 10:12:00 +0800

    Clear selection when backward
    Signed-off-by: liyou88 <liyou44@huawei.com>
    Change-Id: I011e34edf61649b41bfbe8754f0a4a3047e7f0a5

--- content/public/browser/web_contents.h
+++ content/public/browser/web_contents.h
@@ -646,6 +646,7 @@ class WebContents : public PageNavigator,
 #ifdef OHOS_CLIPBOARD
   virtual void SetTouchInsertHandleMenuShow(bool show) = 0;
   virtual bool GetTouchInsertHandleMenuShow() = 0;
+  virtual void CollapseAllFramesSelection() = 0;
 #endif
 
 #ifdef OHOS_ARKWEB_ADBLOCK
