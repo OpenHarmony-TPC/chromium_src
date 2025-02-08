d8244079f16fafb433490ce3e5b2d1db4ed288f4
wen-xue-yang <yangwenxue1@huawei.com>
2025-02-08 20:30:22 +0800

    preload开关设置
    Signed-off-by: wen-xue-yang <yangwenxue1@huawei.com>

--- services/network/prp_preload/src/page_res_parallel_preload_mgr_impl.cc
+++ services/network/prp_preload/src/page_res_parallel_preload_mgr_impl.cc
@@ -31,21 +31,21 @@ if defined(WEBVIEW_ARM64)
 	OHOS::NWeb::OhosAdapterHelper::GetInstance().GetSystemPropertiesInstance().GetPRPPreloadMode();
   bool isMobile = base::ohos::IsMobileDevice();
   if (!isMobile) {
-	LOG(DEBUG) << "PRPPreload.GetPRParallelPreloadModeInner is not Mobile, NONE";
+	LOG(INFO) << "PRPPreload.GetPRParallelPreloadModeInner is not Mobile, NONE";
 	return PRPPreloadMode::NONE;
   }
 
   if (prp_preload_mode == PRP_PRELOAD_MODE_PRECONNECT) {
-	LOG(DEBUG) << "PRPPreload.GetPRParallelPreloadModeInner PRECONNECT";
+	LOG(INFO) << "PRPPreload.GetPRParallelPreloadModeInner PRECONNECT";
 	return PRPPreloadMode::PRECONNECT;
   }
 
   if (prp_preload_mode == PRP_PRELOAD_MODE_PRELOAD) {
-	LOG(DEBUG) << "PRPPreload.GetPRParallelPreloadModeInner PRELOAD";
+	LOG(INFO) << "PRPPreload.GetPRParallelPreloadModeInner PRELOAD";
 	return PRPPreloadMode::PRELOAD;
   }
 
-  LOG(DEBUG) << "PRPPreload.GetPRParallelPreloadModeInner NONE";
+  LOG(INFO) << "PRPPreload.GetPRParallelPreloadModeInner NONE";
   return PRPPreloadMode::NONE;
 #else
   LOG(INFO) << "PRPPreload.GetPRParallelPreloadModeInner only support arm64, NONE";
