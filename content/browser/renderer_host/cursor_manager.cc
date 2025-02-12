e397ad45d70b22e0cabffcb950fc0459d8397387
zhu-sheng-le <zhushengle@huawei.com>
2025-02-12 16:14:08 +0800

    支持鼠标中键
    Signed-off-by: zhu-sheng-le <zhushengle@huawei.com>

--- content/browser/renderer_host/cursor_manager.cc
+++ content/browser/renderer_host/cursor_manager.cc
@@ -121,8 +121,11 @@ void CursorManager::UpdateCursor() {
   }
 
   last_set_cursor_type_for_testing_ = cursor.type();
-
+#if defined(OHOS_INPUT_EVENTS)
+  root_view_->UpdateCursor(cursor);
+#else
   root_view_->DisplayCursor(cursor);
+#endif
 }
 
 }  // namespace content
