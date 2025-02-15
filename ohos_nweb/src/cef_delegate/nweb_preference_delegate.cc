1889cf7486af357c825a306fd61138ec5ddd8cca
ICEMAGE117 <lifeng262@huawei.com>
2025-02-15 11:29:30 +0800

    font weight can follow system
    Signed-off-by: ICEMAGE117 <lifeng262@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_preference_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_preference_delegate.cc
@@ -180,6 +180,7 @@ void NWebPreferenceDelegate::ComputeBrowserSettings(
   browser_settings.text_autosizing_enabled =
       IsTextAutosizingEnabled() ? STATE_ENABLED : STATE_DISABLED;
   browser_settings.force_zero_layout_height = IsFitContent();
+  browser_settings.font_weight_scale = GetFontWeightScale();
 #endif  // BUILDFLAG(IS_OHOS)
 #if defined(OHOS_CLIPBOARD)
   browser_settings.copy_option = static_cast<int>(GetCopyOptionMode());
@@ -796,6 +797,16 @@ void NWebPreferenceDelegate::SetFitContent(bool value) {
 
 bool NWebPreferenceDelegate::IsFitContent() const {
   return fit_content_;
+}
+
+void NWebPreferenceDelegate::SetFontWeightScale(float scale) {
+  LOG(INFO) << "set fontWeightScale = " << scale;
+  font_weight_scale_ = scale;
+  WebPreferencesChanged();
+}
+
+float NWebPreferenceDelegate::GetFontWeightScale() const {
+  return font_weight_scale_;
 }
 #endif
 
