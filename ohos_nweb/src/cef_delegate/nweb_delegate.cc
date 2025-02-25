8bf1189ba534cfca5acd86073bae1e07fd242c53
zzazxc <liyou44@huawei.com>
2025-02-25 20:54:05 +0800

    Revert the modification of no notification of screeninfoschanged in nweb_delegate
    Signed-off-by: zzazxc <liyou44@huawei.com>
    Change-Id: Id884bb20784761fb6df87ac1364e888405ea8de4

--- ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -596,7 +596,7 @@ InitRichtextIdentifier();
   std::shared_ptr<DisplayAdapter> display =
       display_manager_adapter_->GetDefaultDisplay();
   if (display != nullptr) {
-    NotifyScreenInfoChanged(display->GetRotation(), display->GetDisplayOrientation(), true);
+    NotifyScreenInfoChanged(display->GetRotation(), display->GetDisplayOrientation());
     if (!richtext_data_str_.empty()) {
       // Created a richtext component
       SetVirtualPixelRatio(richtextDisplayRatio);
@@ -1005,8 +1005,7 @@ void NWebDelegate::SendMouseEvent(int x,
 }
 
 void NWebDelegate::NotifyScreenInfoChanged(RotationType rotation,
-                                           DisplayOrientation orientation,
-                                           bool isWebinitialization) {
+                                           DisplayOrientation orientation) {
   if (render_handler_ != nullptr) {
     if (display_manager_adapter_ == nullptr) {
       LOG(ERROR) << "Get display_manager_adapter_ failed";
@@ -1033,13 +1032,6 @@ void NWebDelegate::NotifyScreenInfoChanged(RotationType rotation,
     int height = std::ceil(display->GetHeight() / display_ratio);
 #ifdef OHOS_SCREEN_ROTATION
     bool default_portrait = display_manager_adapter_->IsDefaultPortrait();
-    if (hidden_ && !isWebinitialization) {
-      render_handler_->SetLastScreenInfo({rotation, orientation, width, height,
-                                          display_ratio, default_portrait});
-      render_handler_->SetScreenInfo({rotation, orientation, width, height,
-                                      display_ratio, default_portrait});
-      return;
-    }
     render_handler_->SetScreenInfo({rotation, orientation, width, height,
                                     display_ratio, default_portrait});
 #endif  // #ifdef OHOS_SCREEN_ROTATION
@@ -1643,14 +1635,6 @@ void NWebDelegate::OnContinue() {
     LOG(INFO) << "NWebDelegate::OnContinue set unhidden, nweb_id = " << nweb_id_;
     TRACE_EVENT1("base", "NWebDelegate::OnContinue set unhidden", "nweb id = ", nweb_id_);
     GetBrowser()->GetHost()->WasHidden(false);
-    if (render_handler_->IsNeedCefNotifyScreenInfoChanged()) {
-      render_handler_->SetScreenInfo(render_handler_->GetLastScreenInfo());
-      auto browser = GetBrowser();
-      if (browser != nullptr && browser->GetHost() != nullptr) {
-        LOG(INFO) << "NWebDelegate::OnContinue Notify Screen Info Changed";
-        browser->GetHost()->NotifyScreenInfoChanged();
-      }
-    }
 
 #ifdef OHOS_RENDER_PROCESS_MODE
     if (GetBrowser() && GetBrowser()->GetHost() &&
--- ohos_nweb/src/cef_delegate/nweb_render_handler.cc
+++ ohos_nweb/src/cef_delegate/nweb_render_handler.cc
@@ -500,23 +500,6 @@ void NWebRenderHandler::SetScreenInfo(const NWebScreenInfo& screen_info) {
     delegete->SetVirtualPixelRatio(screen_info_.display_ratio);
   }
 }
-
-NWebScreenInfo& NWebRenderHandler::GetLastScreenInfo() {
-  return last_screen_info_;
-}
-
-void NWebRenderHandler::SetLastScreenInfo(const NWebScreenInfo& screen_info) {
-  last_screen_info_ = screen_info;
-}
-
-bool NWebRenderHandler::IsNeedCefNotifyScreenInfoChanged() {
-  if (last_screen_info_.display_ratio <= 0 || screen_info_.display_ratio <= 0) {
-    return false;
-  }
-  return last_screen_info_.display_ratio != screen_info_.display_ratio ||
-         last_screen_info_.width != screen_info_.width ||
-         last_screen_info_.height != screen_info_.height;
-}
 // #endif  // #ifdef OHOS_SCREEN_ROTATION
 
 bool NWebRenderHandler::GetScreenInfo(CefRefPtr<CefBrowser> browser,
