9e16cd241b9c2ba4d28b3e001ea356a8885d9b53
poorhack <wangyufeng21@huawei.com>
2025-03-10 15:16:57 +0800

    update: 更新文件 service_worker_context_core.cc
    Signed-off-by: poorhack <wangyufeng21@huawei.com>

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -1180,11 +1180,11 @@ void ServiceWorkerContextCore::OnReportConsoleMessage(
   // This is called on the IO thread, though, so we can't easily get a
   // BrowserContext and call ContentBrowserClient::IsBuiltinComponent().
   const bool is_builtin_component = HasWebUIScheme(source_url);
-
+#if !BUILDFLAG(IS_OHOS)
   LogConsoleMessage(message_level, message, line_number, is_builtin_component,
                     wrapper_->is_incognito(),
                     base::UTF8ToUTF16(source_url.spec()));
-
+#endif
   observer_list_->Notify(
       FROM_HERE, &ServiceWorkerContextCoreObserver::OnReportConsoleMessage,
       version->version_id(), version->scope(), version->key(),
