9a38d7b0d5d41ece818f482e53b420a4a9ffabd4
zhengjinbo <zhengjinbo1@huawei.com>
2025-03-16 11:47:45 +0800

    feat: add screenLeft and screenTop support
    Signed-off-by: zhengjinbo <zhengjinbo1@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_render_handler.h
+++ ohos_nweb/src/cef_delegate/nweb_render_handler.h
@@ -248,6 +248,11 @@ class NWebRenderHandler : public CefRenderHandler {
   // #ifdef OHOS_HTML_SELECT
   float GetCefDeviceRatio() const { return cef_device_ratio_; }
   // #endif
+
+#if BUILDFLAG(IS_OHOS)
+  void SetScreenOffset(double x, double y);
+  void GetScreenOffset(CefRefPtr<CefBrowser> browser, double& x, double& y) override;
+#endif
   // Include the default reference counting implementation.
   IMPLEMENT_REFCOUNTING(NWebRenderHandler);
 
@@ -288,6 +293,11 @@ class NWebRenderHandler : public CefRenderHandler {
 #endif  // defined(OHOS_INPUT_EVENTS)
   bool isSystemKeyboard_ = true;
   bool gesture_event_result_ = false;
+
+#if BUILDFLAG(IS_OHOS)
+  double screen_x_ = 0;
+  double screen_y_ = 0;
+#endif
 
   std::function<void(double, double)> on_scroll_cb_ = nullptr;
 };
