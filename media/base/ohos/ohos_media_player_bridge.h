636c5a2a6645a486c404ddbaaeae96625fecd0d8
qq_42700029 <wuqiwen7@huawei.com>
2025-03-19 14:13:37 +0800

    update: 更新文件 ohos_media_player_bridge.h
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- media/base/ohos/ohos_media_player_bridge.h
+++ media/base/ohos/ohos_media_player_bridge.h
@@ -131,6 +131,8 @@ class MEDIA_EXPORT OHOSMediaPlayerBridge {
   // Set media player surface and register listener
   void SetPlayerSurface();
 
+  uint64_t uv__get_addr_tag(void* addr);
+
   const std::string surfaceFormat = "SURFACE_FORMAT";
   std::unique_ptr<OHOS::NWeb::PlayerAdapter> player_ = nullptr;
   std::deque<std::shared_ptr<OHOS::NWeb::SurfaceBufferAdapter>> cached_buffers_;
