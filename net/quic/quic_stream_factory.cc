d9ec0d083b72e220586248f5282a57cf7b85460e
TaoXueao <taoxueao1@huawei.com>
2025-03-23 18:36:47 +0800

    update: 更新文件 quic_stream_factory.cc
    Signed-off-by: TaoXueao <taoxueao1@huawei.com>

--- net/quic/quic_stream_factory.cc
+++ net/quic/quic_stream_factory.cc
@@ -1861,6 +1861,9 @@ void QuicStreamFactory::OnIPAddressChanged() {
 
   set_is_quic_known_to_work_on_current_network(false);
   if (params_.close_sessions_on_ip_change) {
+#ifdef OHOS_NETWORK_LOAD
+    LOG(INFO) << "close quic sessions on ip change";
+#endif
     CloseAllSessions(ERR_NETWORK_CHANGED, quic::QUIC_IP_ADDRESS_CHANGED);
   } else {
     DCHECK(params_.goaway_sessions_on_ip_change);
