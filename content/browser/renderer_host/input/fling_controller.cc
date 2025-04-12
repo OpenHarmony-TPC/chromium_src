9df2194fef70a516b311a65c646d285a91107512
caochufeng <caochufeng1@huawei.com>
2025-04-12 20:26:42 +0800

    Web滑动提频报点拆分
    Signed-off-by: caochufeng <caochufeng1@huawei.com>

--- content/browser/renderer_host/input/fling_controller.cc
+++ content/browser/renderer_host/input/fling_controller.cc
@@ -16,6 +16,7 @@
 #if BUILDFLAG(IS_OHOS)
 #include "base/ohos/dynamic_frame_loss_monitor.h"
 #include "base/ohos/ltpo/include/sliding_observer.h"
+#include "base/ohos/sys_info_utils.h"
 #include "content/browser/gpu/gpu_process_host.h"
 #include "base/report_loss_frame.h"
 #include "ohos_adapter_helper.h"
@@ -44,6 +45,8 @@ constexpr base::TimeDelta kMaxMicrosecondsFromFlingTimestampToFirstProgress =
 const float kMinInertialScrollDelta = 0.1f;
 
 const char* kFlingTraceName = "FlingController::HandlingGestureFling";
+
+const int SOC_PERF_WEB_SLIDE_SCROLL = 10097;
 }  // namespace
 
 namespace content {
@@ -190,9 +193,11 @@ bool FlingController::ObserveAndMaybeConsumeGestureEvent(
     OHOS::NWeb::OhosAdapterHelper::GetInstance()
         .GetHiTraceAdapterInstance()
         .StartAsyncTrace(fling_string, 0);
+    int socPerfId = base::ohos::IsPcDevice() ? SOC_PERF_WEB_SLIDE_SCROLL :
+        OHOS::NWeb::SocPerfClientAdapter::SOC_PERF_WEB_GESTURE_ID;
     OHOS::NWeb::OhosAdapterHelper::GetInstance()
         .CreateSocPerfClientAdapter()
-        ->ApplySocPerfConfigByIdEx(OHOS::NWeb::SocPerfClientAdapter::SOC_PERF_WEB_GESTURE_ID, true);
+        ->ApplySocPerfConfigByIdEx(socPerfId, true);
 
     if (instance_count_ == 1) {
       LOG(DEBUG) << "FlingController::ObserveAndMaybeConsumeGestureEvent::dvsyncSwitch=true";
