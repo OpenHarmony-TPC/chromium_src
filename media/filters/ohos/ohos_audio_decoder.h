23b98fbb9e62a82db4a166259478301acf851048
GrowUpEveryday <yangjiawei24@huawei.com>
2025-04-16 16:20:17 +0800

    update: 更新文件 ohos_audio_decoder.h
    Signed-off-by: GrowUpEveryday <yangjiawei24@huawei.com>

--- media/filters/ohos/ohos_audio_decoder.h
+++ media/filters/ohos/ohos_audio_decoder.h
@@ -186,6 +186,8 @@ class AudioDecoderCallback : public AudioDecoderCallbackAdapter {
 
     virtual void UpdateOutputFormat() = 0;
 
+    virtual void OnError(int32_t errorCode) = 0;
+
    protected:
     virtual ~Client() {}
   };
@@ -279,6 +281,8 @@ class OHOSAudioDecoder : public AudioDecoder,
 
   void AddOutputBuffer(uint32_t index, uint8_t* bufferData, uint32_t size, int64_t pts, BufferFlag flag) override;
 
+  void OnError(int32_t errorCode) override;
+
   void UpdateOutputFormat() override;
 
   void OnMediaCryptoReady(InitCB init_cb, void* session, bool requires_secure_video_codec);
@@ -365,6 +369,8 @@ class OHOSAudioDecoder : public AudioDecoder,
 
   // define a timer for waiting licence
   base::RepeatingTimer io_timer_;
+
+  bool audio_decoder_created_ = false;
 };
 
 }
