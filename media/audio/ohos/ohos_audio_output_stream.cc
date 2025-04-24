19ee78513bbf103512de1b58a8223af9a88e9785
GrowUpEveryday <yangjiawei24@huawei.com>
2025-04-24 20:10:51 +0800

    update: 更新文件 ohos_audio_output_stream.cc
    Signed-off-by: GrowUpEveryday <yangjiawei24@huawei.com>

--- media/audio/ohos/ohos_audio_output_stream.cc
+++ media/audio/ohos/ohos_audio_output_stream.cc
@@ -307,8 +307,8 @@ void OHOSAudioOutputStream::Start(AudioSourceCallback* callback) {
   }
 
   WEBCONTENT_SET.insert(webContent_);
+  callback_ = callback;
   if (StartRender()) {
-    callback_ = callback;
     if (memset_s(audio_data_[active_buffer_index_],
         buffer_size_bytes_, 0, buffer_size_bytes_) != EOK) {
       LOG(ERROR) << "audio data memset_s failed.";
@@ -321,6 +321,8 @@ void OHOSAudioOutputStream::Start(AudioSourceCallback* callback) {
       Flush();
       PumpSamples();
     }
+  } else {
+    callback_ = nullptr;
   }
 }
 
