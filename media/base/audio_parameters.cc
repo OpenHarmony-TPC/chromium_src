b08e4561da2b3eaf4b91bbba8b3cf5dc4fbca195
zwpless1 <zhaowenpu1@huawei.com>
2025-04-30 15:58:45 +0800

    update: 更新文件 audio_parameters.cc
    Signed-off-by: zwpless1 <zhaowenpu1@huawei.com>

--- media/base/audio_parameters.cc
+++ media/base/audio_parameters.cc
@@ -173,9 +173,9 @@ AudioParameters::AudioParameters(const AudioParameters&) = default;
 AudioParameters& AudioParameters::operator=(const AudioParameters&) = default;
 
 #if defined(OHOS_MEDIA_POLICY)
-bool AudioParameters::operator==(const AudioParameters& others) const {
-  return render_process_id_ == others.render_process_id() &&
-         render_frame_id_ == others.render_frame_id();
+bool AudioParameters::operator==(const AudioParameters& other) const {
+  return render_process_id_ == other.render_process_id() &&
+         render_frame_id_ == other.render_frame_id();
 }
 #endif
 
