b6d4a4f4622802f1631d6a72b8279f8014d0d3db
zwpless1 <zhaowenpu1@huawei.com>
2025-05-07 17:01:57 +0800

    update: 更新文件 ohos_audio_focus_controller.cc
    Signed-off-by: zwpless1 <zhaowenpu1@huawei.com>

--- media/audio/ohos/ohos_audio_focus_controller.cc
+++ media/audio/ohos/ohos_audio_focus_controller.cc
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "media/audio/ohos/ohos_audio_focus_controller.h"
 #include "base/logging.h"
 #include "base/synchronization/waitable_event.h"
