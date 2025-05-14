99c627b37ee593d78aa13db54d0b05fe63281469
liaoyunfan <liaoyunfan@huawei.com>
2025-05-14 16:38:03 +0800

    Co-authored-by: liaoyunfan<liaoyunfan@huawei.com>

--- ohos_nweb/src/cef_delegate/nweb_event_handler.cc
+++ ohos_nweb/src/cef_delegate/nweb_event_handler.cc
@@ -524,9 +524,7 @@ void NWebEventHandler::SendMouseEvent(int x,
       last_mouse_y_ = y;
       browser_->GetHost()->SendMouseMoveEvent(mouseEvent, false);
     } else if (NWebInputDelegate::IsMouseLeave(action)) {
-      if (NWebInputDelegate::IsMouseUp(previous_action_) || previous_button_ == MBT_RIGHT) {
-        browser_->GetHost()->SendMouseMoveEvent(mouseEvent, true);
-      }
+      browser_->GetHost()->SendMouseMoveEvent(mouseEvent, true);
     } else {
       LOG(DEBUG) << "mouse event action: " << action;
     }
