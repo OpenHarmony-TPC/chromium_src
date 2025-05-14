a3ec0c2f9d3ea1a40e6cd87933beefe4a2224aeb
lingjinyue <lingyue2@huawei.com>
2025-05-14 12:09:43 +0800

    更新version返回位置
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- components/embedder_support/make_ohos_arkweb_version.py
+++ components/embedder_support/make_ohos_arkweb_version.py
@@ -21,7 +21,8 @@ def ReadHeader(options):
             return defaultVersion
         if "app" in data and "versionName" in data["app"]:
             version = data["app"]["versionName"].split("sp")[0]
-    return version
+            return version
+    return defaultVersion
 
 def WriteHeader(options, version):
     header_macros = "ARKWEB_VERSION_H"
