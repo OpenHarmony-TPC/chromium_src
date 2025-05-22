f8d8a3fe7f900ac8e1e2cd7140e9c7c1492b56c1
whuqincheng <liweiyang1@huawei.com>
2025-05-22 11:52:20 +0800

    ScreenCapture callback register error
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- media/base/ohos/ohos_media_player_bridge.cc
+++ media/base/ohos/ohos_media_player_bridge.cc
@@ -386,8 +386,8 @@ base::TimeDelta OHOSMediaPlayerBridge::GetMediaTime() {
   int32_t time = -1;
   int32_t ret = player_->GetCurrentTime(time);
   if (ret == 0 && time == -1) {
-    //if is livestream, return system time	
-    auto system_time = base::Time::Now().ToInternalValue() / base::Time::kMicrosecondsPerMillisecond;	
+    //if is livestream, return system time
+    auto system_time = base::Time::Now().ToInternalValue() / base::Time::kMicrosecondsPerMillisecond;
     return base::Milliseconds(system_time);
   }
   return base::Milliseconds(time);
