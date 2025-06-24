238d5e143a97d7160e829234667525cdfa2e2b6f
Hellen_666 <yanxuejun@h-partners.com>
2025-06-24 21:52:35 +0800

    fix device UT
    Signed-off-by: Hellen_666 <yanxuejun@h-partners.com>

--- device/bluetooth/bluetooth_remote_gatt_characteristic_unittest.cc
+++ device/bluetooth/bluetooth_remote_gatt_characteristic_unittest.cc
@@ -38,7 +38,7 @@
 #elif BUILDFLAG(IS_FUCHSIA)
 #include "device/bluetooth/test/bluetooth_test_fuchsia.h"
 #elif BUILDFLAG(IS_OHOS)
-#include "arkweb/choromum_ext/device/bluetooth/test/bluetooth_test_ohos.h"
+#include "arkweb/chromium_ext/device/bluetooth/test/bluetooth_test_ohos.h"
 #endif
 
 using testing::_;
