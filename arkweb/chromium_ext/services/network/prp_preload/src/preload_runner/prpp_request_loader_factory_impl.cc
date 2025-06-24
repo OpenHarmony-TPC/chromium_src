422d1f29e0bd2d4b01cf9c730b035c977b9939cd
wen-xue-yang <yangwenxue1@huawei.com>
2025-06-24 15:14:18 +0800

    preload bugfix
    Signed-off-by: wen-xue-yang <yangwenxue1@huawei.com>

--- arkweb/chromium_ext/services/network/prp_preload/src/preload_runner/prpp_request_loader_factory_impl.cc
+++ arkweb/chromium_ext/services/network/prp_preload/src/preload_runner/prpp_request_loader_factory_impl.cc
@@ -150,8 +150,12 @@ bool PRPPRequestLoaderFactoryImpl::MatchRequestHeaders(std::shared_ptr<PRRequest
 {
   bool match = true;
   std::set<std::string> dynamic_header_keys;
+  net::HttpRequestHeaders extra_request_headers = req_info->extra_request_headers();
+  if (resource_request.headers.GetHeaderVector().size() != extra_request_headers.GetHeaderVector().size()) {
+	LOG(DEBUG) << "PRPPreload.PRPPRequestLoaderFactoryImpl::MatchRequestHeaders header size not match";
+	return false;
+  }
   for (auto item : resource_request.headers.GetHeaderVector()) {
-	net::HttpRequestHeaders extra_request_headers = req_info->extra_request_headers();
 	net::HttpRequestHeaders::HeaderVector::const_iterator iter =
 	  extra_request_headers.GetHeaderVector().end();
 	for (auto it = extra_request_headers.GetHeaderVector().begin();
