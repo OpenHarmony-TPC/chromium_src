abaf93e0a63f2eb5933645ca716991aeae4f5541
ZhaoPengfei <zhaopengfei35@huawei.com>
2025-06-25 18:31:05 +0800

    add surface layer for same layer
    Signed-off-by: ZhaoPengfei <zhaopengfei35@huawei.com>

--- arkweb/chromium_ext/cc/layer/surface_layer_for_include.cc
+++ arkweb/chromium_ext/cc/layer/surface_layer_for_include.cc
@@ -13,7 +13,21 @@
  * limitations under the License.
  */
 
+#include "base/logging.h"
+
 namespace cc {
+
+#if BUILDFLAG(ARKWEB_SAME_LAYER)
+scoped_refptr<SurfaceLayer> SurfaceLayer::Create(UpdateSubmissionStateCB update_submission_state_callback,
+    RectChangeCallback callback,
+    RectVisibilityChangeCallback visibilitycallback,
+    LayerRemovedVisibilityCallback layerRemovedCallback)
+{
+    return base::WrapRefCounted(
+        new SurfaceLayer(std::move(update_submission_state_callback),
+            std::move(callback), std::move(visibilitycallback), std::move(layerRemovedCallback)));
+}
+#endif
 
 #if BUILDFLAG(ARKWEB_CUSTOM_VIDEO_PLAYER)
 void SurfaceLayer::SetVideoRectChangeCallback(RectChangeCallback callback) {
@@ -23,7 +37,13 @@ void SurfaceLayer::SetVideoRectChangeCallback(RectChangeCallback callback) {
 void SurfaceLayer::OnLayerRectUpdate(const gfx::Rect& rect) {
   if (video_rect_change_callback_) {
     video_rect_change_callback_.Run(rect);
+    return;
   }
+#if BUILDFLAG(ARKWEB_SAME_LAYER)
+  if (!rect_change_callback_.is_null()) {
+    rect_change_callback_.Run(rect);
+  }
+#endif
 }
 #endif  // ARKWEB_CUSTOM_VIDEO_PLAYER
 
@@ -39,4 +59,50 @@ void SurfaceLayer::OnLayerBoundsUpdate(const gfx::Rect& bounds) {
 }
 #endif  // ARKWEB_VIDEO_ASSISTANT
 
+#if BUILDFLAG(ARKWEB_SAME_LAYER)
+SurfaceLayer::SurfaceLayer(UpdateSubmissionStateCB update_submission_state_callback,
+    RectChangeCallback callback,
+    RectVisibilityChangeCallback visibilitycallback,
+    LayerRemovedVisibilityCallback layerRemovedCallback)
+    : update_submission_state_callback_(
+      std::move(update_submission_state_callback)),
+      may_contain_video_(false),
+      deadline_in_frames_(0u),
+      stretch_content_to_fill_bounds_(false),
+      surface_hit_testable_(false),
+      has_pointer_events_none_(false),
+      is_reflection_(false),
+      rect_change_callback_(std::move(callback)),
+      rect_visibility_change_callback_(std::move(visibilitycallback)),
+      layer_removed_visibility_callback_(std::move(layerRemovedCallback)) {}
+
+void SurfaceLayer::OnLayerRectVisibilityChange(bool visibility)
+{
+  LOG(INFO) << "[NativeEmbed] SurfaceLayer::OnLayerRectVisibilityChange " << visibility;
+  if (!rect_visibility_change_callback_.is_null()) {
+    rect_visibility_change_callback_.Run(visibility);
+  }
+}
+
+void SurfaceLayer::ResetLayerRectUpdateCallback()
+{
+  LOG(INFO) << "[NativeEmbed] SurfaceLayer::ResetLayerRectUpdateCallback";
+  if (!rect_change_callback_.is_null()) {
+    rect_change_callback_.Reset();
+  }
+}
+
+void SurfaceLayer::ResetLayerRectVisibilityChangeCallback()
+{
+  if (!rect_visibility_change_callback_.is_null()) {
+    rect_visibility_change_callback_.Reset();
+  }
+}
+
+void SurfaceLayer::CleanupVisibilityForRemovedLayer(bool visibility) {
+  if (!layer_removed_visibility_callback_.is_null()) {
+    layer_removed_visibility_callback_.Run(visibility);
+  }
+}
+#endif
 }  // namespace cc
