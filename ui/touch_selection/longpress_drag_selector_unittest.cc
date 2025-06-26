7bde230dc9825dff6fe6682f93bec2c0c21f0d8c
ia_oi <jiangjiacheng3@h-partners.com>
2025-06-26 15:43:35 +0800

    长按阔选时选区跟手
    Signed-off-by: ia_oi <jiangjiacheng3@h-partners.com>

--- ui/touch_selection/longpress_drag_selector_unittest.cc
+++ ui/touch_selection/longpress_drag_selector_unittest.cc
@@ -6,6 +6,7 @@
 
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/events/test/motion_event_test_utils.h"
+#include "arkweb/ohos_nweb_ex/build/features/features.h"
 
 using ui::test::MockMotionEvent;
 
@@ -69,7 +70,9 @@ class LongPressDragSelectorTest : public testing::Test,
 
   gfx::PointF GetSelectionEnd() const override { return selection_end_; }
 
+#if BUILDFLAG(ARKWEB_MENU)
   gfx::PointF GetSelectionTop() const override { return selection_start_; }
+#endif
 
  private:
   bool dragging_;
