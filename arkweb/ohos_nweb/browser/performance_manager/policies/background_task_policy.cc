1f099682d738057a7cb637c861afd3b54d8200ce
sinat_22543221 <xiajianchun1@huawei.com>
2025-06-26 20:08:12 +0800

    update: 更新文件 background_task_policy.cc
    Signed-off-by: sinat_22543221 <xiajianchun1@huawei.com>

--- arkweb/ohos_nweb/browser/performance_manager/policies/background_task_policy.cc
+++ arkweb/ohos_nweb/browser/performance_manager/policies/background_task_policy.cc
@@ -36,6 +36,7 @@ BackgroundTaskPolicy::BackgroundTaskPolicy()
     : background_task_holder_(
           std::make_unique<mechanism::BackgroundTaskHolder>()),
       media_playing_num_(0),
+      visible_page_num_(0),
       audio_state_num_(0) {}
 BackgroundTaskPolicy::~BackgroundTaskPolicy() = default;
 
