8b22a2b9b0522e653a137ac04ebf50578c5c2fb6
hoo334 <liminghu1@huawei.com>
2025-07-04 16:53:59 +0800

    update: 更新文件 camera_manager_adapter_impl_fuzzer.cpp
    Signed-off-by: hoo334 <liminghu1@huawei.com>

--- arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/camera_adapter/camera_manager_adapter_impl_fuzzer.cpp
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/camera_adapter/camera_manager_adapter_impl_fuzzer.cpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,8 +12,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #define private public
- #include "arkweb/ohos_adapter_ndk/camera_adapter/include/camera_manager_adapter_impl.h"
+#include "arkweb/ohos_adapter_ndk/camera_adapter/include/camera_manager_adapter_impl.h"
  
 #include <fuzzer/FuzzedDataProvider.h>
 #include <inputmethod/inputmethod_controller_capi.h>
