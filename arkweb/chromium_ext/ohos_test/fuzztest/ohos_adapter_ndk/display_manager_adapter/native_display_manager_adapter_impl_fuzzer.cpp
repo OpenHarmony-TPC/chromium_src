cdea98ad147c4019138afcadbfb161efb174b37c
sunbin55 <sunbin55@noreply.gitcode.com>
2025-07-04 10:54:22 +0800

    IssueNo:#3024:[Bug]:132内核fuzz覆盖率提升 Description: (webview的fuzz覆盖率) Sig: SIG_ApplicationFramework Feature or Bugfix: (Bugfix) Binary Source: No 是否涉及非兼容变更: (不涉及) TDD自验结果(如有失败用例需提供基线执行情况): (不涉及) XTS自验结果(如有失败用例需提供基线执行情况): (不涉及) Signed-off-by: sunbin55 <sunbin55@h-partners.com>

---
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/display_manager_adapter/native_display_manager_adapter_impl_fuzzer.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#define private public
+#include <fuzzer/FuzzedDataProvider.h>
+#include "native_display_manager_adapter_impl.h"
+#include "arkweb/ohos_nweb/src/nweb_hilog.h"
+#include "ohos_sdk/openharmony/native/sysroot/usr/include/window_manager/oh_display_info.h"
+
+
+using namespace OHOS::NWeb;
+
+void FuzzNativeDisplayAdapterImpl(FuzzedDataProvider* fdp) {
+    std::shared_ptr<FoldStatusListenerAdapter> listener =
+     std::make_unique<FoldStatusListenerAdapter>();
+    NativeFoldStatusListenerAdapterImpl adapter(listener);
+    int mode = fdp->ConsumeIntegralInRange<int>(0, 5);
+    adapter.OnFoldStatusChanged(static_cast<NativeDisplayManager_FoldDisplayMode>(mode));
+
+    std::shared_ptr<NativeDisplayAdapterImpl> displayAdapter =
+     std::make_unique<NativeDisplayAdapterImpl>();
+
+    int rotation = fdp->ConsumeIntegralInRange<int>(0, 4);
+    displayAdapter->ConvertRotationType(static_cast<NativeDisplayManager_Rotation>(rotation));
+
+    int orientation = fdp->ConsumeIntegralInRange<int>(0, 5);
+    displayAdapter->
+    ConvertOrientationType(static_cast<NativeDisplayManager_Orientation>(orientation));
+    displayAdapter->
+    ConvertDisplayOrientationType(static_cast<NativeDisplayManager_Orientation>(orientation));
+
+    int displayMode = fdp->ConsumeIntegralInRange<int>(0, 5);
+    displayAdapter->
+    ConvertFoldStatus(static_cast<NativeDisplayManager_FoldDisplayMode>(displayMode));
+
+    displayAdapter->GetId();
+    displayAdapter->GetWidth();
+    displayAdapter->GetHeight();
+    displayAdapter->GetVirtualPixelRatio();
+    displayAdapter->GetRotation();
+    displayAdapter->GetOrientation();
+    displayAdapter->GetDpi();
+    displayAdapter->GetDisplayOrientation();
+    displayAdapter->GetFoldStatus();
+    displayAdapter->IsFoldable();
+    displayAdapter->GetName();
+    displayAdapter->GetAvailableWidth();
+    displayAdapter->GetAvailableHeight();
+    displayAdapter->GetAliveStatus();
+    displayAdapter->GetDisplayState();
+    displayAdapter->GetDensityDpi();
+    displayAdapter->GetX();
+    displayAdapter->GetY();
+    displayAdapter->GetDisplaySourceMode();
+    displayAdapter->GetPhysicalWidth();
+    displayAdapter->GetPhysicalHeight();
+    displayAdapter->GetDefaultVirtualPixelRatio();
+
+    std::shared_ptr<NativeDisplayManagerAdapterImpl> displaymanger =
+     std::make_unique<NativeDisplayManagerAdapterImpl>();
+    displaymanger->GetDefaultDisplayId();
+    displaymanger->GetDefaultDisplay();
+    uint32_t displayId = fdp->ConsumeIntegral<uint32_t>();
+    displaymanger->DisplayChangeCallback(displayId);
+
+    std::shared_ptr<DisplayListenerAdapter> displaylistener = nullptr;
+    displaymanger->RegisterDisplayListener(displaylistener);
+    uint32_t id = fdp->ConsumeIntegral<uint32_t>();
+    displaymanger->UnregisterDisplayListener(id);
+    displaymanger->IsDefaultPortrait();
+
+    std::shared_ptr<FoldStatusListenerAdapter> flodlistener =
+     std::make_unique<FoldStatusListenerAdapter>();
+    displaymanger->RegisterFoldStatusListener(flodlistener);
+    uint32_t flodId = fdp->ConsumeIntegral<uint32_t>();
+    displaymanger.UnregisterFoldStatusListener(flodId);
+    displaymanger->GetPrimaryDisplay();
+    displaymanger->GetAllDisplays();
+}
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    if((data == nullptr) || (size < sizeof(int32_t))) {
+        return 0;
+    }
+    FuzzedDataProvider fdp(data, size);
+    FuzzNativeDisplayAdapterImpl(&fdp);
+    return 0;
+}
+ 
---
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/drawing_text_adapter/ohos_drawing_text_adapter_impl_fuzzer.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include "ohos_drawing_text_adapter_impl.h"
+#include "ohos_drawing_text_adapter.h"
+#include "ohos_sdk/openharmony/native/sysroot/usr/include/native_drawing/drawing_types.h"
+#include "ohos_sdk/openharmony/native/sysroot/usr/include/native_drawing/drawing_text_typography.h"
+
+using namespace OHOS::NWeb;
+
+void FuzzdDrawingTextAdapter(FuzzedDataProvider* fdp) {
+    OhosDrawingTextFontAdapterImpl& adapter =
+     OhosDrawingTextFontAdapterImpl::GetInstance();
+    int32_t systemFontType = fdp->ConsumeIntegral<int32_t>();
+    void* drawingArray = nullptr;
+    adapter.GetSystemFontFullNamesByType(systemFontType, &drawingArray);
+
+    OH_Drawing_String obj;
+    uint8_t dataNweb = fdp->ConsumeIntegral<uint8_t>();
+    obj.strData=&dataNweb;
+    obj.strLen=fdp->ConsumeIntegral<uint32_t>();
+    void* drawingFontDescriptor=nullptr;
+    adapter.GetFontDescriptorByFullName(&obj, systemFontType, &drawingFontDescriptor);
+
+    int32_t indexOfFullName = fdp->ConsumeIntegral<int32_t>();
+    const void* drawingString=nullptr;
+    adapter.GetSystemFontFullNameByIndex(drawingArray, indexOfFullName, &drawingString);
+    adapter.DestroySystemFontFullNames(drawingArray);
+    void* descriptor=nullptr;
+    adapter.DestroyFontDescriptor(descriptor);
+}
+
+void FuzzdDrawingTextTypographyAdapter(FuzzedDataProvider* fdp) {
+    OhosDrawingTextTypographyAdapter& adapter = 
+    OhosDrawingTextTypographyAdapter::GetInstance();
+     
+    int errorCode=fdp->ConsumeIntegralInRanger<int>(0, 5);
+    OH_Drawing_FontConfigInfoErrorCode errorPtr=static_cast<OH_Drawing_FontConfigInfoErrorCode>(errorCode);
+    void* fontConfigInfo=nullptr;
+    adapter.GetSystemFontConfigInfo(&errorPtr, &fontConfigInfo);
+
+    void* drawingArray=nullptr;
+    int32_t sizeOfArray=fdp->ConsumeIntegral<int32_t>();
+    adapter.GetDrawingArraySize(drawingArray, sizeOfArray);
+    
+    adapter.DestroySystemFontConfigInfo(nullptr);
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+    if((data == nullptr) || (size < sizeof(int32_t))) {
+        return 0;
+    }
+    FuzzedDataProvider fdp(data, size);
+    FuzzdDrawingTextAdapter(&fdp);
+    FuzzdDrawingTextTypographyAdapter(&fdp);
+    return 0;
+}
+ 
