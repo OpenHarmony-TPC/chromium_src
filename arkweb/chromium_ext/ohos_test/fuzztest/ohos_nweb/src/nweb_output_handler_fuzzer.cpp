48bd8e00b46f8ca08ab59b25ce9afa9878378a70
sunbin55 <sunbin55@noreply.gitcode.com>
2025-07-07 09:09:34 +0800

    IssueNo:#3024:[Bug]:132内核fuzz覆盖率提升 Description: (webview的fuzz覆盖率) Sig: SIG_ApplicationFramework Feature or Bugfix: (Bugfix) Binary Source: No 是否涉及非兼容变更: (不涉及) TDD自验结果(如有失败用例需提供基线执行情况): (不涉及) XTS自验结果(如有失败用例需提供基线执行情况): (不涉及) Signed-off-by: sunbin55 <sunbin55@h-partners.com>

--- arkweb/chromium_ext/ohos_test/fuzztest/ohos_nweb/src/nweb_output_handler_fuzzer.cpp
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_nweb/src/nweb_output_handler_fuzzer.cpp
@@ -48,7 +48,6 @@ void NwebOutputHandlerFuzzTest(FuzzedDataProvider* fdp) {
     handler->GetNativeWindowFromSurface(surface);
 }
 
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     if((data == nullptr) || (size < sizeof(int32_t))) {
         return 0;
