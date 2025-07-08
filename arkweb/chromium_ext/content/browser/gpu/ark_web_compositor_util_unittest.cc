126f800e5949070bedac012dc2567cbc6a86b820
wangshengwen333 <wangshengwen4@h-partners.com>
2025-07-08 18:22:14 +0800

    提交渲染合成UT
    Signed-off-by: wangshengwen333 <wangshengwen4@h-partners.com>

---
+++ arkweb/chromium_ext/content/browser/gpu/ark_web_compositor_util_unittest.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "arkweb\chromium_ext\content\browser\gpu\ark_web_compositor_util.h"
+
+#include <optional>
+#include <gtest/gtest.h>
+#include <sys/mman.h>
+
+#include "arkweb/chromium_ext/base/ohos/sys_info_utils_ext.h"
+#include "arkweb/chromium_ext/content/public/common/content_switches_ext.h"
+#include "base/command_line.h"
+#include "base/test/scoped_command_line.h"
+#include "cc/base/switches.h"
+
+#define private public
+
+using namespace testing;
+
+namespace content {
+
+class ArkWebRenderUtilsTest : public testing::Test {
+protected:
+  void SetUp() {
+    original_command_line_ = base::CommandLine::ForCurrentProcess()->argv();
+  }
+
+  void TearDown() {
+    base::CommandLine::Reset();
+    base::CommandLine::Init(0, nullptr);
+    base::CommandLine::ForCurrentProcess()->InitFromArgv(original_command_line_);
+  }
+private:
+  std::vector<std::string> original_command_line_;
+};
+
+TEST_F(ArkWebRenderUtilsTest, WithoutNwebExFlag)
+{
+    auto result = ArkWebNumberOfRendererRasterThreads();
+    EXPECT_FALSE(result.has_value());
+}
+}
