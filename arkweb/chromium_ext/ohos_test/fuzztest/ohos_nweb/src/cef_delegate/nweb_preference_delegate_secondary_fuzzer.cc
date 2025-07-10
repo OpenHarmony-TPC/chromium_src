3e94522ac326b6692c038c4d9dcdb9f11386f168
langsongping <langsongping1@huawei-partners.com>
2025-07-10 19:52:24 +0800

    add fuzz
    Signed-off-by: langsongping <langsongping1@huawei-partners.com>

--- arkweb/chromium_ext/ohos_test/fuzztest/ohos_nweb/src/cef_delegate/nweb_preference_delegate_secondary_fuzzer.cc
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_nweb/src/cef_delegate/nweb_preference_delegate_secondary_fuzzer.cc
@@ -395,6 +395,400 @@ void NWebPerferenceDelegate067FuzzTest(FuzzedDataProvider* fdp) {
     delegate.StandardFontFamilyName();
 }
 
+void NWebPerferenceDelegate068FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.UserAgent();
+}
+
+void NWebPerferenceDelegate069FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.ZoomingForTextFactor();
+}
+
+void NWebPerferenceDelegate070FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GeolocationAllowed();
+}
+
+void NWebPerferenceDelegate071FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.AccessModeForSecureOriginLoadFromInsecure();
+}
+
+void NWebPerferenceDelegate072FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.RunningInsecureContentAllowed();
+}
+
+void NWebPerferenceDelegate073FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.UseStricMixedContentCheckingAllowed();
+}
+
+void NWebPerferenceDelegate074FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.MixedContentAutoupgradesAllowed();
+}
+
+void NWebPerferenceDelegate075FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.ZoomingfunctionEnabled();
+}
+
+void NWebPerferenceDelegate076FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsNetworkBlocked();
+}
+
+void NWebPerferenceDelegate077FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.CacheMode();
+}
+
+void NWebPerferenceDelegate078FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutHasInternetPermission(ret);
+}
+
+void NWebPerferenceDelegate079FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsWebDebuggingAccess();
+}
+
+void NWebPerferenceDelegate080FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetMediaPlayGestureAccess();
+}
+
+void NWebPerferenceDelegate081FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetPinchSmoothMode();
+}
+
+void NWebPerferenceDelegate082FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsMultiWindowAccess();
+}
+
+void NWebPerferenceDelegate083FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsHorizontalScrollBarAccess();
+}
+
+void NWebPerferenceDelegate084FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsVerticalScrollBarAccess();
+}
+
+void NWebPerferenceDelegate085FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutHorizontalScrollBarAccess(ret);
+}
+
+void NWebPerferenceDelegate086FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutVerticalScrollBarAccess(ret);
+}
+
+void NWebPerferenceDelegate087FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetOverscrollMode();
+}
+
+void NWebPerferenceDelegate088FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE);
+    delegate.SetBlurOnKeyboardHideMode(ret);
+}
+
+void NWebPerferenceDelegate089FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.SetScrollable(ret);
+}
+
+void NWebPerferenceDelegate090FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetScrollable();
+}
+
+void NWebPerferenceDelegate092FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetNativeEmbedMode();
+}
+
+void NWebPerferenceDelegate093FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.SetIntrinsicSizeEnable(ret);
+}
+
+void NWebPerferenceDelegate094FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetIntrinsicSizeEnable();
+}
+
+void NWebPerferenceDelegate095FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.SetCssDisplayChangeEnabled(ret);
+}
+
+void NWebPerferenceDelegate096FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetCssDisplayChangeEnabled();
+}
+
+void NWebPerferenceDelegate097FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeRandomLengthString(MAX_STRING_LENGTH);
+    auto ret2 = fdp->ConsumeRandomLengthString(MAX_STRING_LENGTH);
+    delegate.RegisterNativeEmbedRule(ret, ret2);
+}
+
+void NWebPerferenceDelegate098FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.SetViewportEnable(ret);
+}
+
+void NWebPerferenceDelegate099FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetViewportEnable();
+}
+
+void NWebPerferenceDelegate100FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetScrollBarColor();
+}
+
+void NWebPerferenceDelegate101FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<int32_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.SetBackgroundColor(ret);
+}
+
+void NWebPerferenceDelegate102FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetBackgroundColor();
+}
+
+void NWebPerferenceDelegate103FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<uint32_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.PutScrollBarColor(ret);
+}
+
+void NWebPerferenceDelegate104FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE);
+    delegate.SetDrawMode(ret);
+}
+
+void NWebPerferenceDelegate105FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetDrawMode();
+}
+
+void NWebPerferenceDelegate106FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutTextAutosizingEnabled(ret);
+}
+
+void NWebPerferenceDelegate107FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsTextAutosizingEnabled();
+}
+
+void NWebPerferenceDelegate108FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.SetFitContent(ret);
+}
+
+void NWebPerferenceDelegate109FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsFitContent();
+}
+
+void NWebPerferenceDelegate110FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeFloatingPointInRange<float>(0, MAX_FLOAT_SIZE);
+    delegate.SetFontWeightScale(ret);
+}
+
+void NWebPerferenceDelegate111FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetFontWeightScale();
+}
+
+void NWebPerferenceDelegate112FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetCopyOptionMode();
+}
+
+void NWebPerferenceDelegate113FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<int32_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.SetGestureFocusMode(ret);
+}
+
+void NWebPerferenceDelegate114FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetGestureFocusMode();
+}
+
+void NWebPerferenceDelegate115FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    auto ret2 = fdp->ConsumeBool();
+    delegate.SetNativeVideoPlayerConfig(ret, ret2);
+}
+
+void NWebPerferenceDelegate116FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutOverlayScrollbarEnabled(ret);
+}
+
+void NWebPerferenceDelegate117FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutAudioExclusive(ret);
+}
+
+void NWebPerferenceDelegate118FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetAudioExclusive();
+}
+
+void NWebPerferenceDelegate119FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<int32_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.PutAudioResumeInterval(ret);
+}
+
+void NWebPerferenceDelegate120FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetAudioResumeInterval();
+}
+
+void NWebPerferenceDelegate121FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnDocumentStart();
+}
+
+void NWebPerferenceDelegate122FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnDocumentStartByOrder();
+}
+
+void NWebPerferenceDelegate123FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnDocumentEnd();
+}
+
+void NWebPerferenceDelegate124FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnDocumentEndByOrder();
+}
+
+void NWebPerferenceDelegate125FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnHeadReady();
+}
+
+void NWebPerferenceDelegate126FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetJavaScriptOnHeadReadyByOrder();
+}
+
+void NWebPerferenceDelegate127FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetWholeWebPageDrawing();
+}
+
+void NWebPerferenceDelegate128FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.SetWholePageDrawing();
+}
+
+void NWebPerferenceDelegate129FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetSurfaceId();
+}
+
+void NWebPerferenceDelegate130FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeRandomLengthString(MAX_STRING_LENGTH);
+    delegate.SetSurfaceId(ret);
+}
+
+void NWebPerferenceDelegate131FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.EnableMixedContentAutoUpgrades(ret);
+}
+
+void NWebPerferenceDelegate132FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.IsMixedContentAutoUpgradesEnabled();
+}
+
+void NWebPerferenceDelegate133FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<int32_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.SetUsageScenario(ret);
+}
+
+void NWebPerferenceDelegate134FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetUsageScenario();
+}
+
+void NWebPerferenceDelegate136FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE);
+    auto ret2 = fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE);
+    delegate.PutBackForwardCacheOptions(ret, ret2);
+}
+
+void NWebPerferenceDelegate137FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetCacheSize();
+}
+
+void NWebPerferenceDelegate138FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetTimeToLive();
+}
+
+void NWebPerferenceDelegate139FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = static_cast<int64_t>(fdp->ConsumeIntegralInRange(MIN_INT_SIZE, MAX_INT_SIZE));
+    delegate.SetDelayDurationForBackgroundTabFreezing(ret);
+}
+
+void NWebPerferenceDelegate140FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetDelayDurationForBackgroundTabFreezing();
+}
+
+void NWebPerferenceDelegate141FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    delegate.GetAutofillCallback();
+}
+
+void NWebPerferenceDelegate142FuzzTest(FuzzedDataProvider* fdp) {
+    NWebPreferenceDelegate delegate;
+    auto ret = fdp->ConsumeBool();
+    delegate.PutWebMediaAVSessionEnabled(ret);
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     if ((data == nullptr) || (size < sizeof(int32_t))) {
         return 0;
@@ -464,6 +858,80 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     NWebPerferenceDelegate065FuzzTest(&fdp);
     NWebPerferenceDelegate066FuzzTest(&fdp);
     NWebPerferenceDelegate067FuzzTest(&fdp);
+    NWebPerferenceDelegate068FuzzTest(&fdp);
+    NWebPerferenceDelegate069FuzzTest(&fdp);
+    NWebPerferenceDelegate070FuzzTest(&fdp);
+    NWebPerferenceDelegate071FuzzTest(&fdp);
+    NWebPerferenceDelegate072FuzzTest(&fdp);
+    NWebPerferenceDelegate073FuzzTest(&fdp);
+    NWebPerferenceDelegate074FuzzTest(&fdp);
+    NWebPerferenceDelegate075FuzzTest(&fdp);
+    NWebPerferenceDelegate076FuzzTest(&fdp);
+    NWebPerferenceDelegate077FuzzTest(&fdp);
+    NWebPerferenceDelegate078FuzzTest(&fdp);
+    NWebPerferenceDelegate079FuzzTest(&fdp);
+    NWebPerferenceDelegate080FuzzTest(&fdp);
+    NWebPerferenceDelegate081FuzzTest(&fdp);
+    NWebPerferenceDelegate082FuzzTest(&fdp);
+    NWebPerferenceDelegate083FuzzTest(&fdp);
+    NWebPerferenceDelegate084FuzzTest(&fdp);
+    NWebPerferenceDelegate085FuzzTest(&fdp);
+    NWebPerferenceDelegate086FuzzTest(&fdp);
+    NWebPerferenceDelegate087FuzzTest(&fdp);
+    NWebPerferenceDelegate088FuzzTest(&fdp);
+    NWebPerferenceDelegate089FuzzTest(&fdp);
+    NWebPerferenceDelegate090FuzzTest(&fdp);
+    NWebPerferenceDelegate092FuzzTest(&fdp);
+    NWebPerferenceDelegate093FuzzTest(&fdp);
+    NWebPerferenceDelegate094FuzzTest(&fdp);
+    NWebPerferenceDelegate095FuzzTest(&fdp);
+    NWebPerferenceDelegate096FuzzTest(&fdp);
+    NWebPerferenceDelegate097FuzzTest(&fdp);
+    NWebPerferenceDelegate098FuzzTest(&fdp);
+    NWebPerferenceDelegate099FuzzTest(&fdp);
+    NWebPerferenceDelegate100FuzzTest(&fdp);
+    NWebPerferenceDelegate101FuzzTest(&fdp);
+    NWebPerferenceDelegate102FuzzTest(&fdp);
+    NWebPerferenceDelegate103FuzzTest(&fdp);
+    NWebPerferenceDelegate104FuzzTest(&fdp);
+    NWebPerferenceDelegate105FuzzTest(&fdp);
+    NWebPerferenceDelegate106FuzzTest(&fdp);
+    NWebPerferenceDelegate107FuzzTest(&fdp);
+    NWebPerferenceDelegate108FuzzTest(&fdp);
+    NWebPerferenceDelegate109FuzzTest(&fdp);
+    NWebPerferenceDelegate110FuzzTest(&fdp);
+    NWebPerferenceDelegate111FuzzTest(&fdp);
+    NWebPerferenceDelegate112FuzzTest(&fdp);
+    NWebPerferenceDelegate113FuzzTest(&fdp);
+    NWebPerferenceDelegate114FuzzTest(&fdp);
+    NWebPerferenceDelegate115FuzzTest(&fdp);
+    NWebPerferenceDelegate116FuzzTest(&fdp);
+    NWebPerferenceDelegate117FuzzTest(&fdp);
+    NWebPerferenceDelegate118FuzzTest(&fdp);
+    NWebPerferenceDelegate119FuzzTest(&fdp);
+    NWebPerferenceDelegate120FuzzTest(&fdp);
+    NWebPerferenceDelegate121FuzzTest(&fdp);
+    NWebPerferenceDelegate122FuzzTest(&fdp);
+    NWebPerferenceDelegate123FuzzTest(&fdp);
+    NWebPerferenceDelegate124FuzzTest(&fdp);
+    NWebPerferenceDelegate125FuzzTest(&fdp);
+    NWebPerferenceDelegate126FuzzTest(&fdp);
+    NWebPerferenceDelegate127FuzzTest(&fdp);
+    NWebPerferenceDelegate128FuzzTest(&fdp);
+    NWebPerferenceDelegate129FuzzTest(&fdp);
+    NWebPerferenceDelegate130FuzzTest(&fdp);
+    NWebPerferenceDelegate131FuzzTest(&fdp);
+    NWebPerferenceDelegate132FuzzTest(&fdp);
+    NWebPerferenceDelegate133FuzzTest(&fdp);
+    NWebPerferenceDelegate134FuzzTest(&fdp);
+    NWebPerferenceDelegate136FuzzTest(&fdp);
+    NWebPerferenceDelegate137FuzzTest(&fdp);
+    NWebPerferenceDelegate138FuzzTest(&fdp);
+    NWebPerferenceDelegate139FuzzTest(&fdp);
+    NWebPerferenceDelegate140FuzzTest(&fdp);
+    NWebPerferenceDelegate141FuzzTest(&fdp);
+    NWebPerferenceDelegate142FuzzTest(&fdp);
+
     return 0;
- }
+}
  
