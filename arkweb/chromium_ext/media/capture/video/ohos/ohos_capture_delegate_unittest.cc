67c99705c6ed57797fcbba61e00a8e0c8ea2f17b
aiu2333 <yanxinghui@huawei.com>
2025-07-10 19:32:44 +0800

    update: 更新文件 ohos_capture_delegate_unittest.cc
    Signed-off-by: aiu2333 <yanxinghui@huawei.com>

--- arkweb/chromium_ext/media/capture/video/ohos/ohos_capture_delegate_unittest.cc
+++ arkweb/chromium_ext/media/capture/video/ohos/ohos_capture_delegate_unittest.cc
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <gtest/gtest.h>
 #include "gmock/gmock.h"
 #include "media/capture/video/ohos/ohos_capture_delegate.h"
