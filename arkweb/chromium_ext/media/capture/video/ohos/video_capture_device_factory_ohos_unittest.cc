e2a27807cb9410a5369ae4ec2610005a2a47718f
aiu2333 <yanxinghui@huawei.com>
2025-07-10 19:33:08 +0800

    update: 更新文件 video_capture_device_factory_ohos_unittest.cc
    Signed-off-by: aiu2333 <yanxinghui@huawei.com>

--- arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_factory_ohos_unittest.cc
+++ arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_factory_ohos_unittest.cc
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include <gtest/gtest.h>
 #include "media/capture/video/ohos/video_capture_device_factory_ohos.h"
 
