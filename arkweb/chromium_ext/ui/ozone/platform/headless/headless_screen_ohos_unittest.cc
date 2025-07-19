eb0740fa5e4e29cfc00b71ad6b460cbbef0ad5f3
geraltXu <xufanghan@huawei.com>
2025-07-19 15:38:22 +0800

    渲染增加ut，解耦
    Signed-off-by:xufanghan <xufanghan@huawei.com>

--- arkweb/chromium_ext/ui/ozone/platform/headless/headless_screen_ohos_unittest.cc
+++ arkweb/chromium_ext/ui/ozone/platform/headless/headless_screen_ohos_unittest.cc
@@ -22,6 +22,19 @@
 #include "ui/display/display_list.h"
 #include "ui/display/display.h"
 #include "ui/display/display_observer.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "mojo/core/embedder/configuration.h"
+#include "mojo/core/embedder/embedder.h"
+
+int main(int argc, char** argv) {
+  base::TestSuite test_suite(argc, argv);
+  mojo::core::Init(mojo::core::Configuration());
+
+  return base::LaunchUnitTestsSerially(
+      argc, argv,
+      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
+}
 
 namespace ui {
 
