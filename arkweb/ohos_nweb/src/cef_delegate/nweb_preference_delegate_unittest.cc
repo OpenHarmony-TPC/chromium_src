a9418a870a1105c8e3a97b457bea317ae3dd0743
whuqincheng <liweiyang1@huawei.com>
2025-07-22 09:25:55 +0800

    update: 更新文件 nweb_preference_delegate_unittest.cc Set Media Resume From BFCachePage
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_preference_delegate_unittest.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_preference_delegate_unittest.cc
@@ -211,7 +211,6 @@ class MockCefBrowser : public CefBrowser, public CefBrowserHost {
   void RegisterScreenCaptureDelegateListener(CefRefPtr<CefScreenCaptureCallback> listener) override {}
   void CustomWebMediaPlayer(bool enable) override {}
 
-  void SetMediaResumeFromBFCachePage(bool resume) override {}
   void UpdateBrowserSettings(const CefBrowserSettings& browser_settings);
 #if BUILDFLAG(ARKWEB_PIP)
   void SetPipNativeWindow(int delegate_id,
@@ -223,6 +222,9 @@ class MockCefBrowser : public CefBrowser, public CefBrowserHost {
                     int frame_routing_id,
                     int event) override {}
 #endif
+#if BUILDFLAG(ARKWEB_BFCACHE)
+  void SetMediaResumeFromBFCachePage(bool resume) override {}
+#endif // BUILDFLAG(ARKWEB_BFCACHE)
   CefBrowserSettings settings_;
   std::unique_ptr<CefBrowserContentsDelegate> contents_delegate_;
 };
