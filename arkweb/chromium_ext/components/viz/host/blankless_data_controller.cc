4f994db8146ea167e2bc0766256258c0442075ae
hw_yang <yangjingxiao1@huawei.com>
2025-07-28 20:41:15 +0800

    回退关于相似度的计算
    Signed-off-by: hw_yang <yangjingxiao1@huawei.com>
    Change-Id: Iea31e503d06f96b31394cb0bdc4e78cfdbfa2189

--- arkweb/chromium_ext/components/viz/host/blankless_data_controller.cc
+++ arkweb/chromium_ext/components/viz/host/blankless_data_controller.cc
@@ -178,7 +178,49 @@ static double CalculateSnapshotSimilarity(std::vector<double>& pixels1,
     return 0.0;
   }
 
-  return CalculateSSIM(pixels1, pixels2, depth);
+  std::vector<double> samePixels;
+  samePixels.resize(width * height, 0);
+  int samePixelsNum = 0;
+  for (int i = 0; i < quad_list.size(); i++) {
+    int rectWidth = quad_list[i].w / 2;
+    int rectHeight = quad_list[i].h / 2;
+    if (rectWidth <= 0 || rectHeight <= 0) {
+      continue;
+    }
+    int x0 = quad_list[i].x / 2;
+    int y0 = quad_list[i].y / 2;
+    if (x0 < 0) {
+      x0 = 0;
+    }
+    if (y0 < 0) {
+      y0 = 0;
+    }
+    std::vector<double> rectPixels1;
+    std::vector<double> rectPixels2;
+    rectPixels1.resize(rectWidth * rectHeight);
+    rectPixels2.resize(rectWidth * rectHeight);
+    for (int y = y0; y < height && y < y0 + rectHeight; y++) {
+      for (int x = x0; x < width && x < x0 + rectWidth; x++) {
+        rectPixels1[(y - y0) * rectWidth + x - x0] = pixels1[y * width + x];
+        rectPixels2[(y - y0) * rectWidth + x - x0] = pixels2[y * width + x];
+      }
+    }
+    double SSIM = CalculateSSIM(rectPixels1, rectPixels2, depth);
+    if (SSIM < SSIM_THRESHOLD) {
+      continue;
+    }
+    for (int y = y0; y < height && y < y0 + rectHeight; y++) {
+      for (int x = x0; x < width && x < x0 + rectWidth; x++) {
+        samePixelsNum += (samePixels[y * width + x] == 0);
+        samePixels[y * width + x] = 1;
+      }
+    }
+    LOG(DEBUG) << "blankless CalculateSnapshotSimilarity SSIM[" << i << "]=" << SSIM;
+  }
+
+  double percent = (double)samePixelsNum / (double)(width * height);
+  LOG(DEBUG) << "blankless CalculateSnapshotSimilarity percent:" << percent;
+  return percent;
 }
 
 static bool LoadBitmap(const char* path, SkBitmap& bitmap) {
