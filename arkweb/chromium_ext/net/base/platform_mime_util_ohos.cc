8857ed18a7c9a9a36f7d383ca783523a301bf7f5
liuzhijie2 <liuzhijie10@huawei.com>
2025-08-01 10:05:22 +0800

    compatibility mimetype
    Signed-off-by: liuzhijie2 <liuzhijie10@huawei.com>

--- arkweb/chromium_ext/net/base/platform_mime_util_ohos.cc
+++ arkweb/chromium_ext/net/base/platform_mime_util_ohos.cc
@@ -9,30 +9,14 @@
 #include "base/nix/mime_util_xdg.h"
 #include "build/build_config.h"
 #include "net/base/platform_mime_util.h"
+#include "third_party/xdg_shared_mime_info/mime_cache.h"
 
 namespace net {
 
 bool PlatformMimeUtil::GetPlatformMimeTypeFromExtension(
     const base::FilePath::StringType& ext,
     std::string* result) const {
-  base::FilePath dummy_path("foo." + ext);
-  std::string out = base::nix::GetFileMimeType(dummy_path);
-
-  // GetFileMimeType likes to return application/octet-stream
-  // for everything it doesn't know - ignore that.
-  if (out == "application/octet-stream" || out.empty())
-    return false;
-
-  // GetFileMimeType returns image/x-ico because that's what's in the XDG
-  // mime database. That database is the merger of the Gnome and KDE mime
-  // databases. Apparently someone working on KDE in 2001 decided .ico
-  // resolves to image/x-ico, whereas the rest of the world uses image/x-icon.
-  // FWIW, image/vnd.microsoft.icon is the official IANA assignment.
-  if (out == "image/x-ico")
-    out = "image/x-icon";
-
-  *result = out;
-  return true;
+  return xdg_shared_mime_info::GetMimeCacheTypeFromExtension(ext, result);
 }
 
 bool PlatformMimeUtil::GetPlatformPreferredExtensionForMimeType(
