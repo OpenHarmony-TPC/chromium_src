121393e745dce33b26a5c371cd4c7d80b2670dac
christmas001 <zhangchao345@huawei.com>
2025-08-01 17:10:59 +0800

    revert fix impl_on_gpu UAF
    Signed-off-by: christmas001 <zhangchao345@huawei.com>

--- arkweb/chromium_ext/components/viz/service/display_embedder/skia_output_surface_impl_on_gpu_utils.h
+++ arkweb/chromium_ext/components/viz/service/display_embedder/skia_output_surface_impl_on_gpu_utils.h
@@ -13,32 +13,31 @@
  * limitations under the License.
  */
 
-#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_DEPENDENCY_IMPL_UTILS_H_
-#define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_DEPENDENCY_IMPL_UTILS_H_
+#ifndef COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_IMPL_ON_GPU_UTILS_H_
+#define COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_IMPL_ON_GPU_UTILS_H_
 
 #include "arkweb/build/features/features.h"
 
-namespace viz {
-class SkiaOutputSurfaceDependencyImpl;
-
-class SkiaOutputSurfaceDependencyImplUtils {
- public:
-  SkiaOutputSurfaceDependencyImpl* skiaOutputSurfaceDependencyImpl;
-  SkiaOutputSurfaceDependencyImplUtils(SkiaOutputSurfaceDependencyImpl* impl);
-
-#if BUILDFLAG(ARKWEB_D_VSYNC)
-  bool GetIsScroll();
+#if BUILDFLAG(ARKWEB_BLANK_OPTIMIZE)
+#include <vector>
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/geometry/rect.h"
 #endif
 
+namespace viz {
+class SkiaOutputSurfaceImplOnGpu;
+
+class SkiaOutputSurfaceImplOnGpuUtils {
+ public:
+  SkiaOutputSurfaceImplOnGpu* skiaOutputSurfaceImplOnGpu;
+  SkiaOutputSurfaceImplOnGpuUtils(SkiaOutputSurfaceImplOnGpu* impl);
+
 #if BUILDFLAG(ARKWEB_BLANK_OPTIMIZE)
-  void SendBlanklessSnapshotInfo(uint64_t blankless_key,
-                                 int32_t lcp_time,
-                                 int64_t pref_hash,
-                                 const SkBitmap& bitmap,
+  void SendBlanklessSnapshotInfo(uint64_t blankless_key, int32_t lcp_time, int64_t pref_hash, const SkBitmap& bitmap,
                                  const std::vector<gfx::Rect>& quad_list);
 #endif
 };
 
 } // namespace viz
 
-#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_DEPENDENCY_IMPL_UTILS_H_
+#endif  // COMPONENTS_VIZ_SERVICE_DISPLAY_EMBEDDER_SKIA_OUTPUT_SURFACE_IMPL_ON_GPU_UTILS_H_
