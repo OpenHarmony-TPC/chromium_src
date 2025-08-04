f31beb8f643ca2b4d477466011a1501a553e78f6
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-08-04 21:28:59 +0800

    2
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

---
+++ arkweb/chromium_ext/ohos_test/unittest/crashpad_dfx/crashpad_dfx_unittest.cc
@@ -0,0 +1,90 @@
+#include <gtest/gtest.h>
+#include <sys/mman.h>
+
+#include "arkweb/chromium_ext/third_party/crashpad/crashpad/util/linux/crashpad_dfx.h"
+
+#include "third_party/bounds_checking_function/include/securec.h"
+#include "third_party/ohos_ndk/includes/ohos_adapter/ohos_adapter_helper.h"
+
+extern uid_t g_process_uid;
+extern std::string g_bundle_name;
+extern std::string g_happen_time;
+
+//模拟全局变量
+uid_t g_process_uid = 0;
+std::string g_bundle_name = "test_bundle";
+std::string g_happen_time = "20250804123456";
+
+namespace crashpad{
+
+class CrashpadDfxTest : public testing :: Test {
+public :
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void CrashpadDfxTest::SetUpTestCase() {}
+
+void CrashpadDfxTest::TearDownTestCase() {}
+
+void CrashpadDfxTest::SetUp() {}
+
+void CrashpadDfxTest::TearDown() {}
+
+TEST_F(CrashpadDfxTest, CrashpadDfxTest001){
+    auto dfxtest = std::make_shared<CrashpadDfx>();
+    EXPECT_NE(dfxtest,nullptr);
+    const std::string process_type = "browser";
+    const std::string happen_time = "20250804123456";
+    const std::string bundle_name = "test bundle";
+    const std::string error_reason = "test_error";
+    dfxtest->ProcessCrashReport(process_type,
+                                happen_time,
+                                bundle_name,
+                                error_reason);
+}
+
+TEST_F(CrashpadDfxTest, CrashpadDfxTest002){
+    auto dfxtest = std::make_shared<CrashpadDfx>();
+    EXPECT_NE(dfxtest,nullptr);
+
+    //传入无效PID -1
+    std::string process_type = dfxtest->GetProcessTypeByPid(-1);
+
+    //预期：无法打开文件，返回“unknown”
+    EXPECT_EQ(process_type, "unknown");
+}
+
+TEST_F(CrashpadDfxTest,  CrashpadDfxTest003){
+    auto dfxtest = std::make_shared<CrashpadDfx>();
+    EXPECT_NE(dfxtest,nullptr);
+    pid_t current_pid = getpid();
+
+    std::string process_type = dfxtest->GetProcessTypeByPid(current_pid);
+    EXPECT_EQ(process_type, "unknown");
+}
+
+TEST_F(CrashpadDfxTest,  CrashpadDfxTest004){
+    auto dfxtest = std::make_shared<CrashpadDfx>();
+    EXPECT_NE(dfxtest,nullptr);
+
+    std::string bundle_name = dfxtest->GetProcessBundleName();
+    EXPECT_EQ(bundle_name, "bundle-name");
+}
+
+TEST_F(CrashpadDfxTest,  CrashpadDfxTest005){
+    std::string current_time = CrashpadDfx::GetCurrentTime();
+    EXPECT_EQ(current_time.length(),14);
+    // 验证格式是否正确，4y2m2d2h2m2s
+}
+
+TEST_F(CrashpadDfxTest,  CrashpadDfxTest006){
+    g_process_uid = 1000;
+    const std::string expected = "crashpad-test-bundle_name-1000-unsupported-20250804123456";
+    const std::string result = CrashpadDfx::UpdateCrashDumpPathSuffix();
+    EXPECT_EQ(result,expected);
+}
+
+}//namespace crashpad
