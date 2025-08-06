0af862923b83546166254ca7f89f8c544c8a31fb
shulssins <yuanshusen@huawei.com>
2025-08-06 21:07:07 +0800

    修复菜单闪烁问题
    Signed-off-by: shulssins <yuanshusen@huawei.com>

--- content/public/browser/touch_selection_controller_client_manager.h
+++ content/public/browser/touch_selection_controller_client_manager.h
@@ -81,6 +81,8 @@ class CONTENT_EXPORT TouchSelectionControllerClientManager {
 #if BUILDFLAG(ARKWEB_PDF)
   // Update the client clipped selection bounds in arkweb.
   virtual void UpdateClientClippedSelectionBounds(const gfx::Rect& clipped_selection_bounds) {}
+
+  virtual void HideHandleAndQuickMenuIfNecessary(bool hide_handles) {}
 #endif  // BUILDFLAG(ARKWEB_PDF)
 };
 
