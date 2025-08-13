e87b22d96332bd82f3678cb10ca7e393ebcf7fc0
ia_oi <jiangjiacheng3@h-partners.com>
2025-08-13 15:04:46 +0800

    Web硬编码整改
    Signed-off-by: ia_oi <jiangjiacheng3@h-partners.com>

--- components/shared_highlighting/core/common/disabled_sites.cc
+++ components/shared_highlighting/core/common/disabled_sites.cc
@@ -67,13 +67,12 @@ bool ShouldOfferLinkToText(const GURL& url) {
   return true;
 }
 
-#if BUILDFLAG(ARKWEB_PRIVACY_COMPLIANCE)
 bool SupportsLinkGenerationInIframe(GURL main_frame_url) {
+#if BUILDFLAG(ARKWEB_PRIVACY_COMPLIANCE)
   const std::unordered_set<std::string> good_hosts = {
       "x.x.x.x", "x.x.x.x", "x.x.x.x",
       "x.x.x.x", "x.x.x.x", "x.x.x.x"};
 #else
-bool SupportsLinkGenerationInIframe(GURL main_frame_url) {
   const std::unordered_set<std::string> good_hosts = {
       "www.google.com", "m.google.com", "mobile.google.com",
       "www.bing.com",   "m.bing.com",   "mobile.bing.com"};
