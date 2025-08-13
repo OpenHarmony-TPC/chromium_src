28840b410fb1244aa1ff049be6d61ce6c6202bc9
GrowUpEveryday <yangjiawei24@huawei.com>
2025-08-13 15:05:56 +0800

    update: 更新文件 drm_adapter_impl.cpp
    Signed-off-by: GrowUpEveryday <yangjiawei24@huawei.com>

--- arkweb/ohos_adapter_ndk/media_adapter/drm_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/media_adapter/drm_adapter_impl.cpp
@@ -29,7 +29,6 @@
 #include "arkweb/ohos_nweb/src/nweb_hilog.h"
 
 namespace OHOS::NWeb {
-const int32_t MAX_PROVISION_RESPONSE_LEN = 16 * 1024;
 std::unordered_map<MediaKeySystem*, std::shared_ptr<DrmCallbackImpl>> DrmAdapterImpl::mediaKeySystemCallbackMap_;
 std::unordered_map<MediaKeySession*, std::shared_ptr<DrmCallbackImpl>> DrmAdapterImpl::mediaKeySessionCallbackMap_;
 std::mutex DrmAdapterImpl::mediaKeySystemCallbackMapMutex_;
@@ -1007,10 +1006,6 @@ int32_t DrmAdapterImpl::ProcessKeySystemResponse(const std::string& response, bo
     bool success = true;
     if (isResponseReceived) {
         int32_t responseLen = static_cast<int32_t>(response.size());
-        if (responseLen > MAX_PROVISION_RESPONSE_LEN) {
-            WVLOG_E("[DRM]response length is too large.");
-            return static_cast<int32_t>(DrmResult::DRM_RESULT_ERROR);
-        }
         std::vector<uint8_t> vec(responseLen);
         errno_t retCopy = memcpy_s(vec.data(), responseLen, response.data(), response.size());
         if (retCopy != 0) {
