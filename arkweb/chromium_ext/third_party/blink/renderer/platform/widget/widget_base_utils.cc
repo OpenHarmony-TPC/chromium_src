9493aee971546fb16db74065bafc992f364940b6
GoNewWay <duzhihao2@huawei.com>
2025-08-16 16:30:29 +0800

    蓝区特性宏隔离
    Signed-off-by: GoNewWay <duzhihao2@huawei.com>

--- arkweb/chromium_ext/third_party/blink/renderer/platform/widget/widget_base_utils.cc
+++ arkweb/chromium_ext/third_party/blink/renderer/platform/widget/widget_base_utils.cc
@@ -132,12 +132,16 @@ void WidgetBaseUtils::ReportForegroundThreadPool() {
     std::vector<scoped_refptr<base::internal::WorkerThread>>& create_workers =
       foreground_thread_group->ReportCreateWorkers();
     if (create_workers.size()) {
+#if BUILDFLAG(IS_ARKWEB)
       GetThreadIdsAndReport(create_workers, true);
+#endif
     }
     std::vector<base::PlatformThreadId>& destroy_workers_ids_ =
       foreground_thread_group->ReportDestroyWorkers();
     if (destroy_workers_ids_.size()) {
+#if BUILDFLAG(IS_ARKWEB)
       GetThreadIdsAndReport(destroy_workers_ids_, false);
+#endif
     }
   }
 }
