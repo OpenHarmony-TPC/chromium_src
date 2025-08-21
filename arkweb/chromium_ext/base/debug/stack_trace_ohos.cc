98182204ce60d67280c409b31c7c3565c5628e37
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-08-21 21:22:21 +0800

    update: 更新文件 stack_trace_ohos.cc 2
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- arkweb/chromium_ext/base/debug/stack_trace_ohos.cc
+++ arkweb/chromium_ext/base/debug/stack_trace_ohos.cc
@@ -112,8 +112,8 @@ char* itoa_r(intptr_t i, char* buf, size_t sz, int base, size_t padding) {
     }
 
     // Output the next digit.
-    *ptr++ = "0123456789abcdef"[j % static_cast<uintptr_t>base];
-    j /= static_cast<uintptr_t>base;
+    *ptr++ = "0123456789abcdef"[j % base];
+    j /= base;
 
     if (padding > 0) {
       padding--;
