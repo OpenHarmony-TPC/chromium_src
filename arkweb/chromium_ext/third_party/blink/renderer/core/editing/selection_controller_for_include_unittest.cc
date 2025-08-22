e901f712fa8e54b26c5a62cd4612d1e94ea3e8a6
duanshaofei <duanshaofei@huawei-partners.com>
2025-08-22 10:07:45 +0800

    FixReview
    Signed-off-by: duanshaofei <duanshaofei@huawei-partners.com>

--- arkweb/chromium_ext/third_party/blink/renderer/core/editing/selection_controller_for_include_unittest.cc
+++ arkweb/chromium_ext/third_party/blink/renderer/core/editing/selection_controller_for_include_unittest.cc
@@ -28,8 +28,6 @@ class SelectionControllerForIncludeTest : public SelectionControllerTest {
   bool GetMenuShow();
   void SetLongPress(HitTestResult& result);
   void SetLinkPress(HitTestResult& result);
-
-  void TestBody() override {}
 };
 
 void SelectionControllerForIncludeTest::SetStartSelect(bool start_select_) {
