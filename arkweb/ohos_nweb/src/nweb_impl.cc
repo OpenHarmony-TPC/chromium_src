6364ebc00e39be53d10eec4953f22ac706de31bc
SprucEovo <lichenwei2@huawei.com>
2025-08-22 15:41:34 +0800

    修改告警
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- arkweb/ohos_nweb/src/nweb_impl.cc
+++ arkweb/ohos_nweb/src/nweb_impl.cc
@@ -5834,7 +5834,6 @@ void NWebImpl::SetTransformHint(uint32_t rotation) {
 #if BUILDFLAG(ARKWEB_PERFORMANCE_JITTER)
 void NWebImpl::SetPopupSurface(void* popupSurface) {
   uint32_t width, height;
-  output_handler_->GetWindowInfo(width, height);
   if (nweb_delegate_ == nullptr) {
     WVLOG_E("SetPopupSurface failed,nweb_delegate is nullptr.");
     return;
@@ -5844,6 +5843,7 @@ void NWebImpl::SetPopupSurface(void* popupSurface) {
     WVLOG_E("SetPopupSurface failed, NWeb output handler is not ready");
     return;
   }
+  output_handler_->GetWindowInfo(width, height);
   void* popup_window = nullptr;
   popup_window = output_handler_->GetNativeWindowFromSurface(popupSurface);
 
