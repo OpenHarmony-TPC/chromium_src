aa5d8a01a387b94cf2d3d1eb0e4bbb0192d307ed
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-08-22 16:31:18 +0800

    update: 更新文件 stack_trace_ohos.cc
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- arkweb/chromium_ext/base/debug/stack_trace_ohos.cc
+++ arkweb/chromium_ext/base/debug/stack_trace_ohos.cc
@@ -86,6 +86,8 @@ char* itoa_r(intptr_t i, char* buf, size_t sz, int base, size_t padding) {
 
   char* start = buf;
 
+  uintptr_t j = i;
+
   if(i<0) {
     j = static_cast<uintptr_t>(-(i + 1)) + 1;
   }
