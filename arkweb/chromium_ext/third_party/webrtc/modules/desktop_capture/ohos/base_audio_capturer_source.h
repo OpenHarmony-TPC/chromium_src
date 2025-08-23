cf73a475089e05863040998230542a65657cc07a
qq_42700029 <wuqiwen7@huawei.com>
2025-08-23 15:24:10 +0800

    update: 更新文件 base_audio_capturer_source.h
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_audio_capturer_source.h
+++ arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_audio_capturer_source.h
@@ -17,6 +17,7 @@
 #define MEDIA_OHOS_AUDIO_BASE_AUDIO_CAPTURER_SOURCE_H_
 
 #include "base/files/file_util.h"
+#include "base/files/scoped_file.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
@@ -101,7 +102,7 @@ private:
     AudioParameters params_;
     raw_ptr<CaptureCallback> base_callback_ = nullptr;
     base::Lock base_callback_lock_;
-    FILE* dumpFile_ = nullptr;
+    base::ScopedFILE dumpFile_ = nullptr;
 
     bool isStart_ = false;
     bool portal_init_failed_ = false;
