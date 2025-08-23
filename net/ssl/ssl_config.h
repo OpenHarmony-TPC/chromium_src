dda9dd4f397b96b0bdb616aa85e8e71fff83c0c0
huangjunjie31_1 <huangjunjie31@huawei.com>
2025-08-23 19:54:29 +0800

    update: 更新文件 ssl_config.h
    Signed-off-by: huangjunjie31_1 <huangjunjie31@huawei.com>

--- net/ssl/ssl_config.h
+++ net/ssl/ssl_config.h
@@ -78,9 +78,11 @@ struct NET_EXPORT SSLConfig {
   // If true, causes only ECDHE cipher suites to be enabled.
   bool require_ecdhe = false;
 
+#if BUILDFLAG(ARKWEB_SSL_AUTH_ALGO)
   // If true, causes SHA-1 signatures to be rejected from servers during
   // a TLS handshake.
   bool disable_sha1_server_signatures = false;
+#endif
 
   // TODO(wtc): move the following members to a new SSLParams structure.  They
   // are not SSL configuration settings.
