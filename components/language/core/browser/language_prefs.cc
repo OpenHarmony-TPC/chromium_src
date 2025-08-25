3ae28e15c651b518205c170a6b063d10ddccd2b8
zwpless1 <zhaowenpu1@huawei.com>
2025-08-25 19:24:07 +0800

    2
    Signed-off-by: zwpless1 <zhaowenpu1@huawei.com>

--- components/language/core/browser/language_prefs.cc
+++ components/language/core/browser/language_prefs.cc
@@ -165,12 +165,12 @@ bool LanguagePrefs::IsForcedLanguage(const std::string& language) {
 void LanguagePrefs::InitializeSelectedLanguagesPref() {
   // Initializes user-selected languages if they're empty.
   // This is important so that previously saved languages aren't overwritten.
-#if BUILDFLAG(ARKWEB_I18N)
+#if !BUILDFLAG(ARKWEB_I18N)
   if (prefs_->GetString(language::prefs::kSelectedLanguages).empty()) {
 #endif
     prefs_->SetString(language::prefs::kSelectedLanguages,
                       prefs_->GetString(language::prefs::kAcceptLanguages));
-#if BUILDFLAG(ARKWEB_I18N)
+#if !BUILDFLAG(ARKWEB_I18N)
   }
 #endif
 }
