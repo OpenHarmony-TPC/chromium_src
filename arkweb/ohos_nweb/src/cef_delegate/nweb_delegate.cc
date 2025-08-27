ade452fbe6f6cc9828c7a22e997c2b375d206560
hb0908 <hongbin4@huawei.com>
2025-08-27 22:50:37 +0800

    fix: 白屏插帧特性，对请求返回的错误码进行判断校验
    Signed-off-by: hb0908 <hongbin4@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -6000,7 +6000,9 @@ void NWebDelegate::SetBlanklessLoadingKey(uint32_t nweb_id, uint64_t blankless_k
   // before send to render_frame, frame_sink_id will be get from compositor.
   int64_t pref_hash = preference_delegate_ ? preference_delegate_->GetPreferenceHash() : 0;
   browser->GetMainFrame()->AsArkWebFrame()->SendBlanklessKeyToRenderFrame(nweb_id, blankless_key, 0, pref_hash);
-  handler_delegate_->SetBlanklessLoadingKey(blankless_key);
+  if (handler_delegate_) {
+    handler_delegate_->SetBlanklessLoadingKey(blankless_key);
+  }
 }
 
 int64_t NWebDelegate::GetPreferenceHash() {
