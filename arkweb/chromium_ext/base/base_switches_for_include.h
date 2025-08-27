c820549e70dc58769470b7e283924e9255540404
liuzhijie2 <liuzhijie10@huawei.com>
2025-08-27 15:55:35 +0800

    disable autopreconnect
    Signed-off-by: liuzhijie2 <liuzhijie10@huawei.com>

--- arkweb/chromium_ext/base/base_switches_for_include.h
+++ arkweb/chromium_ext/base/base_switches_for_include.h
@@ -38,4 +38,8 @@ extern const char kEnableVideoAssistant[];
 extern const char kEnableLoggerReport[];
 #endif
 
+#if BUILDFLAG(ARKWEB_NO_STATE_PREFETCH)
+extern const char kDisableAutoPreconnect[];
+#endif
+
 #endif  // ARKWEB_CHROMIUM_EXT_BASE_BASE_SWITCHES_FOR_INCLUDE_H_
