e0e82340c9103096d25f841af0f93599ac629ed3
diao-gaoyang <diaogaoyang@huawei.com>
2025-08-28 15:52:18 +0800

    update: 更新文件 nweb_delegate.h 追加
    Signed-off-by: diao-gaoyang <diaogaoyang@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.h
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.h
@@ -437,7 +437,7 @@ class NWebDelegate : public NWebDelegateInterface, public virtual CefRefCount {
 #endif  // BUILDFLAG(ARKWEB_MEDIA_POLICY)
 
   // #if BUILDFLAG(ARKWEB_NO_STATE_PREFETCH)
-  void PrefetchPage(const PrefetchOptions prefetch_options) override;
+  void PrefetchPage(const PrefetchOptions& prefetch_options) override;
   // #endif  // BUILDFLAG(ARKWEB_NO_STATE_PREFETCH)
 
 #if BUILDFLAG(ARKWEB_INPUT_EVENTS)
