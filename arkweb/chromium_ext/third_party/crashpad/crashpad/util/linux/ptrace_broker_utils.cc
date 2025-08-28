3c62f57e49d84038985a505f65132d3b2797dad4
songjie42 <songjie42@huawei.com>
2025-08-28 16:37:18 +0800

    update: 更新文件 ptrace_broker_utils.cc ptraceBroker指针未判空
    Signed-off-by: songjie42 <songjie42@huawei.com>

--- arkweb/chromium_ext/third_party/crashpad/crashpad/util/linux/ptrace_broker_utils.cc
+++ arkweb/chromium_ext/third_party/crashpad/crashpad/util/linux/ptrace_broker_utils.cc
@@ -44,6 +44,9 @@ using namespace crashpad;
 #if BUILDFLAG(ARKWEB_CRASHPAD)
 int PtraceBrokerUtils::ConvertRealtidToNstid(int real_tid, PtraceBroker* ptraceBroker)
 {
+  if (ptraceBroker == nullptr) {
+    return real_tid;
+  }
   if (ptraceBroker->is_in_pid_ns_) {
     auto it = ptraceBroker->tid_nstid_map_.find(real_tid);
     if (it != ptraceBroker->tid_nstid_map_.end()) {
