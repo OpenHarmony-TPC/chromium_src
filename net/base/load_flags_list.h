51655a37b936e7dfa3fdfcdbff87d3982cac127e
diao-gaoyang <diaogaoyang@huawei.com>
2025-08-28 10:28:24 +0800

    prprefetch追加修改
    Signed-off-by: diao-gaoyang <diaogaoyang@huawei.com>

--- net/base/load_flags_list.h
+++ net/base/load_flags_list.h
@@ -119,5 +119,7 @@ LOAD_FLAG(SHOULD_BYPASS_HSTS, 1 << 19)
 // This load will not send any cookies. For CEF usage.
 LOAD_FLAG(DO_NOT_SEND_COOKIES, 1 << 20)
 
+#if BUILDFLAG(ARKWEB_NO_STATE_PREFETCH)
 // This load will ignoring Cache-Control: no-store
 LOAD_FLAG(IGNORE_CACHE_CONTROL, 1 << 21)
+#endif
