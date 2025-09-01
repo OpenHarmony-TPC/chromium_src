530788d3d10fa59a1e62bfb163dcf76bffbfd2ff
yourkindfather <jianghengbo@h-partners.com>
2025-09-01 10:52:50 +0800

    代码review意见整改
    Signed-off-by: yourkindfather <jianghengbo@h-partners.com>

--- arkweb/chromium_ext/net/cert/cert_verify_proc_ohos.cc
+++ arkweb/chromium_ext/net/cert/cert_verify_proc_ohos.cc
@@ -287,8 +287,7 @@ void AddAppCert(const std::string_view& hostname, X509_STORE* ca_store) {
       }
     }
   } else {
-    LOG(ERROR) << "GetTrustAnchorsForHostName host:"
-               << url::LogUtils::ConvertUrlWithMask(host) << " failed.";
+    LOG(ERROR) << "GetTrustAnchorsForHostName host: ***" << " failed.";
   }
 
   return;
@@ -578,7 +577,6 @@ int TryVerifyWithAIAFetching(const std::vector<std::string>& cert_bytes,
   }
 
   NOTREACHED();
-  return X509_V_ERR_UNABLE_TO_GET_ISSUER_CERT_LOCALLY;
 }
 
 void SetCertStatus(int status, CertVerifyResult* verify_result) {
@@ -599,8 +597,6 @@ void SetCertStatus(int status, CertVerifyResult* verify_result) {
         break;
       default:
         NOTREACHED();
-        verify_result->cert_status |= CERT_STATUS_INVALID;
-        break;
   }
 }
 
