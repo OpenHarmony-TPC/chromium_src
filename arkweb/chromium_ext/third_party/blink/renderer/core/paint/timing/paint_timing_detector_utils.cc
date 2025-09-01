5f0b5b25d69e4ff56a266418ba71930b8289e931
some-one-mmx <maominxian1@huawei.com>
2025-09-01 09:55:43 +0800

    fix code check
    Signed-off-by: some-one-mmx <maominxian1@huawei.com>

--- arkweb/chromium_ext/third_party/blink/renderer/core/paint/timing/paint_timing_detector_utils.cc
+++ arkweb/chromium_ext/third_party/blink/renderer/core/paint/timing/paint_timing_detector_utils.cc
@@ -58,8 +58,7 @@ void PaintTimingDetectorUtils::NotifyLcpForBlankless() {
   if (!base::ohos::BlanklessController::CheckGlobalProperty()) {
     return;
   }
-  if (!paint_timing_detector_ || !paint_timing_detector_->frame_view_
-      || !paint_timing_detector_->frame_view_->GetFrame()) {
+  if (!paint_timing_detector_ || !paint_timing_detector_->frame_view_) {
     return;
   }
   Document* document = paint_timing_detector_->frame_view_->GetFrame().GetDocument();
