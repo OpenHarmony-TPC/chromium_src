eda8400f3668c91850ea77dbb315ef527defa8c6
qq_42700029 <wuqiwen7@huawei.com>
2025-09-01 21:12:36 +0800

    add fix
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/ohos_adapter_ndk/ohos_adapter_helper_decouple.cpp
+++ arkweb/ohos_adapter_ndk/ohos_adapter_helper_decouple.cpp
@@ -82,7 +82,6 @@
 #include "arkweb/ohos_adapter_ndk/audio_capturer_adapter/audio_capturer_adapter_impl.h"
 #include "arkweb/ohos_adapter_ndk/screen_capture_adapter/screen_capture_adapter_impl.h"
 #include "arkweb/ohos_adapter_ndk/access_token_adapter/access_token_adapter_impl.h"
-#include "arkweb/ohos_adapter_ndk/media_adapter/player_framework_adapter_impl.h"
 #include "arkweb/ohos_adapter_ndk/graphic_adapter/native_window_adapter_impl.h"
 #include "arkweb/ohos_adapter_ndk/multimodalinputnew_adapter/mmi_new_adapter_impl.h"
 #include "pasteboard_adapter/include/pasteboard_client_adapter_impl.h"
