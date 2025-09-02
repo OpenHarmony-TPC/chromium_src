b4153676f41e6059975a85d93fdcb132a96e093e
lingjinyue <lingyue2@huawei.com>
2025-09-02 16:15:32 +0800

    取消对域名的打印
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- arkweb/chromium_ext/components/subresource_filter/content/browser/ohos_adblock_config.cc
+++ arkweb/chromium_ext/components/subresource_filter/content/browser/ohos_adblock_config.cc
@@ -153,7 +153,6 @@ bool AdBlockConfig::CheckIsInDisallowData(GURL url) {
           net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 
   std::vector<uint8_t> key = ToByteArray(registrable_domain);
-  LOG(DEBUG) << "[Adblock] registrable key:" << url::LogUtils::ConvertUrlWithMask(registrable_domain);
   std::reverse(std::begin(key), std::end(key));
 
   {
@@ -182,8 +181,6 @@ AdBlockConfig::AddAdsBlockDisallowListInternal(
   for (auto& item : disallow_list_) {
     std::string trimed_str;
     base::TrimWhitespaceASCII(item, base::TRIM_ALL, &trimed_str);
-    LOG(DEBUG) << "[Adblock] trimed key:"
-               << url::LogUtils::ConvertUrlWithMask(trimed_str);
     std::vector<uint8_t> key = ToByteArray(trimed_str);
     std::reverse(std::begin(key), std::end(key));
     disallow_data->AddDataForKey(key, trimed_str);
