8359460ec6a44e3e79bd7fba81504955fd8f1643
lingjinyue <lingyue2@huawei.com>
2025-09-02 16:21:45 +0800

    删除url日志
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- arkweb/chromium_ext/components/subresource_filter/content/browser/ohos_adblock_config.cc
+++ arkweb/chromium_ext/components/subresource_filter/content/browser/ohos_adblock_config.cc
@@ -15,7 +15,6 @@
 
 #include "components/subresource_filter/content/browser/ohos_adblock_config.h"
 
-#include "arkweb/chromium_ext/url/ohos/log_utils.h"
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
@@ -153,7 +152,6 @@ bool AdBlockConfig::CheckIsInDisallowData(GURL url) {
           net::registry_controlled_domains::INCLUDE_PRIVATE_REGISTRIES);
 
   std::vector<uint8_t> key = ToByteArray(registrable_domain);
-  LOG(DEBUG) << "[Adblock] registrable key:" << url::LogUtils::ConvertUrlWithMask(registrable_domain);
   std::reverse(std::begin(key), std::end(key));
 
   {
@@ -182,7 +180,6 @@ AdBlockConfig::AddAdsBlockDisallowListInternal(
   for (auto& item : disallow_list_) {
     std::string trimed_str;
     base::TrimWhitespaceASCII(item, base::TRIM_ALL, &trimed_str);
-    LOG(DEBUG) << "[Adblock] trimed key:" << trimed_str;
     std::vector<uint8_t> key = ToByteArray(trimed_str);
     std::reverse(std::begin(key), std::end(key));
     disallow_data->AddDataForKey(key, trimed_str);
