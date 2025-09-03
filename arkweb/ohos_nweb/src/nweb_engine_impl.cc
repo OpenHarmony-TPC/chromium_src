a453dfc03115445e3cbcc7962953fc9626a50f97
quyue00807312 <quyue5@huawei.com>
2025-09-03 20:22:23 +0800

    site isolation arkweb
    Signed-off-by: quyue00807312 <quyue5@huawei.com>

--- arkweb/ohos_nweb/src/nweb_engine_impl.cc
+++ arkweb/ohos_nweb/src/nweb_engine_impl.cc
@@ -182,6 +182,27 @@ void NWebEngineImpl::PrefetchResource(
 #endif
 }
 
+int32_t NWebEngineImpl::SetSiteIsolationMode(SiteIsolationMode mode) {
+#if BUILDFLAG(ARKWEB_SITE_ISOLATION_MODE)
+  LOG(INFO) << "NWebEngineImpl::SetSiteIsolationMode";
+  bool isStrict = (mode == SiteIsolationMode::STRICT)? true:false;
+  return NWebImpl::SetSiteIsolationMode(isStrict);
+#else
+  return 0;
+#endif
+}
+
+SiteIsolationMode NWebEngineImpl::GetSiteIsolationMode() {
+#if BUILDFLAG(ARKWEB_RENDER_PROCESS_MODE)
+  bool resMode = NWebImpl::GetSiteIsolationModeResult();
+  LOG(INFO) << "NWebEngineImpl::GetSiteIsolationMode" << resMode;
+  SiteIsolationMode mode = (resMode == false)? SiteIsolationMode::PARTIAL : SiteIsolationMode::STRICT;
+  return mode;
+#else
+  return SiteIsolationMode::PARTIAL;
+#endif
+}
+
 void NWebEngineImpl::SetRenderProcessMode(RenderProcessMode mode) {
 #if BUILDFLAG(ARKWEB_RENDER_PROCESS_MODE)
   NWebImpl::SetRenderProcessMode(mode);
