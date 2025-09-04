9b6ae64d76d36f827af603d033c1d1610fdd14cc
hoo334 <liminghu1@huawei.com>
2025-09-04 11:20:00 +0800

    webrtc流类型修改
    Signed-off-by: hoo334 <liminghu1@huawei.com>

--- arkweb/chromium_ext/media/audio/ohos/ohos_audio_output_stream.cc
+++ arkweb/chromium_ext/media/audio/ohos/ohos_audio_output_stream.cc
@@ -383,7 +383,7 @@ bool OHOSAudioOutputStream::InitRender() {
 
 void OHOSAudioOutputStream::SetStreamUsage() {
   if (isCommunication_) {
-    OH_AudioStreamBuilder_SetRendererInfo(audio_stream_builder_, AUDIOSTREAM_USAGE_VOICE_COMMUNICATION);
+    OH_AudioStreamBuilder_SetRendererInfo(audio_stream_builder_, AUDIOSTREAM_USAGE_VIDEO_COMMUNICATION);
     return;
   }
  
