52a4948a48433264feb9bbe665af1f1a0a121cd7
uuz <liuzihao45@huawei.com>
2025-09-04 16:16:27 +0800

    fix opendownload crash
    Signed-off-by: uuz <liuzihao45@huawei.com>
    Change-Id: I89178be9e030c6c5bd6357c98601d5edd95ac1b7

--- arkweb/chromium_ext/chrome/browser/chrome_content_browser_client_for_include.cc
+++ arkweb/chromium_ext/chrome/browser/chrome_content_browser_client_for_include.cc
@@ -143,12 +143,17 @@ class ChromeContentBrowserClientUtils {
       WebContents* web_contents,
       raw_ptr<ChromeContentBrowserClient> obj) {
     auto rvh = web_contents->GetRenderViewHost();
-    CefRenderWidgetHostViewOSR* rwhvb =
-        static_cast<CefRenderWidgetHostViewOSR*>(rvh->GetWidget()->GetView());
+    auto rwhv_base =
+        static_cast<content::RenderWidgetHostViewBase*>(rvh->GetWidget()->GetView());
     CefRefPtr<CefBrowserHostBase> browser_host =
         CefBrowserHostBase::GetBrowserForHost(rvh);
+    if (rwhv_base && rwhv_base->GetViewType().empty()) {
+      LOG(ERROR) << "GetViewType is empty, access wrong RenderWidgetHostView";
+      return browser_host;
+    }
+    auto rwhvb = static_cast<CefRenderWidgetHostViewOSR*>(rwhv_base);
     if (rwhvb && rwhvb->IsRenderWidgetHostViewChildFrame() && browser_host) {
-      return;
+      return browser_host;
     }
 
     if (rwhvb && browser_host && rwhvb->AsArkWebRenderWidgetHostViewOSRExt()) {
