7826060e13f53356337344a83885c4afd3496318
whuqincheng <liweiyang1@huawei.com>
2025-09-09 19:49:19 +0800

    NDK Callback
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- arkweb/ohos_adapter_ndk/media_adapter/media_codec_decoder_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/media_adapter/media_codec_decoder_adapter_impl.cpp
@@ -309,11 +309,11 @@ DecoderAdapterCode MediaCodecDecoderAdapterImpl::ReleaseDecoder()
         return DecoderAdapterCode::DECODER_ERROR;
     }
     OH_AVErrCode ret = OH_VideoDecoder_Destroy(decoder_);
-    decoder_ = nullptr;
     if (ret != OH_AVErrCode::AV_ERR_OK) {
         WVLOG_E("MediaCodecDecoder OH_VideoDecoder_Destroy fail, ret=%{public}u.", static_cast<uint32_t>(ret));
         return DecoderAdapterCode::DECODER_ERROR;
     }
+    decoder_ = nullptr;
     std::unique_lock<std::mutex> lock(bufferMutex_);
     bufferMap_.clear();
     return DecoderAdapterCode::DECODER_OK;
