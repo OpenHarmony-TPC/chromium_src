dee535f4f46a51305adf80e6f2bd2376f0cbed52
qq_42700029 <wuqiwen7@huawei.com>
2025-09-11 21:39:05 +0800

    update: 更新文件 base_screen_capture_source_bridge.cc
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_screen_capture_source_bridge.cc
+++ arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_screen_capture_source_bridge.cc
@@ -110,16 +110,6 @@ class OHOSScreenCaptureCallback
       base::BindOnce(&BaseScreenCaptureSource::SetScreenCaptureState, capturer_, stateCode, nweb_id));
   }
 
-  void OnDisplaySelectedV2(uint64_t displayId, int32_t nweb_id) override {
-    if (!BaseScreenCaptureSource::GetInstance().WindowCallbackIsExist(nweb_id) ||
-      BaseScreenCaptureSource::GetInstance().window_callback_map_[nweb_id] == nullptr) {
-      LOG(ERROR) << "BaseScreenCaptureSource window callback is nullptr";
-      return;
-    }
-    
-    BaseScreenCaptureSource::GetInstance().OnDisplaySelectCallback(displayId);
-  }
-
   void OnAudioBufferAvailable(bool isReady, OHOS::NWeb::AudioCaptureSourceTypeAdapter type) override {}
   void OnVideoBufferAvailable(bool isReadye) override {}
   void OnStateChange(OHOS::NWeb::ScreenCaptureStateCodeAdapter stateCode) override {}
@@ -167,12 +157,6 @@ class OHOSScreenCaptureCallback
 
   bool BaseScreenCaptureSource::SetScreenCaptureConfig(int nweb_id)
   {
-      LOG(INFO) << __FUNCTION__ << " enter ";
-      if (screen_capture_adapter_map_.find(nweb_id) != screen_capture_adapter_map_.end()) {
-          LOG(INFO) <<"nweb_id_ =" << nweb_id << ",already exist";
-          screen_capture_adapter_map_[nweb_id]->ClearBufferQueue(nweb_id);
-          return true;
-      }
       SetScreenCaptureState(ScreenCaptureStateCodeAdapter::SCREEN_CAPTURE_STATE_INVLID, nweb_id);
       main_task_runner_ = base::SingleThreadTaskRunner::GetCurrentDefault();
       if (!main_task_runner_) {
@@ -367,13 +351,6 @@ class OHOSScreenCaptureCallback
         LOG(ERROR) << "BaseScreenCaptureSource::StartCapture, screen capture is nullptr";
         return -1;
       }
-#if BUILDFLAG(ARKWEB_ARKWEB_EXTENSIONS)
-      if (capture_state_code_map_[nweb_id] ==
-          OHOS::NWeb::ScreenCaptureStateCodeAdapter::
-              SCREEN_CAPTURE_STATE_STARTED) {
-      return 0;
-      }
-#endif
       ret = screen_capture->second->StartCapture();
     }
     SetScreenCaptureState(ScreenCaptureStateCodeAdapter::SCREEN_CAPTURE_STATE_STARTED, nweb_id);
@@ -454,33 +431,5 @@ class OHOSScreenCaptureCallback
       return true;
     }
     return false;
-  }
-
-  void BaseScreenCaptureSource::OnDisplaySelectCallback(uint64_t displayId) {
-    std::lock_guard<std::mutex> lock(displaySelectedCallbackMutex_);
-    LOG(INFO) << __FUNCTION__ << " enter ";
-    if (displaySelectedCallback_.is_null() || main_task_runner_ == nullptr) {
-      LOG(ERROR) << "displaySelectedCallback_.is_null() || "
-                    "main_task_runner_ == nullptr";
-      return;
-    }
-
-    main_task_runner_->PostTask(
-        FROM_HERE,
-        base::BindOnce([](base::OnceCallback<void(uint64_t displayId)> cb,
-                          uint64_t id) { std::move(cb).Run(id); },
-                       std::move(displaySelectedCallback_), displayId));
-  }
-
-  void BaseScreenCaptureSource::SetDisplaySelectCallback(
-      base::OnceCallback<void(uint64_t displayId)> callback) {
-    std::lock_guard<std::mutex> lock(displaySelectedCallbackMutex_);
-    LOG(INFO) << "SetDisplaySelectCallback is called";
-    if (!callback) {
-      LOG(ERROR) << "BaseScreenCaptureSource::SetDisplaySelectCallback nullptr";
-      return;
-    }
-
-    displaySelectedCallback_ = std::move(callback);
   }
 } // webrtc
