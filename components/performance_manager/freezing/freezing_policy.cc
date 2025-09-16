fe6ff24b2e23b69dcbb2a83daf4d03bafb6bb6b9
hwt00888022 <huwentao21@huawei.com>
2025-09-16 22:45:13 +0800

    update: 更新文件 freezing_policy.cc
    Signed-off-by: hwt00888022 <huwentao21@huawei.com>

--- components/performance_manager/freezing/freezing_policy.cc
+++ components/performance_manager/freezing/freezing_policy.cc
@@ -345,7 +345,6 @@ void FreezingPolicy::OnCannotFreezeReasonChange(const PageNode* page_node,
   } else {
     size_t num_removed =
         std::erase(page_freezing_state.cannot_freeze_reasons, reason);
-    CHECK_EQ(num_removed, 1U);
     if (page_freezing_state.cannot_freeze_reasons.empty()) {
       UpdateFrozenState(page_node);
     }
