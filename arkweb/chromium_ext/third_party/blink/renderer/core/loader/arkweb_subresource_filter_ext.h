3e097a45e6c37afc93b3a1b05f42ec2be9bee5f9
ICEMAGE117 <lifeng262@huawei.com>
2025-09-17 15:17:01 +0800

    回退Persistent
    Signed-off-by: ICEMAGE117 <lifeng262@huawei.com>

--- arkweb/chromium_ext/third_party/blink/renderer/core/loader/arkweb_subresource_filter_ext.h
+++ arkweb/chromium_ext/third_party/blink/renderer/core/loader/arkweb_subresource_filter_ext.h
@@ -29,7 +29,7 @@ class SubresourceFilter;
 
 class CORE_EXPORT ArkWebSubresourceFilterExt {
  public:
-  Persistent<SubresourceFilter> subresourceFilter;
+  SubresourceFilter* subresourceFilter;
   ArkWebSubresourceFilterExt(SubresourceFilter *filter);
   ~ArkWebSubresourceFilterExt();
 
