952a724581c7f95ef8a2d272f52925ee2bc62d22
lizhengkun3 <lizhengkun3@h-partners.com>
2025-09-17 19:48:21 +0800

    modify RunCallbackIfExistsCallback  call status
    Signed-off-by: lizhengkun3 <lizhengkun3@h-partners.com>

--- arkweb/chromium_ext/components/download/internal/common/arkweb_download_item_impl_ext.h
+++ arkweb/chromium_ext/components/download/internal/common/arkweb_download_item_impl_ext.h
@@ -142,6 +142,7 @@ class COMPONENTS_DOWNLOAD_EXPORT ArkWebDownloadItemImplExt : public DownloadItem
   ArkWebDownloadItemImplExt& operator=(const ArkWebDownloadItemImplExt&) = delete;
 
 #if BUILDFLAG(ARKWEB_EXT_DOWNLOAD)
+  ~ArkWebDownloadItemImplExt() override;
   void ReadDownloadData(
       const std::string& guid,
       const int32_t read_size,
