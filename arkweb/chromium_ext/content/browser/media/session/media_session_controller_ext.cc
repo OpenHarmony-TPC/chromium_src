4d28322a383165532b74ebda85b99306a7d222b2
qq_42700029 <wuqiwen7@huawei.com>
2025-09-17 11:10:19 +0800

    update: 更新文件 media_session_controller_ext.cc
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/chromium_ext/content/browser/media/session/media_session_controller_ext.cc
+++ arkweb/chromium_ext/content/browser/media/session/media_session_controller_ext.cc
@@ -47,14 +47,14 @@ void MediaSessionControllerExt::SetSessionStateIfNeed(bool isNeedMediaSession)
   }
 }
 
-void MediaSessionControllerExt::HasWebrtcOneShotPlayers(
+void MediaSessionControllerExt::HasOneShotPlayersWhenSetMetadata(
     media::MediaContentType media_content_type) {
   if (!media_session_) {
     return;
   }
   if (media_content_type == media::MediaContentType::kOneShot) {
     LOG(INFO) << "MediaSessionController contentType is oneShot, has webrtc oneShot players";
-    media_session_->has_webrtc_one_shot_players_ = true;
+    media_session_->has_one_shot_players_ = true;
   }
 }
 #endif  // BUILDFLAG(ARKWEB_MEDIA_POLICY)
