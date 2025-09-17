0e1f4de325300b5a106a848b254697f0fee42adf
qq_42700029 <wuqiwen7@huawei.com>
2025-09-17 09:16:04 +0800

    update: 更新文件 media_session_controller_ext.cc
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/chromium_ext/content/browser/media/session/media_session_controller_ext.cc
+++ arkweb/chromium_ext/content/browser/media/session/media_session_controller_ext.cc
@@ -49,10 +49,10 @@ void MediaSessionControllerExt::SetSessionStateIfNeed(bool isNeedMediaSession)
 #endif  // BUILDFLAG(ARKWEB_MEDIA_POLICY)
 
 void MediaSessionControllerExt::HasWebrtcOneShotPlayers(
-    bool has_audio, bool has_video, media::MediaContentType media_content_type) {
+    media::MediaContentType media_content_type) {
   if (!media_session_)
     return;
-  if (media_content_type_ == media::MediaContentType::kOneShot) {
+  if (media_content_type == media::MediaContentType::kOneShot) {
     LOG(INFO) << "MediaSessionController contentType is oneShot, has webrtc oneShot players";
     media_session_->has_webrtc_one_shot_players_ = true;
   }
