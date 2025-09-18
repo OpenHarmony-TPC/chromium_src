67b9ead01f9965a7bfbb2f5f5624083408885851
qq_54685442 <3431282879@qq.com>
2025-09-18 16:08:21 +0800

    update: 更新文件 res_request_info_cache_mgr_unittest.cc
    Signed-off-by: qq_54685442 <3431282879@qq.com>

--- arkweb/chromium_ext/services/network/prp_preload/tests/res_request_info_cache_mgr_unittest.cc
+++ arkweb/chromium_ext/services/network/prp_preload/tests/res_request_info_cache_mgr_unittest.cc
@@ -123,6 +123,8 @@ TEST_F(ResRequestInfoCacheMgrTest,ResRequestInfoCacheMgrTest_004)
   });
 
   ResReqInfoCacheMgr cache("www.test.com",networkAnonymizationKey,disk_cache_backend_factory,info_cache_cb);
+  cache.SetPageOrigin("test");
+  EXPECT_NE(cache.page_origin_, "test");
   cache.OnEntryLoadedCallback("");
   cache.is_start_ = true;
   cache.OnEntryLoadedCallback("");
@@ -214,6 +216,8 @@ TEST_F(ResRequestInfoCacheMgrTest,JsonToResReqPreloadInfoListTest)
   });
 
   ResReqInfoCacheMgr cache("www.test.com",networkAnonymizationKey,disk_cache_backend_factory,info_cache_cb);
+  cache.SetPageOrigin("test");
+  EXPECT_NE(cache.page_origin_, "test");
   cache.is_start_=true;
   cache.OnEntryLoadedCallback("test");
 
@@ -253,6 +257,8 @@ TEST_F(ResRequestInfoCacheMgrTest,JsonToResReqPreloadInfoListTest_002)
   });
 
   ResReqInfoCacheMgr cache("www.test.com",networkAnonymizationKey,disk_cache_backend_factory,info_cache_cb);
+  cache.SetPageOrigin("test");
+  EXPECT_NE(cache.page_origin_, "test");
   cache.is_start_=true;
   std::string content4 = R"json([
     {
@@ -293,6 +299,8 @@ TEST_F(ResRequestInfoCacheMgrTest,JsonToResReqPreloadInfoListTest_003)
   });
 
   ResReqInfoCacheMgr cache("www.test.com",networkAnonymizationKey,disk_cache_backend_factory,info_cache_cb);
+  cache.SetPageOrigin("test");
+  EXPECT_NE(cache.page_origin_, "test");
   cache.is_start_=true;
   std::string content3 = R"json([
     {
@@ -324,6 +332,7 @@ TEST_F(ResRequestInfoCacheMgrTest,ResReqPreloadInfoListToJsonTest)
 
   ResReqInfoCacheMgr cache("www.test.com",networkAnonymizationKey,disk_cache_backend_factory,info_cache_cb);
   cache.is_start_=true;
+  EXPECT_EQ(cache.is_start_,true);
   cache.page_origin_ = "https://example.com";
   GURL request_url1("https://example.com/path/to/resource1");
   std::shared_ptr<PRRequestInfo> request_info1 = std::make_shared<PRRequestInfo>(request_url1, false);
@@ -358,7 +367,8 @@ TEST_F(ResRequestInfoCacheMgrTest,ResRequestInfoCacheMgrTest_005)
   });
 
   auto mgr = std::make_unique<ResReqInfoCacheMgr>("",networkAnonymizationKey,factory, info_cache_cb);
-
+  mgr->is_start_ = true;
+  EXPECT_EQ(mgr->is_start_,true);
 }
 } // namespace ohos_prp_preload
 
