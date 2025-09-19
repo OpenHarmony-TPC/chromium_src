08fec06de3052d31acf015ff1c2c71f794568c24
li_wenpeng <liwenpeng25@huawei-partners.com>
2025-09-19 08:13:17 +0000

    Unittest_2025_09_10
    Issue:  https://gitcode.com/openharmony-tpc/chromium_src/issues/3949
    Signed-off-by: li_wenpeng <liwenpeng25@huawei-partners.com>

--- arkweb/ohos_adapter_ndk/multimodalinputnew_adapter/mmi_new_adapter_impl_unittest.cc
+++ arkweb/ohos_adapter_ndk/multimodalinputnew_adapter/mmi_new_adapter_impl_unittest.cc
@@ -15,10 +15,12 @@
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "arkweb/ohos_adapter_ndk/mock_ndk_api/include/mock_ndk_api.h"
 #include "arkweb/ohos_adapter_ndk/multimodalinputnew_adapter/mmi_new_adapter_impl.h"
 
 using namespace testing;
 using namespace OHOS::NWeb;
+using namespace MockNdkApi;
 
 class MMINewAdapterImplTest : public ::testing::Test {};
 
@@ -26,6 +28,7 @@ namespace OHOS::NWeb {
 void OnDeviceAdded(int32_t deviceId);
 void OnDeviceRemoved(int32_t deviceId);
 }
+
 class MockMMIListenerAdapter : public MMIListenerAdapter {
  public:
   MOCK_METHOD(void, OnDeviceAdded, (int32_t deviceId, const std::string& type), (override));
@@ -55,9 +58,8 @@ class MockMMIDeviceInfoAdapter : public MMIDeviceInfoAdapter {
 
 /**
  * @tc.name: MMINewAdapterImplTest_OnDeviceAdded_001.
- * @tc.desc: test of OnDeviceAdded in MMINewAdapterImplTest 
+ * @tc.desc: test of OnDeviceAdded in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_OnDeviceAdded_001) {
   std::string type;
@@ -68,14 +70,13 @@ TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_OnDeviceAdded_001) {
   EXPECT_CALL(*listener, OnDeviceAdded(::testing::_, ::testing::_))
       .WillRepeatedly(::testing::Assign(&callback_called, true));
   OnDeviceAdded(deviceId);
-  EXPECT_EQ(callback_called, true); 
+  EXPECT_EQ(callback_called, true);
 }
 
 /**
  * @tc.name: MMINewAdapterImplTest_OnDeviceRemoved_001.
- * @tc.desc: test of OnDeviceRemoved in MMINewAdapterImplTest 
+ * @tc.desc: test of OnDeviceRemoved in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_OnDeviceRemoved_001) {
   std::string type;
@@ -86,14 +87,13 @@ TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_OnDeviceRemoved_001) {
   EXPECT_CALL(*listener, OnDeviceRemoved(::testing::_, ::testing::_))
       .WillRepeatedly(::testing::Assign(&callback_called, true));
   OnDeviceRemoved(deviceId);
-  EXPECT_EQ(callback_called, true); 
+  EXPECT_EQ(callback_called, true);
 }
 
 /**
  * @tc.name: MMINewAdapterImplTest_RegisterDevListener_001.
- * @tc.desc: test of RegisterDevListener in MMINewAdapterImplTest 
+ * @tc.desc: test of RegisterDevListener in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_RegisterDevListener_001) {
   std::string type;
@@ -108,9 +108,8 @@ TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_RegisterDevListener_001) {
 
 /**
  * @tc.name: MMINewAdapterImplTest_UnregisterDevListener_001.
- * @tc.desc: test of UnregisterDevListener in MMINewAdapterImplTest 
+ * @tc.desc: test of UnregisterDevListener in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_UnregisterDevListener_001) {
   std::string type = MMINewAdapterImpl::GetInstance().GetType();
@@ -124,21 +123,33 @@ TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_UnregisterDevListener_001) {
 
 /**
  * @tc.name: MMINewAdapterImplTest_GetDeviceIds_001.
- * @tc.desc: test of GetDeviceIds in MMINewAdapterImplTest 
+ * @tc.desc: test of GetDeviceIds in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_GetDeviceIds_001) {
   std::vector<int32_t> ids;
+
+  g_mock_OH_Input_GetDeviceIds = [](int32_t *deviceIds, int32_t inSize, int32_t *outSize) {
+    return __real_OH_Input_GetDeviceIds(deviceIds, -1, outSize);
+  };
   auto result = MMINewAdapterImpl::GetInstance().GetDeviceIds(ids);
+  EXPECT_EQ(result, -1);
+
+  g_mock_OH_Input_GetDeviceIds = [](int32_t *deviceIds, int32_t inSize, int32_t *outSize) {
+    return __real_OH_Input_GetDeviceIds(deviceIds, 0, outSize);
+  };
+  result = MMINewAdapterImpl::GetInstance().GetDeviceIds(ids);
+  EXPECT_EQ(result, 0);
+
+  g_mock_OH_Input_GetDeviceIds = nullptr;
+  result = MMINewAdapterImpl::GetInstance().GetDeviceIds(ids);
   EXPECT_EQ(result, 0);
 }
 
 /**
  * @tc.name: MMINewAdapterImplTest_GetDeviceInfo_001.
- * @tc.desc: test of GetDeviceInfo in MMINewAdapterImplTest 
+ * @tc.desc: test of GetDeviceInfo in MMINewAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_GetDeviceInfo_001) {
   int32_t deviceId = 0;
@@ -147,6 +158,13 @@ TEST_F(MMINewAdapterImplTest, MMINewAdapterImplTest_GetDeviceInfo_001) {
   EXPECT_EQ(result, -1);
 
   info = std::make_shared<testing::NiceMock<MockMMIDeviceInfoAdapter>>();
+  g_mock_OH_Input_CreateDeviceInfo = []() {
+    return nullptr;
+  };
+  result = MMINewAdapterImpl::GetInstance().GetDeviceInfo(deviceId, info);
+  EXPECT_EQ(result, -1);
+
+  g_mock_OH_Input_CreateDeviceInfo = nullptr;
   result = MMINewAdapterImpl::GetInstance().GetDeviceInfo(deviceId, info);
   EXPECT_EQ(result, 0);
 }
--- arkweb/ohos_adapter_ndk/net_connect_adapter/net_connect_adapter_impl_unittest.cc
+++ arkweb/ohos_adapter_ndk/net_connect_adapter/net_connect_adapter_impl_unittest.cc
@@ -15,12 +15,14 @@
 
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "arkweb/ohos_adapter_ndk/mock_ndk_api/include/mock_ndk_api.h"
 #define private public
 #include "arkweb/ohos_adapter_ndk/net_connect_adapter/net_connect_adapter_impl.h"
 #undef private
 
 using namespace testing;
 using namespace OHOS::NWeb;
+using namespace MockNdkApi;
 
 class NetConnectAdapterImplTest : public ::testing::Test {};
 
@@ -30,7 +32,6 @@ class MockNetConnectAdapterImpl : public NetConnectAdapterImpl {
   MOCK_METHOD(void, RegisterVpnListener, (std::shared_ptr<VpnListener>), (override));
   MOCK_METHOD(void, UnRegisterVpnListener, (), (override));
 };
-
 
 class MockNetConnCallback : public NetConnCallback {
  public:
@@ -47,7 +48,6 @@ class MockNetConnCallback : public NetConnCallback {
  * @tc.name: NetConnectAdapterImplTest_NetAvailable_001.
  * @tc.desc: test of NetAvailable in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetAvailable_001) {
   auto cb = std::make_shared<MockNetConnCallback>();
@@ -63,7 +63,6 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetAvailable_001) {
  * @tc.name: NetConnectAdapterImplTest_NetCapabilitiesChange_001.
  * @tc.desc: test of NetCapabilitiesChange in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetCapabilitiesChange_001) {
   std::shared_ptr<MockNetConnCallback> cb = nullptr;
@@ -78,15 +77,22 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetCapabilitiesChang
   EXPECT_CALL(*cb, OnNetCapabilitiesChanged(::testing::_))
       .Times(1)
       .WillOnce(Return(-1));
+  g_mock_OH_Telephony_GetDefaultCellularDataSlotId = []() {;
+    return -1;
+  };
+  g_mock_OH_Telephony_GetNetworkStateForSlot = [](int32_t, Telephony_NetworkState *) {
+    return TEL_RADIO_SUCCESS;
+  };
   result = NetConnectAdapterImpl::NetCapabilitiesChange(cb, &netHandle, &netCapabilities);
   EXPECT_EQ(result, -1);
+  g_mock_OH_Telephony_GetDefaultCellularDataSlotId = nullptr;
+  g_mock_OH_Telephony_GetNetworkStateForSlot = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_NetConnectionPropertiesChange_001.
  * @tc.desc: test of NetConnectionPropertiesChange in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetConnectionPropertiesChange_001) {
   std::shared_ptr<MockNetConnCallback> cb = nullptr;
@@ -104,10 +110,76 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_NetConnectionPropert
 }
 
 /**
+ * @tc.name: NetConnectAdapterImplTest_InitNetConnCallback_001.
+ * @tc.desc: test of InitNetConnCallback in NetConnectAdapterImplTest
+ * @tc.type: FUNC.
+ */
+TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_InitNetConnCallback_001) {
+  MockNetConnectAdapterImpl net_connect_adapter_impl;
+  auto cb = std::make_shared<MockNetConnCallback>();
+  g_mock_OH_NetConn_RegisterDefaultNetConnCallback = [](NetConn_NetConnCallback *, uint32_t *callbackId) {
+    *callbackId = 0;
+    return 0;
+  };
+  net_connect_adapter_impl.RegisterNetConnCallback(cb);
+  g_mock_OH_NetConn_RegisterDefaultNetConnCallback = nullptr;
+  NetConn_NetConnCallback netConnCallback;
+  NetConnectAdapterImpl::InitNetConnCallback(&netConnCallback);
+  NetConn_NetHandle netHandle;
+  bool callback_called = false;
+  EXPECT_CALL(*cb, NetAvailable()).WillRepeatedly(::testing::Invoke([&callback_called]() {
+    callback_called = true;
+    return 0;
+  }));
+  netConnCallback.onNetworkAvailable(nullptr);
+  EXPECT_EQ(callback_called, false);
+  netConnCallback.onNetworkAvailable(&netHandle);
+  EXPECT_EQ(callback_called, true);
+
+  NetConn_NetCapabilities netCap;
+  callback_called = false;
+  EXPECT_CALL(*cb, OnNetCapabilitiesChanged(::testing::_)).WillRepeatedly(::testing::Invoke([&callback_called]() {
+    callback_called = true;
+    return 0;
+  }));
+  netConnCallback.onNetCapabilitiesChange(nullptr, nullptr);
+  EXPECT_EQ(callback_called, false);
+  netConnCallback.onNetCapabilitiesChange(&netHandle, &netCap);
+  EXPECT_EQ(callback_called, false);
+
+  NetConn_ConnectionProperties connProp;
+  callback_called = false;
+  EXPECT_CALL(*cb, OnNetConnectionPropertiesChanged(::testing::_)).
+      WillRepeatedly(::testing::Invoke([&callback_called]() {
+    callback_called = true;
+    return 0;
+  }));
+  netConnCallback.onConnetionProperties(nullptr, nullptr);
+  EXPECT_EQ(callback_called, false);
+  netConnCallback.onConnetionProperties(&netHandle, &connProp);
+  EXPECT_EQ(callback_called, true);
+
+  callback_called = false;
+  EXPECT_CALL(*cb, NetUnavailable()).WillRepeatedly(::testing::Invoke([&callback_called]() {
+    callback_called = true;
+    return 0;
+  }));
+  netConnCallback.onNetBlockStatusChange(&netHandle, false);
+  EXPECT_EQ(callback_called, false);
+
+  callback_called = false;
+  netConnCallback.onNetLost(&netHandle);
+  EXPECT_EQ(callback_called, true);
+
+  callback_called = false;
+  netConnCallback.onNetUnavailable();
+  EXPECT_EQ(callback_called, true);
+}
+
+/**
  * @tc.name: NetConnectAdapterImplTest_RegisterNetConnCallback_001.
  * @tc.desc: test of RegisterNetConnCallback in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_RegisterNetConnCallback_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
@@ -118,13 +190,20 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_RegisterNetConnCallb
   cb = std::make_shared<MockNetConnCallback>();
   result = net_connect_adapter_impl.RegisterNetConnCallback(cb);
   EXPECT_EQ(result, -1);
+
+  g_mock_OH_NetConn_RegisterDefaultNetConnCallback = [](NetConn_NetConnCallback *, uint32_t *callbackId) {
+    *callbackId = 0;
+    return 0;
+  };
+  result = net_connect_adapter_impl.RegisterNetConnCallback(cb);
+  EXPECT_EQ(result, 0);
+  g_mock_OH_NetConn_RegisterDefaultNetConnCallback = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_UnregisterNetConnCallback_001.
  * @tc.desc: test of UnregisterNetConnCallback in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_UnregisterNetConnCallback_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
@@ -136,52 +215,89 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_UnregisterNetConnCal
   NetConnectAdapterImpl::netConnCallbackMap_[id] = std::make_shared<MockNetConnCallback>();
   result = net_connect_adapter_impl.UnregisterNetConnCallback(id);
   EXPECT_EQ(result, -1);
+
+  g_mock_OH_NetConn_UnregisterNetConnCallback = [](uint32_t) {
+    return 0;
+  };
+  result = net_connect_adapter_impl.UnregisterNetConnCallback(id);
+  EXPECT_EQ(result, 0);
+  g_mock_OH_NetConn_UnregisterNetConnCallback = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_GetDefaultNetConnect_001.
  * @tc.desc: test of GetDefaultNetConnect in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_GetDefaultNetConnect_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
   NetConnectType type;
   NetConnectSubtype subtype;
-  auto result = net_connect_adapter_impl.GetDefaultNetConnect(type, subtype);
-  EXPECT_EQ(result, -1);
+  EXPECT_EQ(net_connect_adapter_impl.GetDefaultNetConnect(type, subtype), -1);
+
+  g_mock_OH_NetConn_GetDefaultNet = [](NetConn_NetHandle *) {
+    return 0;
+  };
+  EXPECT_EQ(net_connect_adapter_impl.GetDefaultNetConnect(type, subtype), -1);
+
+  g_mock_OH_NetConn_GetNetCapabilities = [](NetConn_NetHandle *, NetConn_NetCapabilities *netCapabilities) {
+    netCapabilities->bearerTypes[0] = NETCONN_BEARER_CELLULAR;
+    return 0;
+  };
+  g_mock_OH_Telephony_GetDefaultCellularDataSlotId = []() {;
+    return -1;
+  };
+  g_mock_OH_Telephony_GetNetworkStateForSlot = [](int32_t, Telephony_NetworkState *) {
+    return TEL_RADIO_SUCCESS;
+  };
+  EXPECT_EQ(net_connect_adapter_impl.GetDefaultNetConnect(type, subtype), 0);
+  g_mock_OH_Telephony_GetNetworkStateForSlot = nullptr;
+  g_mock_OH_Telephony_GetDefaultCellularDataSlotId = nullptr;
+  g_mock_OH_NetConn_GetNetCapabilities = nullptr;
+  g_mock_OH_NetConn_GetDefaultNet = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_GetDnsServersInternal_001.
  * @tc.desc: test of GetDnsServersInternal in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_GetDnsServersInternal_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
   NetConn_NetHandle netHandle;
   auto result = net_connect_adapter_impl.GetDnsServersInternal(netHandle);
   EXPECT_EQ(result.empty(), true);
+
+  g_mock_OH_NetConn_GetConnectionProperties = [](NetConn_NetHandle *, NetConn_ConnectionProperties *) {
+    return 0;
+  };
+  result = net_connect_adapter_impl.GetDnsServersInternal(netHandle);
+  EXPECT_EQ(result.empty(), true);
+  g_mock_OH_NetConn_GetConnectionProperties = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_GetDnsServers_001.
  * @tc.desc: test of GetDnsServers in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_GetDnsServers_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
   auto result = net_connect_adapter_impl.GetDnsServers();
   EXPECT_EQ(result.empty(), true);
+
+  g_mock_OH_NetConn_GetDefaultNet = [](NetConn_NetHandle *) {
+    return 0;
+  };
+  result = net_connect_adapter_impl.GetDnsServers();
+  EXPECT_EQ(result.empty(), true);
+  g_mock_OH_NetConn_GetDefaultNet = nullptr;
 }
 
 /**
  * @tc.name: NetConnectAdapterImplTest_GetDnsServersByNetId_001.
  * @tc.desc: test of GetDnsServersByNetId in NetConnectAdapterImplTest
  * @tc.type: FUNC.
- * @tc.require:
  */
 TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_GetDnsServersByNetId_001) {
   MockNetConnectAdapterImpl net_connect_adapter_impl;
@@ -192,4 +308,19 @@ TEST_F(NetConnectAdapterImplTest, NetConnectAdapterImplTest_GetDnsServersByNetId
   netId = 0;
   result = net_connect_adapter_impl.GetDnsServersByNetId(netId);
   EXPECT_EQ(result.empty(), true);
+
+  g_mock_OH_NetConn_GetAllNets = [](NetConn_NetHandleList *netHandleList) {
+    return 0;
+  };
+  result = net_connect_adapter_impl.GetDnsServersByNetId(netId);
+  EXPECT_EQ(result.empty(), true);
+
+  g_mock_OH_NetConn_GetAllNets = [](NetConn_NetHandleList *netHandleList) {
+    netHandleList->netHandleListSize = 1;
+    netHandleList->netHandles[0].netId = 0;
+    return 0;
+  };
+  result = net_connect_adapter_impl.GetDnsServersByNetId(netId);
+  EXPECT_EQ(result.empty(), true);
+  g_mock_OH_NetConn_GetAllNets = nullptr;
 }
