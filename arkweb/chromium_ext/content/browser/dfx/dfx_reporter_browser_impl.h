34be03bcd421fe4f0450ed4cbf09cfb0e242b720
qq_44167590 <wangdong344@huawei.com>
2025-09-19 11:52:46 +0800

    fix bug6.0-release
    Signed-off-by: qq_44167590 <wangdong344@huawei.com>

--- arkweb/chromium_ext/content/browser/dfx/dfx_reporter_browser_impl.h
+++ arkweb/chromium_ext/content/browser/dfx/dfx_reporter_browser_impl.h
@@ -37,4 +37,12 @@ public:
   void ReportHiSysEvent(const std::string& eventName, const std::string& sysEventInfoJson) override;
 };
 
+class FreezeReporterImpl : public dfx::mojom::FreezeReporter {
+public:
+  FreezeReporterImpl() {}
+  ~FreezeReporterImpl() override {}
+  static void ProcessPendingReceiver(mojo::PendingReceiver<dfx::mojom::FreezeReporter> receiver);
+  void ReportRenderFreeze(const std::string& eventInfo) override;
+};
+
 #endif // BROWSER_PERFORMANCE_DFX_REPORTER_IMPL_H_
