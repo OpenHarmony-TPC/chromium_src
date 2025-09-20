35a497fb682d8612781f7be5785724cdb49074a0
slimshady999z <zouzhiwei2@h-partners.com>
2025-09-20 17:20:17 +0800

    fix delete
    Signed-off-by: slimshady999z <zouzhiwei2@h-partners.com>

--- arkweb/ohos_nweb/src/nweb_inputmethod_handler.cc
+++ arkweb/ohos_nweb/src/nweb_inputmethod_handler.cc
@@ -1020,7 +1020,7 @@ void NWebInputMethodHandler::DeleteForwardHandlerOnUI(int32_t length) {
     std::unique_lock<std::mutex> lock(textCursorMutex_);
     textCursorReady_ += text_cursor_length_;
   }
-  for (int32_t i = 0; i < text_cursor_length_; i++) {
+  for (int32_t i = 0; i < length; i++) {
     keyEvent.type = KEYEVENT_RAWKEYDOWN;
     host->SendKeyEvent(keyEvent);
     keyEvent.type = KEYEVENT_CHAR;
@@ -1058,7 +1058,7 @@ void NWebInputMethodHandler::DeleteBackwardHandlerOnUI(int32_t length) {
     std::unique_lock<std::mutex> lock(textCursorMutex_);
     textCursorReady_ += text_cursor_length_;
   }
-  for (int32_t i = 0; i < text_cursor_length_; i++) {
+  for (int32_t i = 0; i < length; i++) {
     keyEvent.type = KEYEVENT_RAWKEYDOWN;
     host->SendKeyEvent(keyEvent);
     keyEvent.type = KEYEVENT_CHAR;
