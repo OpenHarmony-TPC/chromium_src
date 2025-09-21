fdc29c95fefeae3142ef9ea45f15af792affea68
xhh_cy_ <congyue1@h-partners.com>
2025-09-21 18:05:51 +0800

    UT代码提交
    Signed-off-by: xhh_cy_ <congyue1@h-partners.com>

--- content/test/test_render_frame.cc
+++ content/test/test_render_frame.cc
@@ -435,6 +435,13 @@ void TestRenderFrame::BindToFrame(blink::WebNavigationControl* frame) {
           frame->GetLocalFrameToken()));
 }
 
+#if BUILDFLAG(ARKWEB_TEST)
+void TestRenderFrame::SetFrameHostForTest(mojom::FrameHost* frame_host) {
+  frame_host_test_mode = true;
+  frame_host_test = frame_host;
+}
+#endif
+
 mojom::FrameHost* TestRenderFrame::GetFrameHost() {
   // Need to mock this interface directly without going through a binding,
   // otherwise calling its sync methods could lead to a deadlock.
@@ -458,6 +465,12 @@ mojom::FrameHost* TestRenderFrame::GetFrameHost() {
   // Because the first invocation to GetFrameHost() may come while we are inside
   // a message loop already, pumping messags before 1.2 would constitute a
   // nested message loop and is therefore undesired.
+#if BUILDFLAG(ARKWEB_TEST)
+  if (frame_host_test_mode) {
+    frame_host_test_mode = false;
+    return frame_host_test;
+  }
+#endif
   return mock_frame_host_.get();
 }
 
