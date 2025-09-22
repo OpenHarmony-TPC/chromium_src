9b69efefd8e22e538b5a9956e2dcc3eee936c4a8
li-zhiwei-gitee <lizhiwei32@huawei.com>
2025-09-22 14:35:26 +0800

    密码迁移
    Signed-off-by: li-zhiwei-gitee <lizhiwei32@huawei.com>

--- arkweb/chromium_ext/components/os_crypt/sync/os_crypt_linux_for_include.h
+++ arkweb/chromium_ext/components/os_crypt/sync/os_crypt_linux_for_include.h
@@ -24,12 +24,15 @@
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/ohos/nweb_engine_event_logger.h"
+#include "base/ohos/nweb_engine_event_logger_code.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
 #include "cef/libcef/browser/prefs/browser_prefs.h"
 #include "chrome/browser/browser_process.h"
 #include "components/prefs/pref_service.h"
 #include "third_party/ohos_ndk/includes/ohos_adapter/ohos_adapter_helper.h"
+#include "arkweb/ohos_adapter_ndk/interfaces/ohos_adapter_helper.h"
 #endif
 
 #if BUILDFLAG(ARKWEB_ENCRYPT)
@@ -43,6 +46,17 @@ namespace {
 const size_t kIVSizeAESGCM = 12;
 
 constexpr char kDataKeyAlias[] = "nweb_data_key";
+#endif
+
+#if BUILDFLAG(ARKWEB_EXT_PASSWORD)
+enum WebStorageMigrationErrorCode {
+  MIGRATE_SUCCESS = 0,
+  DIRECTORY_OR_FILE_NOT_EXIST,
+  ASSET_QUERY_FAILED,
+  LOGIN_DATA_OPEN_FAILED,
+  PASSWORD_VAULT_CONNECT_FAILED,
+  PASSWORD_IMPORT_FAILED
+};
 #endif
 }  // namespace
 
