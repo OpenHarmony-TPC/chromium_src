546c688c36a6d769c214c127c67d235017a37d52
mac_huang <machuang10@h-partners.com>
2025-09-22 14:05:08 +0800

    update: 更新文件 base_audio_capturer_source.cc
    Signed-off-by: mac_huang <machuang10@h-partners.com>

--- arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_audio_capturer_source.cc
+++ arkweb/chromium_ext/third_party/webrtc/modules/desktop_capture/ohos/base_audio_capturer_source.cc
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 
 #include "third_party/webrtc/modules/desktop_capture/ohos/base_audio_capturer_source.h"
 
