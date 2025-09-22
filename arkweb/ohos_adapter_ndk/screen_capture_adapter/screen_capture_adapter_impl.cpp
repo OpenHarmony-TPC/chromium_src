abe491f617066819f6f4d8ceae4886574d7b59b1
qq_42700029 <wuqiwen7@huawei.com>
2025-09-22 16:03:23 +0800

    update: 更新文件 screen_capture_adapter_impl.cpp
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/ohos_adapter_ndk/screen_capture_adapter/screen_capture_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/screen_capture_adapter/screen_capture_adapter_impl.cpp
@@ -382,7 +382,7 @@ void ScreenCaptureAdapterImpl::ScreenCaptureCallbackOnStateChange(struct OH_AVSc
 
 ScreenCaptureAdapterImpl::~ScreenCaptureAdapterImpl()
 {
-    WVLOG_I("ScreenCaptureAdapterImpl::~ScreenCaptureAdapterImpl, nweb_id = %{public}d", callback_info_.nweb_id);
+    WVLOG_I("ScreenCaptureAdapterImpl::~ScreenCaptureAdapterImpl, nweb_id = %{public}d", nweb_id_);
     {
         std::unique_lock<std::shared_mutex> lock(surface_map_lock_);
         auto video = bufferAvailableQueueMap_.find(nweb_id_);
