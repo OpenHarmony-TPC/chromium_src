3824c3608374eaa83f03774cb5e2c3c8bc4d3462
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-09-25 02:20:44 +0800

    update: 更新文件 event_reporter.cc 1
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- arkweb/ohos_nweb/src/sysevent/event_reporter.cc
+++ arkweb/ohos_nweb/src/sysevent/event_reporter.cc
@@ -18,11 +18,6 @@
 #include "oh_web_performance_timing.h"
 #include "third_party/ohos_ndk/includes/ohos_adapter/ohos_adapter_helper.h"
 
-#include "arkweb/build/features/features.h"
-#include "base/logging.h"
-#include "base/feature_list.h"
-#include "build/build_config.h"
-
 using OHOS::NWeb::HiSysEventAdapter;
 using OHOS::NWeb::OhosAdapterHelper;
 namespace {
