17e40b52ca63f9707e20cb788c636aca95c7ea05
liubo <liubo419@huawei.com>
2025-09-25 10:36:36 +0800

    告警修复 Signed-off-by: liubo419 <liubo419@huawei.com>

--- arkweb/ohos_adapter_ndk/screen_capture_adapter/screen_capture_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/screen_capture_adapter/screen_capture_adapter_impl.cpp
@@ -487,7 +487,6 @@ int32_t ScreenCaptureAdapterImpl::StartCapture()
         WVLOG_E("start capture failed, ret = %{public}d", ret);
         return -1;
     }
-    WVLOG_I("start screen capture[%{public}p] success", screenCapture_);
     return 0;
 }
 
@@ -502,7 +501,6 @@ int32_t ScreenCaptureAdapterImpl::StopCapture()
         WVLOG_E("stop capture failed, ret = %{public}d", ret);
         return -1;
     }
-    WVLOG_I("stop screen capture[%{public}p] success", screenCapture_);
     return 0;
 }
 
