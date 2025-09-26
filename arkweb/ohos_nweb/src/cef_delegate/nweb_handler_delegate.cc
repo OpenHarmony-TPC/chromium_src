8ce18adeb0ddba301f5704ac6956894aff5bfccd
liuzhijie2 <liuzhijie10@huawei.com>
2025-09-26 17:35:28 +0800

    fix handlecancel
    Signed-off-by: liuzhijie2 <liuzhijie10@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
@@ -1995,11 +1995,12 @@ bool NWebHandlerDelegate::OnBeforeBrowse(CefRefPtr<CefBrowser> browser,
   return result;
 }
 
-bool NWebHandlerDelegate::OnCertificateError(CefRefPtr<CefBrowser> browser,
-                                             cef_errorcode_t cert_error,
-                                             const CefString& request_url,
-                                             CefRefPtr<CefSSLInfo> ssl_info,
-                                             CefRefPtr<CefCallback> callback) {
+bool NWebHandlerDelegate::OnCertificateErrorExt(
+    CefRefPtr<CefBrowser> browser,
+    cef_errorcode_t cert_error,
+    const CefString& request_url,
+    CefRefPtr<CefSSLInfo> ssl_info,
+    CefRefPtr<ArkWebCefSslCallback> callback) {
   LOG(INFO) << "NWebHandlerDelegate::OnCertificateError happened";
   SslError error = SslErrorConvert(cert_error);
 
