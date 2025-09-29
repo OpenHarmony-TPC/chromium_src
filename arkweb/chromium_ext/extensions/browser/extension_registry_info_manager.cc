af6d893ce7b58f67d3140b9b389123f114700c4b
xmf1995 <zhaodan97@h-partners.com>
2025-09-29 15:54:13 +0800

    检视意见修改
    Signed-off-by: xmf1995 <zhaodan97@h-partners.com>

--- arkweb/chromium_ext/extensions/browser/extension_registry_info_manager.cc
+++ arkweb/chromium_ext/extensions/browser/extension_registry_info_manager.cc
@@ -676,7 +676,7 @@ void ExtensionRegistryInfoManager::RegisterWebExtensionManagerListener(
 
 // static
 void ExtensionRegistryInfoManager::UnRegisterWebExtensionManagerListener() {
-  LOG(INFO) << "ExtensionRegistryInfoManager::RegisterWebExtensionManagerListener";
+  LOG(INFO) << "ExtensionRegistryInfoManager::UnRegisterWebExtensionManagerListener";
   *g_extension_manager_listener = nullptr;
 }
 
