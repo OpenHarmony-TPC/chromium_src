80eb456125c58dd321f0bfb988b623bfc1b1db4a
SprucEovo <lichenwei2@huawei.com>
2025-09-30 15:02:42 +0800

    修改剪贴板fuzz问题
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/pasteboard_adapter/src/pasteboard_client_adapter_impl_fuzzer.cc
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/pasteboard_adapter/src/pasteboard_client_adapter_impl_fuzzer.cc
@@ -176,7 +176,7 @@ void PasteDataRecordAdapterImplFuzzTest(FuzzedDataProvider* fdp) {
 
   impl->GetCustomData();
 
-  PasteDataRecordAdapterImpl impl2(nullptr);
+  PasteDataRecordAdapterImpl impl2(nullptr, false);
 
   impl2.HtmlToPlainText(html);
 
