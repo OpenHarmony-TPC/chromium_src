362f6fd80db42a32d5fe349228fcd3d8f2e5cc14
shenxinchen <shenxinchen@h-partners.com>
2025-10-10 19:59:06 +0800

    [extension]支持预置扩展
    Signed-off-by: shenxinchen <shenxinchen@h-partners.com>

--- arkweb/chromium_ext/chrome/browser/extensions/extension_service_for_include.cc
+++ arkweb/chromium_ext/chrome/browser/extensions/extension_service_for_include.cc
@@ -12,9 +12,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
+#include "base/stl_util.h"
+
 namespace extensions {
- 
+
 namespace {
 
 void NotifyOnInstalledExtensionsLoadedInFileTask() {
@@ -33,4 +35,33 @@ void NotifyOnInstalledExtensionsLoaded() {
 } 
 
 }  // namespace
+
+void ExtensionService::SetForbidDisplayInSettings(
+    const ExtensionIdSet& extension_ids) {
+  ExtensionIdSet no_longer_forbid = base::STLSetDifference<ExtensionIdSet>(
+      forbid_display_in_settings_, extension_ids);
+  ExtensionIdSet newly_forbid = base::STLSetDifference<ExtensionIdSet>(
+      extension_ids, forbid_display_in_settings_);
+
+  for (const ExtensionId& id : no_longer_forbid) {
+    forbid_display_in_settings_.erase(id);
+    extension_prefs_->SetNotDisplayInSettings(id, false);
+  }
+
+  for (const ExtensionId& id : newly_forbid) {
+    forbid_display_in_settings_.insert(id);
+    extension_prefs_->SetNotDisplayInSettings(id, true);
+  }
+}
+
+void ExtensionService::LoadForbidDisplayInSettingsExtensions() {
+  forbid_display_in_settings_.clear();
+  ExtensionIdSet ids = registry_->GenerateInstalledExtensionsSet().GetIDs();
+  for (const auto& id : ids) {
+    if (extension_prefs_->IsNotDisplayInSettings(id)) {
+      forbid_display_in_settings_.insert(id);
+    }
+  }
+}
+
 }  // namespace extensions
