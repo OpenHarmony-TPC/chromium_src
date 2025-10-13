660058a075112fabc1d25721f17080519d0aa177
liaoyunfan <liaoyunfan@huawei.com>
2025-10-13 14:53:52 +0800

    左滑backpress修复，补全收起虚拟键盘场景
    Signed-off-by: liaoyunfan <liaoyunfan@huawei.com>

--- arkweb/ohos_nweb/src/nweb_inputmethod_handler.cc
+++ arkweb/ohos_nweb/src/nweb_inputmethod_handler.cc
@@ -438,6 +438,7 @@ bool NWebInputMethodHandler::AttachToSystemIME(bool is_need_reset_listener, int3
   }
   isFocusSwitchOnBlur_ = false;
 
+  UpdateTextFieldStatus();
 #if BUILDFLAG(ARKWEB_PASSWORD_AUTOFILL)
   if (!fill_content_.empty()) {
     if (fill_content_node_id_ == input_node_id_) {
@@ -852,7 +853,6 @@ void NWebInputMethodHandler::SetIMEStatusOnUI(bool status) {
     isManualCloseKeyboard_ = false;
   }
   ime_shown_ = status;
-  UpdateTextFieldStatus();
 }
 
 // LCOV_EXCL_START
