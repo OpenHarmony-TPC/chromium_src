89cbecd2795115e8711d6367ae6a44bf0b64d26a
spruceovo <lichenwei2@huawei.com>
2025-10-15 18:15:46 +0800

    启用eyedropper
    Signed-off-by: spruceovo <lichenwei2@huawei.com>

--- arkweb/ohos_adapter_ndk/color_picker_adapter/color_picker_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/color_picker_adapter/color_picker_adapter_impl.cpp
@@ -31,6 +31,96 @@ const std::string FUNC_NAME = "HMS_GCP_StartColorPicker";
 const std::string FUNC_WITH_VALUE_NAME =
     "HMS_GCP_StartColorPickerWithColorValue";
 
+typedef enum {
+  /** an unknown color space */
+  HMS_GCP_UNKNOWN = 0,
+  /** the color space based on Adobe RGB (1998) */
+  HMS_GCP_ADOBE_RGB_1998 = 1,
+  /** the color space based on SMPTE RP 431-2-2007 and IEC 61966-2.1:1999 */
+  HMS_GCP_DCI_P3 = 2,
+  /** the color space based on SMPTE RP 431-2-2007 and IEC 61966-2.1:1999 */
+  HMS_GCP_DISPLAY_P3 = 3,
+  /** the standard red green blue (SRGB) color space based on IEC 61966-2.1:1999
+   */
+  HMS_GCP_SRGB = 4,
+  /** the color space based on ITU-R BT.709, PRIMARIES_BT709 | TRANSFUNC_BT709 |
+     RANGE_FULL */
+  HMS_GCP_BT709 = 6,
+  /** the color space based on ITU-R BT.601, PRIMARIES_BT601_P | TRANSFUNC_BT709
+     | RANGE_FULL */
+  HMS_GCP_BT601_EBU = 7,
+  /** the color space based on ITU-R BT.601, PRIMARIES_BT601_N | TRANSFUNC_BT709
+     | RANGE_FULL */
+  HMS_GCP_BT601_SMPTE_C = 8,
+  /** the color space based on ITU-R BT.2020, PRIMARIES_BT2020 | TRANSFUNC_HLG |
+     RANGE_FULL */
+  HMS_GCP_BT2020_HLG = 9,
+  /** the color space based on ITU-R BT.2020, PRIMARIES_BT2020 | TRANSFUNC_PQ |
+     RANGE_FULL */
+  HMS_GCP_BT2020_PQ = 10,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_HLG | RANGE_FULL */
+  HMS_GCP_P3_HLG = 11,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_PQ | RANGE_FULL */
+  HMS_GCP_P3_PQ = 12,
+  /** PRIMARIES_ADOBE_RGB | TRANSFUNC_ADOBE_RGB | RANGE_LIMIT */
+  HMS_GCP_ADOBE_RGB_1998_LIMIT = 13,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_SRGB | RANGE_LIMIT */
+  HMS_GCP_DISPLAY_P3_LIMIT = 14,
+  /** PRIMARIES_SRGB | TRANSFUNC_SRGB | RANGE_LIMIT */
+  HMS_GCP_SRGB_LIMIT = 15,
+  /** PRIMARIES_BT709 | TRANSFUNC_BT709 | RANGE_LIMIT */
+  HMS_GCP_BT709_LIMIT = 16,
+  /** PRIMARIES_BT601_P | TRANSFUNC_BT709 | RANGE_LIMIT */
+  HMS_GCP_BT601_EBU_LIMIT = 17,
+  /** PRIMARIES_BT601_N | TRANSFUNC_BT709 | RANGE_LIMIT */
+  HMS_GCP_BT601_SMPTE_C_LIMIT = 18,
+  /** PRIMARIES_BT2020 | TRANSFUNC_HLG | RANGE_LIMIT */
+  HMS_GCP_BT2020_HLG_LIMIT = 19,
+  /** PRIMARIES_BT2020 | TRANSFUNC_PQ | RANGE_LIMIT */
+  HMS_GCP_BT2020_PQ_LIMIT = 20,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_HLG | RANGE_LIMIT */
+  HMS_GCP_P3_HLG_LIMIT = 21,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_PQ | RANGE_LIMIT */
+  HMS_GCP_P3_PQ_LIMIT = 22,
+  /** PRIMARIES_P3_D65 | TRANSFUNC_LINEAR */
+  HMS_GCP_LINEAR_P3 = 23,
+  /** PRIMARIES_SRGB | TRANSFUNC_LINEAR */
+  HMS_GCP_LINEAR_SRGB = 24,
+  /** PRIMARIES_BT2020 | TRANSFUNC_LINEAR */
+  HMS_GCP_LINEAR_BT2020 = 25,
+  /** a customized color space */
+  CUSTOM = 5,
+} HMS_GCP_ColorSpace;
+
+typedef struct {
+  /** The red channel. */
+  int32_t red;
+  /** The green channel. */
+  int32_t green;
+  /** The blue channel. */
+  int32_t blue;
+  /** The alpha channel. */
+  int32_t alpha;
+} HMS_GCP_Color;
+
+typedef struct {
+  /** The picked color value. */
+  HMS_GCP_Color color;
+  /** The colorspace of color present in. */
+  HMS_GCP_ColorSpace colorSpace;
+  /** The timestamp of color picked. */
+  int64_t timestamp;
+} HMS_GCP_PickedColorInfo;
+
+using HMS_GCP_OnResult = void (*)(void* userData,
+                                  HMS_GCP_PickedColorInfo colorInfo,
+                                  const int32_t code);
+
+using HMS_GCP_StartColorPicker = int32_t (*)(int32_t initialPosX,
+                                             int32_t initialPosY,
+                                             HMS_GCP_OnResult onResultCallback,
+                                             void* userData);
+
 ColorPickerAdapterImpl::ColorPickerAdapterImpl() {
   library_handle_ = dlopen(COLOR_PICKER_SO_PATH.c_str(), RTLD_LAZY);
 }
