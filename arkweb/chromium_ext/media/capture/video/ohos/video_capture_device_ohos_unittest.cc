a7a2dae22def626177a9ad3c12b511fb41fb81ba
SprucEovo <lichenwei2@huawei.com>
2025-10-17 12:05:52 +0800

    修改UT编译错误
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_ohos_unittest.cc
+++ arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_ohos_unittest.cc
@@ -166,6 +166,7 @@ class OhosAdapterHelperMock : public OhosAdapterHelper {
   MOCK_METHOD(std::unique_ptr<CertManagerAdapter>, GetCertManagerAdapter, (), (override));
   MOCK_METHOD(std::unique_ptr<NetConfigAdapter>, GetNetConfigAdapter, (), (override));
   MOCK_METHOD(std::unique_ptr<NetEventAdapter>, CreateNetEventAdapter, (), (override));
+  MOCK_METHOD(ColorPickerAdapter &, GetColorPickerAdapter, (), (override));
 };
 
 class CameraSurfaceBufferAdapterMock : public CameraSurfaceBufferAdapter {
