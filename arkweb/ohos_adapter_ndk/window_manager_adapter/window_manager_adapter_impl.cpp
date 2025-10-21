1a879bedb394009394d87a81a8cbe44f40a58de7
panliujun <panliujun3@h-partners.com>
2025-10-21 14:53:56 +0800

    Support PointerLock
    Signed-off-by: panliujun <panliujun3@h-partners.com>

--- arkweb/ohos_adapter_ndk/window_manager_adapter/window_manager_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/window_manager_adapter/window_manager_adapter_impl.cpp
@@ -14,10 +14,13 @@
  */
 #include "window_manager_adapter_impl.h"
 
+#include <dlfcn.h>
 #include <window_manager/oh_window.h>
 #include "arkweb/ohos_nweb/src/nweb_hilog.h"
 
 namespace OHOS::NWeb {
+using OHWindowManagerLockCursor = int32_t (*)(int32_t windowId, bool isCursorFollowMovement);
+using OHWindowManagerUnLockCursor = int32_t (*)(int32_t windowId);
 
 WindowManagerAdapterImpl& WindowManagerAdapterImpl::GetInstance()
 {
@@ -35,5 +38,52 @@ bool WindowManagerAdapterImpl::GetWindowPrivacyMode(uint32_t windowId)
         return false;
     }
     return windowProperties.isPrivacyMode;
+}
+
+int32_t WindowManagerAdapterImpl::LockPointer(int32_t windowId) {
+#if defined(__aarch64__)
+  void* dl = dlopen("/system/lib64/ndk/libnative_window_manager.so", RTLD_LAZY);
+#else
+  void* dl = dlopen("/system/lib/ndk/libnative_window_manager.so", RTLD_LAZY);
+#endif
+
+  if (dl == nullptr) {
+    WVLOG_W("cannot find the Window's path");
+    return WindowManager_ErrorCode::OK;
+  }
+  OHWindowManagerLockCursor set_lock_curosr = nullptr;
+  set_lock_curosr = reinterpret_cast<OHWindowManagerLockCursor>(
+      dlsym(dl, "OH_WindowManager_LockCursor"));
+  if (set_lock_curosr == nullptr) {
+    WVLOG_W("cannot find the function called OH_WindowManager_LockCursor");
+    return WindowManager_ErrorCode::OK;
+  }
+  int32_t res = set_lock_curosr(windowId, false);
+  WVLOG_I("LockPointer result code is %{public}d.", res);
+  return res;
+}
+
+int32_t WindowManagerAdapterImpl::UnlockPointer(int32_t windowId) {
+#if defined(__aarch64__)
+  void* dl = dlopen("/system/lib64/ndk/libnative_window_manager.so", RTLD_LAZY);
+#else
+  void* dl = dlopen("/system/lib/ndk/libnative_window_manager.so", RTLD_LAZY);
+#endif
+
+  OHWindowManagerUnLockCursor set_unlock_curosr = nullptr;
+  if (dl == nullptr) {
+    WVLOG_W("cannot find the Window's path");
+    return WindowManager_ErrorCode::OK;
+  }
+
+  set_unlock_curosr = reinterpret_cast<OHWindowManagerUnLockCursor>(
+      dlsym(dl, "OH_WindowManager_UnlockCursor"));
+  if (set_unlock_curosr == nullptr) {
+    WVLOG_W("cannot find the function called OH_WindowManager_UnlockCursor");
+    return WindowManager_ErrorCode::OK;
+  }
+  int32_t res = set_unlock_curosr(windowId);
+  WVLOG_I("UnLockPointer result code is %{public}d.", res);
+  return res;
 }
 } // namespace OHOS::NWeb
