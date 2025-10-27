c034ad9b70e9b594e99ca062a8f69d655499585c
SprucEovo <lichenwei2@huawei.com>
2025-10-27 20:56:51 +0800

    修改UT编译错误
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_event_handler_unittest.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_event_handler_unittest.cc
@@ -508,6 +508,8 @@ class MockCefBrowserHost : public ArkWebBrowserHostExt {
 
   MOCK_METHOD0(GetShrinkViewportHeight, int());
 
+  void OnEyeDropperResult(bool success, uint32_t color) override {}
+
   void SetPrintBackground(bool enable) override {}
 
   bool GetPrintBackground() override { return false; }
