1c73beed75e77bf868e78b70b4095854f27e31b9
weixin_46366341 <836253168@qq.com>
2025-10-27 16:20:23 +0800

    update: 更新文件 webpack.umd.common.js
    Signed-off-by: weixin_46366341 <836253168@qq.com>

--- arkweb/ohos_autolayout/build/umd/common/webpack.umd.common.js
+++ arkweb/ohos_autolayout/build/umd/common/webpack.umd.common.js
@@ -79,8 +79,6 @@ module.exports = merge(common, {
                             'console.log',
                             'console.info',
                             'console.debug',
-                            'Log.debug',
-                            'Log.info',
                             'Log.d',
                             'Log.i',
                             'Log.info',
