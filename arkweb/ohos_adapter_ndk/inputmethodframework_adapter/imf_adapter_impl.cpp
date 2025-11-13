d9bdaacc44620cdae56d3685b12bc9aae867a9dd
slimshady999z <zouzhiwei2@h-partners.com>
2025-11-13 10:03:53 +0800

    fix inputmethod crash
    Signed-off-by: slimshady999z <zouzhiwei2@h-partners.com>

--- arkweb/ohos_adapter_ndk/inputmethodframework_adapter/imf_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/inputmethodframework_adapter/imf_adapter_impl.cpp
@@ -855,6 +855,7 @@ bool IMFAdapterImpl::Attach(std::shared_ptr<IMFTextListenerAdapter> listener, bo
     }
     InputMethod_AttachOptions *options = OH_AttachOptions_Create(isShowKeyboard);
     InputMethod_ErrorCode ret = OH_InputMethodController_Attach(textEditorProxy_, options, &inputMethodProxy_);
+    OH_AttachOptions_Destroy(options);
     if (ret != IME_ERR_OK) {
         WVLOG_E("inputmethod attach failed, errcode=%{public}d", ret);
         return false;
@@ -886,10 +887,6 @@ bool IMFAdapterImpl::Attach(std::shared_ptr<IMFTextListenerAdapter> listener, bo
 
     std::lock_guard<std::mutex> lock(textEditorProxyMutex_);
 
-    if (textEditorProxy_ != nullptr && isResetListener) {
-        IMFTextEditorProxyImpl::TextEditorProxyDestroy(textEditorProxy_);
-        textEditorProxy_ = nullptr;
-    }
     if (textEditorProxy_ == nullptr) {
         InputMethod_TextEditorProxy* textEditorNewProxy = IMFTextEditorProxyImpl::TextEditorProxyCreate(listener);
         if (textEditorNewProxy == nullptr) {
@@ -905,6 +902,7 @@ bool IMFAdapterImpl::Attach(std::shared_ptr<IMFTextListenerAdapter> listener, bo
     }
     InputMethod_AttachOptions *options = OH_AttachOptions_Create(isShowKeyboard);
     ret = OH_InputMethodController_Attach(textEditorProxy_, options, &inputMethodProxy_);
+    OH_AttachOptions_Destroy(options);
     if (ret != IME_ERR_OK) {
         WVLOG_E("Inputmethod attach failed, ret=%{public}d", isShowKeyboard);
     }
@@ -933,10 +931,6 @@ bool IMFAdapterImpl::AttachWithRequestKeyboardReason(std::shared_ptr<IMFTextList
 
     std::lock_guard<std::mutex> lock(textEditorProxyMutex_);
 
-    if (textEditorProxy_ != nullptr && isResetListener) {
-        IMFTextEditorProxyImpl::TextEditorProxyDestroy(textEditorProxy_);
-        textEditorProxy_ = nullptr;
-    }
     if (textEditorProxy_ == nullptr) {
         InputMethod_TextEditorProxy* textEditorNewProxy = IMFTextEditorProxyImpl::TextEditorProxyCreate(listener);
         if (textEditorNewProxy == nullptr) {
@@ -950,11 +944,12 @@ bool IMFAdapterImpl::AttachWithRequestKeyboardReason(std::shared_ptr<IMFTextList
     if (ret != IME_ERR_OK) {
         return false;
     }
-    InputMethod_AttachOptions *options = OH_AttachOptions_Create(isShowKeyboard);
-    /*TODO: wait arkweb upgrade ndk15
-    InputMethod_AttachOptions* options = OH_AttachOptions_CreateWithRequestKeyboardReason(isShowKeyboard, requestKeyboardReason);
-    */
+    InputMethod_AttachOptions* options =
+        OH_AttachOptions_CreateWithRequestKeyboardReason(
+            isShowKeyboard, static_cast<InputMethod_RequestKeyboardReason>(
+                                requestKeyboardReason));
     ret = OH_InputMethodController_Attach(textEditorProxy_, options, &inputMethodProxy_);
+    OH_AttachOptions_Destroy(options);
     if (ret != IME_ERR_OK) {
         WVLOG_E("Inputmethod attach failed, ret=%{public}d", isShowKeyboard);
     }
