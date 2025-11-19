2dec4d4482a7572fd0876e734566f4f6353736c6
gongyadong <gongyadong@h-partners.com>
2025-11-19 14:24:28 +0800

    【安全】ArkWeb迁移：Close google request
    Signed-off-by: gongyadong <gongyadong@h-partners.com>
    Change-Id: I232a022cc1c5c3f42c6502b065b9a7ab2c3c0236

--- components/update_client/update_checker.cc
+++ components/update_client/update_checker.cc
@@ -107,6 +107,12 @@ void UpdateCheckerImpl::CheckForUpdates(
 
   update_check_callback_ = std::move(update_check_callback);
 
+#if BUILDFLAG(IS_OHOS)
+  UpdateCheckFailed(ErrorCategory::kUpdateCheck,
+                    static_cast<int>(ProtocolError::MISSING_URLS), 0);
+  return;
+#endif
+
   auto check_for_updates_invoker = base::BindOnce(
       &UpdateCheckerImpl::CheckForUpdatesHelper, weak_factory_.GetWeakPtr(),
       context, config_->UpdateUrl(), additional_attributes);
