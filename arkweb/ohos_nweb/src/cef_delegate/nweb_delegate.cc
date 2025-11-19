33e18f36f51811ef525081f60b01e944cd9969dd
defeng20201 <wudefeng@huawei.com>
2025-11-19 20:17:20 +0800

    fix return result
    Signed-off-by: mac_huang <machuang10@h-partners.com>
    
    
    revert merge request: fix return result
    
    3d6c2ca0dc57520d8b53402b2b977436dacf6047
    
    This revert merge request !6125

--- arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -858,7 +858,7 @@ bool NWebDelegate::Init(bool is_enhance_surface,
 #if BUILDFLAG(ARKWEB_WEBRTC)
   if (GetBrowser() == nullptr || GetBrowser()->GetHost() == nullptr) {
     LOG(ERROR) << "can not get browser ,can not set NWeb id";
-    return false;
+    return true;
   }
   GetBrowser()->GetHost()->SetNWebId(GetBrowser()->GetNWebId());
 #endif  // BUILDFLAG(ARKWEB_WEBRTC)
