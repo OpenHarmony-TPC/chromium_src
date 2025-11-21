8731fa3c74b7fe370af4b56600a431faa26fefa3
sylvieQaQ <yuanshiyu3@h-partners.com>
2025-11-21 09:18:09 +0800

    update: 更新文件 sys_param.h
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- arkweb/ohos_adapter_ndk/utils/include/sys_param.h
+++ arkweb/ohos_adapter_ndk/utils/include/sys_param.h
@@ -131,7 +131,7 @@ typedef void *CachedHandle;
 static inline const char *CachedParameterGetChanged(CachedHandle handle, int *changed)
 {
     struct CachedParameter_ *param = (struct CachedParameter_ *)handle;
-    if (param == NULL || param->workspace == NULL || param->workspace->area == NULL) {
+    if (param == NULL) {
         return NULL;
     }
     // no change, do not to find
