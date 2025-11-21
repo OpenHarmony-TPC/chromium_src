bf49ee4a006518108b87f5fc92055937d93c3e05
hoo334 <liminghu1@huawei.com>
2025-11-21 10:04:56 +0800

    fix codecheck
    Signed-off-by: hoo334 <liminghu1@huawei.com>

--- arkweb/ohos_adapter_ndk/audio_capturer_adapter/audio_capturer_adapter_impl.h
+++ arkweb/ohos_adapter_ndk/audio_capturer_adapter/audio_capturer_adapter_impl.h
@@ -71,7 +71,7 @@ public:
 
     static int32_t OnInterruptEvent(OH_AudioCapturer* capturer, void* userData, OH_AudioInterrupt_ForceType type,
                                     OH_AudioInterrupt_Hint hint);
-    static std::shared_mutex& GetAdapterMutex()
+    static std::shared_mutex& GetAdapterMutex();
 private:
     OH_AudioCapturer* audio_capturer_ = nullptr;
     size_t callback_index_ = 0;
