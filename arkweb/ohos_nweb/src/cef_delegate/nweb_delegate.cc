0fced62014c2ebe83aa7b2871f3a63ed8d5b1b4e
mac_huang <machuang10@h-partners.com>
2025-11-21 18:16:21 +0800

    update: 更新文件 nweb_delegate.cc
    Signed-off-by: mac_huang <machuang10@h-partners.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_delegate.cc
@@ -857,7 +857,6 @@ bool NWebDelegate::Init(bool is_enhance_surface,
   }
 #if BUILDFLAG(ARKWEB_WEBRTC)
   if (GetBrowser() == nullptr || GetBrowser()->GetHost() == nullptr) {
-    LOG(ERROR) << "can not get browser ,can not set NWeb id";
     return true;
   }
   GetBrowser()->GetHost()->SetNWebId(GetBrowser()->GetNWebId());
