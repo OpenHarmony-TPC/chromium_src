df19073e89edbf22ac5900b2d82d57b7eacb97e5
guzhaoyang <guzhaoyang@h-partners.com>
2025-11-24 17:00:45 +0800

    update: 更新文件 partition_bucket.h
    Signed-off-by: guzhaoyang <guzhaoyang@h-partners.com>

--- src/base/allocator/partition_allocator/src/partition_alloc/partition_bucket.h
+++ src/base/allocator/partition_allocator/src/partition_alloc/partition_bucket.h
@@ -1,7 +1,6 @@
 // Copyright 2018 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 #ifndef PARTITION_ALLOC_PARTITION_BUCKET_H_
 #define PARTITION_ALLOC_PARTITION_BUCKET_H_
 
