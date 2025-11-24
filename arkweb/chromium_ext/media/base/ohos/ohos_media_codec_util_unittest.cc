3cd9e2049618bfd4337a8cf4a1091af0855bde86
lvshiqi1 <lvshiqi1@h-partners.com>
2025-11-24 16:09:51 +0800

    代码同步
    Signed-off-by: lvshiqi1 <lvshiqi1@h-partners.com>
    Change-Id: I7424851530439a71db13ff93de8d5b1134d30e92

--- arkweb/chromium_ext/media/base/ohos/ohos_media_codec_util_unittest.cc
+++ arkweb/chromium_ext/media/base/ohos/ohos_media_codec_util_unittest.cc
@@ -203,16 +203,16 @@ class MockOhosAdapterHelper : public OHOS::NWeb::OhosAdapterHelper {
               CreateMigrationMgrAdapter,
               (),
               (override));
+  MOCK_METHOD(std::unique_ptr<ScreenlockManagerAdapter>,
+              CreateScreenlockManagerAdapter,
+              (),
+              (override));
   MOCK_METHOD(std::unique_ptr<CertManagerAdapter>,
               GetCertManagerAdapter,
               (),
               (override));
   MOCK_METHOD(std::unique_ptr<NetConfigAdapter>,
               GetNetConfigAdapter,
-              (),
-              (override));
-  MOCK_METHOD(std::unique_ptr<ScreenlockManagerAdapter>,
-              CreateScreenlockManagerAdapter,
               (),
               (override));
   MOCK_METHOD(std::unique_ptr<NetEventAdapter>,
