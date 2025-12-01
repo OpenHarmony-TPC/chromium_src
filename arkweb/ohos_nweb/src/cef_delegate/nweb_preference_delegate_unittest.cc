95ebb713a588711dba909f637bec9c7ab82ebfbf
lingjinyue <lingyue2@huawei.com>
2025-12-01 09:21:20 +0800

    增加, bool canDock
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_preference_delegate_unittest.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_preference_delegate_unittest.cc
@@ -606,7 +606,7 @@ class MockPreferenceCefBrowserHost : public ArkWebBrowserHostExt {
 
   void ShowDevToolsWith(CefRefPtr<ArkWebBrowserHostExt> frontend_browser,
                         CefRefPtr<CefDevToolsMessageHandlerDelegate> delegate,
-                        const CefPoint& inspect_element_at) override {}
+                        const CefPoint& inspect_element_at, bool canDock) override {}
 
   bool IsFullscreen() override { return false; }
 
