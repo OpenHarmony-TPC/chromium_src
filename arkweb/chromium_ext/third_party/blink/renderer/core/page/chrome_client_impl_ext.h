480f0d8c257d0d4b5be970115a33d6e4e911bffb
SprucEovo <lichenwei2@huawei.com>
2025-12-04 11:44:18 +0800

    限制文件上传请求队列数为1
    Signed-off-by: SprucEovo <lichenwei2@huawei.com>

--- arkweb/chromium_ext/third_party/blink/renderer/core/page/chrome_client_impl_ext.h
+++ arkweb/chromium_ext/third_party/blink/renderer/core/page/chrome_client_impl_ext.h
@@ -33,10 +33,6 @@ class ChromeClientImplExt : public ChromeClientImpl {
   gfx::Rect GetVisibleRectToWeb(LocalFrame*) override;
 #endif  // ARKWEB_DRAG_DROP
 
-#if BUILDFLAG(ARKWEB_FILE_UPLOAD)
-  void DisconnectClient();
-#endif  // ARKWEB_FILE_UPLOAD
-
 #if BUILDFLAG(ARKWEB_AI)
   void CreateOverlay(
       LocalFrame* frame,
