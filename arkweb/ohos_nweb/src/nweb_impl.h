7da1c6f3da7dbed735a5b01ac5efa30cacf7d5ac
icaozhengfei <caozhengfei@huawei-partners.com>
2025-12-06 16:54:07 +0800

    softKeyboard BehaviorMode avoid errorcodev2
    Signed-off-by: icaozhengfei <caozhengfei@huawei-partners.com>

--- arkweb/ohos_nweb/src/nweb_impl.h
+++ arkweb/ohos_nweb/src/nweb_impl.h
@@ -1175,7 +1175,7 @@ class NWebImpl : public NWeb {
   bool SetFocusByPosition(float x, float y) override;
 #endif  // BUILDFLAG(ARKWEB_INPUT_EVENTS)
 #if BUILDFLAG(ARKWEB_SOFTKEYBOARD_AVOID)
-  static void SetSoftKeyboardBehaviorMode(WebSoftKeyboardBehaviorMode mode);
+  static void SetSoftKeyboardBehaviorModeV2(WebSoftKeyboardBehaviorMode mode);
 #endif
 #if BUILDFLAG(ARKWEB_PIP)
   void SetPipNativeWindow(int delegate_id,
