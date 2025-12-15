74c9cc824eb74aa9cf6c9a1fa4cf5297723eb98d
lingjinyue <lingyue2@huawei.com>
2025-12-15 14:47:52 +0800

    SetDockMode和SetInspectedPageBounds返回值改为void
    Signed-off-by: lingjinyue <lingyue2@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_devtools_message_handler_impl.h
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_devtools_message_handler_impl.h
@@ -44,11 +44,11 @@ class NWebDevToolsMessageHandlerImpl
   bool BringToFront() override;
   bool CloseWindow() override;
   bool ActiveDevToolsWindow() override;
-  bool SetInspectedPageBounds(int left,
+  void SetInspectedPageBounds(int left,
                               int top,
                               int width,
                               int height) override;
-  bool SetDockMode(int mode) override;
+  void SetDockMode(int mode) override;
 
  private:
   void GetFileSelectorTitle(std::string& title, uint32_t mode);
