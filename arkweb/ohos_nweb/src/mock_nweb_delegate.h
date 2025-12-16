fd8cacfdf79eb82ad34170de3c0ef157c6e4d0d1
zhangyanchuan <zhangyanchuan@huawei.com>
2025-12-16 10:24:19 +0800

    修复tabs.onHighlighted接口
    Signed-off-by: zhangyanchuan <zhangyanchuan@huawei.com>

--- arkweb/ohos_nweb/src/mock_nweb_delegate.h
+++ arkweb/ohos_nweb/src/mock_nweb_delegate.h
@@ -1110,10 +1110,6 @@ class MockNWebDelegate : public NWebDelegateInterface {
                std::unique_ptr<NWebExtensionTabDetachInfo> detachInfo),
               (override));
   MOCK_METHOD(void,
-              WebExtensionTabHighlighted,
-              (NWebExtensionTabHighlightInfo & highlightInfo),
-              (override));
-  MOCK_METHOD(void,
               WebExtensionTabMoved,
               (int32_t tab_id,
                std::unique_ptr<NWebExtensionTabMoveInfo> moveInfo),
