63b41523e5fac705eede7a82571e761d87979024
whuqincheng <liweiyang1@huawei.com>
2025-12-18 10:20:15 +0800

    GetNativeWindow安全整改
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- arkweb/chromium_ext/gpu/ipc/common/nweb_native_window_tracker.cc
+++ arkweb/chromium_ext/gpu/ipc/common/nweb_native_window_tracker.cc
@@ -48,6 +48,9 @@ void* NWebNativeWindowTracker::GetNativeWindow(int32_t native_window_id, bool is
       if (window) {
         native_window_map_.emplace(native_window_id, std::move((void *)window));
         LOG(DEBUG) << "Add native window id = " << native_window_id;
+        if (is_ref) {
+          OHOS::NWeb::OhosAdapterHelper::GetInstance().GetWindowAdapterInstance().AddNativeWindowRef(window);
+        }
         return window;
       }
     }
