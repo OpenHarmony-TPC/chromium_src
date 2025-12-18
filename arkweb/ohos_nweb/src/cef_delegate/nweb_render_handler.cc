daef701afc840e50e46f7950b3f7d22316e12b66
zhuhousheng <zhuhousheng@huawei-partners.com>
2025-12-18 20:37:06 +0800

    web首屏渲染补
    Signed-off-by: zhuhousheng <zhuhousheng@huawei-partners.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_render_handler.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_render_handler.cc
@@ -1625,6 +1625,12 @@ void NWebRenderHandler::OnDetectedBlankScreen(
 void NWebRenderHandler::OnFirstScreenPaint(const std::string& url,
                                            int64_t navigationStartTime,
                                            int64_t firstScreenPaintTime) {
+  LOG(INFO) << "NWebRenderHandler::OnFirstScreenPaint"
+      << " duration: " << (firstScreenPaintTime - navigationStartTime)
+      << "  url: " << url
+      << " navigationStartTime: " << navigationStartTime
+      << " firstScreenPaintTime: " << firstScreenPaintTime;
+  
   if (auto handler = handler_.lock()) {
     handler->OnFirstScreenPaint(url, navigationStartTime, firstScreenPaintTime);
   }
