578121a6a126e35c3d4910cbf027e27a707f68d0
dujiafeng <dujiafeng@huawei.com>
2025-12-23 16:17:49 +0800

    【功能-BUGFIX】修复录音时进入休眠模式，可能崩溃的问题
    Signed-off-by: dujiafeng <dujiafeng@huawei.com>
    Change-Id: I8c9fad114312d386017e8d1f84996fa648a9b190

--- media/audio/ohos/ohos_audio_capturer_source.cc
+++ media/audio/ohos/ohos_audio_capturer_source.cc
@@ -112,6 +112,14 @@ int32_t AudioCapturerOnReadData(OH_AudioCapturer* capturer,
   return 0;
 }
 
+int32_t AudioCapturerOnInterruptEvent(OH_AudioCapturer* capturer,
+                                      void* userData,
+                                      OH_AudioInterrupt_ForceType type,
+                                      OH_AudioInterrupt_Hint hint) {
+  LOG(INFO) << "audioCapturer OnInterrupt,type:" << type << " hint:" << hint;
+  return 0;
+}
+
 void OHOSAudioCapturerSource::InitializeOnCapturerThread() {
   DCHECK(capturer_task_runner_->BelongsToCurrentThread());
   // create builder
@@ -125,6 +133,9 @@ void OHOSAudioCapturerSource::InitializeOnCapturerThread() {
   // set callbacks
   OH_AudioCapturer_Callbacks callbacks;
   callbacks.OH_AudioCapturer_OnReadData = AudioCapturerOnReadData;
+  callbacks.OH_AudioCapturer_OnInterruptEvent = AudioCapturerOnInterruptEvent;
+  callbacks.OH_AudioCapturer_OnStreamEvent = nullptr;
+  callbacks.OH_AudioCapturer_OnError = nullptr;
   OH_AudioStreamBuilder_SetCapturerCallback(audio_stream_builder_, callbacks,
                                             this);
   if ((params_.effects() &
