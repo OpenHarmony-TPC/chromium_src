120cccbbd8e0309ac740e0b3e48f8de29b71d902
caizhiwei <caizhiwei7@huawei.com>
2025-12-26 16:09:56 +0800

    【功能-BUGFIX】解决触发拖拽后拖拽未正常结束导致鼠标和触摸事件无法正常处理的问题
    Signed-off-by: caizhiwei <caizhiwei7@huawei.com>
    Change-Id: I59a0479a25dc247fe9f27089a80ad44e4d202313

--- ui/ozone/platform/ohos/drag/ohos_window_drag_manager.cc
+++ ui/ozone/platform/ohos/drag/ohos_window_drag_manager.cc
@@ -125,13 +125,6 @@ bool OhosWindowDragManager::NeedSendWindowEventToUi(const int32_t widget_id,
                  << widget_id << ", action:" << action;
     return false;
   }
-  if (IsTouchingDragTab() && event_id != touch_drag_tab_finger_id_) {
-    LOG(WARNING) << "[OhosTabDrag] " << __FUNCTION__
-                 << " touch event is filtered when current fingerId is not "
-                 << "shift fingerId, current finger id:" << event_id
-                 << ", touch_drag_tab_finger_id_:" << touch_drag_tab_finger_id_;
-    return false;
-  }
 
   // When tab is dragging, the up event of source window and down event of
   // target window are simulated after the window event is transferred. The two
