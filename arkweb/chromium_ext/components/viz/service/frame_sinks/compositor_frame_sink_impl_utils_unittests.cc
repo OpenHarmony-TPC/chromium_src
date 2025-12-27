ea1c72fae6529ff8a1d5f6b0f3ed91eb496449d5
wang-yanhan <wangyanhan3@huawei.com>
2025-12-27 14:32:54 +0800

    update: 更新文件 compositor_frame_sink_impl_utils_unittests.cc
    Signed-off-by: wang-yanhan <wangyanhan3@huawei.com>

--- arkweb/chromium_ext/components/viz/service/frame_sinks/compositor_frame_sink_impl_utils_unittests.cc
+++ arkweb/chromium_ext/components/viz/service/frame_sinks/compositor_frame_sink_impl_utils_unittests.cc
@@ -92,7 +92,7 @@ TEST_F(CompositorFrameSinkImplUtilTest, OnSetBypassVsyncCondition) {
 
 #if BUILDFLAG(ARKWEB_THROTTLE_FRAME)
 TEST_F(CompositorFrameSinkImplUtilTest, UpdateThrottleMode) {
-    util(nullptr);
+    CompositorFrameSinkImplUtil util(nullptr);
     ASSERT_NO_FATAL_FAILURE(util.UpdateThrottleMode(false));
 }
 #endif
