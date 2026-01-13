88017ff2435ade2ecc900bd614953a9923f3c547
zouchengyuan <zouchengyuan@h-partners.com>
2026-01-13 11:07:33 +0800

    【功能-BUGIFX】修复welink粘贴崩溃问题
    Signed-off-by: zouchengyuan <zouchengyuan@h-partners.com>
    Change-Id: Ie34e9565903d0a1c32927fe0e17ef55db60544d2

--- ui/base/clipboard/clipboard_ohos.cc
+++ ui/base/clipboard/clipboard_ohos.cc
@@ -1095,7 +1095,7 @@ class ClipboardOHOSInternal {
     ClipboardSequenceNumberToken token;
   } clipboard_sequence_;
   ohos::adapter::common::SharedLibrary pasteboard_lib_;
-  PasteboardGetChangeCountFunc* pasteboard_get_change_count_func_;
+  PasteboardGetChangeCountFunc* pasteboard_get_change_count_func_ = nullptr;
 
   base::WeakPtrFactory<ClipboardOHOSInternal> weak_factory_{this};
 };
