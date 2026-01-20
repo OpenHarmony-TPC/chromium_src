7a98c7a1f53f7762beff3457c8b110fe6958dbc2
zwpless1 <zhaowenpu1@huawei.com>
2026-01-20 21:35:29 +0800

    update: 更新文件 media_avsession_adapter_impl.cpp
    Signed-off-by: zwpless1 <zhaowenpu1@huawei.com>

--- arkweb/ohos_adapter_ndk/media_avsession_adapter/media_avsession_adapter_impl.cpp
+++ arkweb/ohos_adapter_ndk/media_avsession_adapter/media_avsession_adapter_impl.cpp
@@ -202,11 +202,7 @@ void MediaAVSessionAdapterImpl::DestroyAVSession() {
     if (avSessionKey_) {
         auto iter = avSessionMap.find(avSessionKey_->ToString());
         if (iter != avSessionMap.end()) {
-            if (iter->second && iter->second->is_avcast_) {
-                WVLOG_I("DestroyAVSession, current process is casting, no need to clear avSessionMap");
-            } else {
-                avSessionMap.erase(iter);
-            }
+            avSessionMap.erase(iter);
         }
     }
     poster_url_ = "";
@@ -921,7 +917,6 @@ bool MediaAVSessionAdapterImpl::GetUiSeekingByClient() {
 
 void MediaAVSessionAdapterImpl::SetAvCast(bool is_avcast) {
     WVLOG_I("MediaAVSessionAdapterImpl SetAvCast: %{public}d", is_avcast);
-    is_avcast_ = is_avcast;
     auto media = callback_wrapper_.GetCallback(callback_index_);
     if (!media) {
         WVLOG_E("SetAvCast, ohmedia: media is null");
