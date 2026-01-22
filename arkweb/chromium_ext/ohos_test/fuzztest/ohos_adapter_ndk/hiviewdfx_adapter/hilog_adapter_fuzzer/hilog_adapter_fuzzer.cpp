a7ae514e167acd5b2cd73cccf15ee05230d7441f
sylvieQaQ <yuanshiyu3@h-partners.com>
2026-01-22 14:48:29 +0800

    update: 更新文件 hilog_adapter_fuzzer.cpp 1
    Signed-off-by: sylvieQaQ <yuanshiyu3@h-partners.com>

--- arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/hiviewdfx_adapter/hilog_adapter_fuzzer/hilog_adapter_fuzzer.cpp
+++ arkweb/chromium_ext/ohos_test/fuzztest/ohos_adapter_ndk/hiviewdfx_adapter/hilog_adapter_fuzzer/hilog_adapter_fuzzer.cpp
@@ -39,7 +39,6 @@ void PrintLogFuzzTest(const uint8_t* data, size_t size) {
     LogLevelAdapter logLevel = static_cast<LogLevelAdapter>(num);
     
     std::string tag = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
-    std::string fmt = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
     std::string message = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
     HiLogAdapter::PrintLog(logLevel, tag.c_str(), "%s", message.c_str());
 }
@@ -53,7 +52,6 @@ void PrintConsoleLogFuzzTest(const uint8_t* data, size_t size) {
     LogLevelAdapter logLevel = static_cast<LogLevelAdapter>(num);
     
     std::string tag = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
-    std::string fmt = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
     std::string message = dataProvider.ConsumeRandomLengthString(MAX_STRING_LENGTH);
     HiLogAdapter::PrintConsoleLog(logLevel, tag.c_str(), "%s", message.c_str());
 }
