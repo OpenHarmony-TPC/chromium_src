d3e62ca0247f9d4464d631377103dcf0ce4ebd00
Andy <gaopengzhen@huawei.com>
2026-01-22 19:55:19 +0800

    [LoggerReport] Fix NetworkContext crash.
    Change-Id: I18c1c658f74ecacca0ff7a0853055994667d282e
    Signed-off-by: Andy <gaopengzhen@huawei.com>

--- arkweb/chromium_ext/services/network/network_service_network_delegate_ext.cc
+++ arkweb/chromium_ext/services/network/network_service_network_delegate_ext.cc
@@ -91,7 +91,8 @@ void NetworkServiceNetworkDelegateExt::RecordErrorInfo(net::URLRequest* request,
                      << ostr.str();
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           ::switches::kEnableLoggerReport)) {
-    if (!network_context_->AsArkWebNetworkContextExt()->IsStrictLogMode()) {
+    if (!network_context_->IsDestructing() &&
+        !network_context_->AsArkWebNetworkContextExt()->IsStrictLogMode()) {
       std::string url_info = request->url().spec();
       const size_t url_print_len = 1024;
       if (url_info.length() > url_print_len) {
