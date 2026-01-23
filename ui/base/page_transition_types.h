5866259542d662e245ce027850a46a1c75e44e4b
chendawei18 <chendawei18@huawei.com>
2026-01-23 14:43:00 +0800

    update: 更新文件 page_transition_types.h
    Signed-off-by: chendawei18 <chendawei18@huawei.com>

--- ui/base/page_transition_types.h
+++ ui/base/page_transition_types.h
@@ -1,3 +1,8 @@
+// Copyright (c) 2026 Huawei Device Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Based on page_transition_types.h originally written by
 // Copyright 2012 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
