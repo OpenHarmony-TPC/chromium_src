6a65279417a022b95420177fb81972fddc0cc1f4
lijiancheng21 <lijiancheng21@huawei-partners.com>
2026-01-29 16:15:28 +0800

    onSafeBrowsingCheckDetail Parameter PB Optimization
    Signed-off-by: lijiancheng21 <lijiancheng21@huawei-partners.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_handler_delegate.cc
@@ -5788,20 +5788,17 @@ void NWebHandlerDelegate::OnMediaCastEnter() {
 void NWebHandlerDelegate::OnSafeBrowsingCheckDetail(int code,
                                                     int policy,
                                                     int threat) {
-  LOG(INFO) << "NWebHandlerDelegate::OnSafeBrowsingCheckDetail code:" << code
-            << " ,policy:" << policy << " ,threat:" << threat;
+  SafeBrowsingParams param;
+  param.code = code;
+  param.policy = policy;
+  param.threat = threat;
+  LOG(INFO) << "NWebHandlerDelegate::OnSafeBrowsingCheckDetail";
 #if BUILDFLAG(ARKWEB_NWEB_EX)
   if (IsNativeApiEnable()) {
-    dispatcher_.OnSafeBrowsingCheckDetail(code, policy, threat);
+    dispatcher_.OnSafeBrowsingCheckDetail(param);
     return;
   }
 #endif
-  if (web_app_client_extension_listener_ != nullptr &&
-      web_app_client_extension_listener_->OnSafeBrowsingCheckDetail !=
-          nullptr) {
-    web_app_client_extension_listener_->OnSafeBrowsingCheckDetail(
-        web_app_client_extension_listener_->nweb_id, code, policy, threat);
-  }
 }
 #endif
 
