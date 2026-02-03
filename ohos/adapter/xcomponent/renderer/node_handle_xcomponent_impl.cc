8ee4ba0fb118d35072044e24b9efdac612267df3
caizhiwei <caizhiwei7@huawei.com>
2026-02-03 14:34:41 +0800

    【功能-BUGFIX】修复node和surface销毁顺序错误导致标签页拖入拖出崩溃问题
    Signed-off-by: Blake <zhenlong.zhu@haitaichina.com>
    Change-Id: I9c032dcfae0cd19540d07e6d556c8d98bb2ae704

--- ohos/adapter/xcomponent/renderer/node_handle_xcomponent_impl.cc
+++ ohos/adapter/xcomponent/renderer/node_handle_xcomponent_impl.cc
@@ -430,11 +430,6 @@ NodeHandleXComponentImpl::NodeHandleXComponentImpl(const std::string& id,
 
 __attribute__((no_sanitize("cfi", "cfi-icall")))
 NodeHandleXComponentImpl::~NodeHandleXComponentImpl() {
-  if (node_handle_) {
-    UnRegisterCallBack();
-    GetNativeNodeAPI()->disposeNode(node_handle_);
-    node_handle_ = nullptr;
-  }
   if (surface_holder_) {
     nodeHandle::NodeHandleImpl::GetInstance().SurfaceHolderDispose(
         surface_holder_);
@@ -444,6 +439,11 @@ NodeHandleXComponentImpl::~NodeHandleXComponentImpl() {
     nodeHandle::NodeHandleImpl::GetInstance().SurfaceCallbackDispose(
         surface_callback_);
     surface_callback_ = nullptr;
+  }
+  if (node_handle_) {
+    UnRegisterCallBack();
+    GetNativeNodeAPI()->disposeNode(node_handle_);
+    node_handle_ = nullptr;
   }
 }
 
