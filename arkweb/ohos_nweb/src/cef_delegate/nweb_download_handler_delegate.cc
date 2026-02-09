cc5411f9d33429be6b4ae07a3c501a4b3637ee0f
chendawei18 <chendawei18@huawei.com>
2026-02-09 11:42:32 +0800

    增加dataURLzuida最大长度校验，避免crash
    Signed-off-by: chendawei18 <chendawei18@huawei.com>

--- arkweb/ohos_nweb/src/cef_delegate/nweb_download_handler_delegate.cc
+++ arkweb/ohos_nweb/src/cef_delegate/nweb_download_handler_delegate.cc
@@ -22,6 +22,7 @@
 #include "cef/include/wrapper/cef_closure_task.h"
 #include "cef/include/wrapper/cef_helpers.h"
 #include "chrome/grit/generated_resources.h"
+#include "content/common/content_constants_internal.h"
 #include "net/base/filename_util.h"
 #include "net/http/http_content_disposition.h"
 #include "nweb_impl.h"
@@ -36,6 +37,9 @@
 namespace OHOS::NWeb {
 
 namespace {
+
+const int kDataSchemePrefixLen = 5;
+
 const std::string GetContentDisposition(
     CefRefPtr<CefDownloadItem> download_item, std::string default_charset) {
     std::string origin_content_disposition =
@@ -64,6 +68,16 @@ std::string DesensitizeStr(const std::string& str) {
     }
     return str.substr(0, 2) + "**";
 }
+
+static bool IsDataScheme(const CefString& url) {
+  if (url.length() < kDataSchemePrefixLen) {
+    return false;
+  }
+  CefString prefix(url.c_str(), kDataSchemePrefixLen, true);
+  CefString dataScheme("data:");
+  return prefix == dataScheme;
+}
+
 }
 
 NWebDownloadHandlerDelegate::NWebDownloadHandlerDelegate(
@@ -182,9 +196,11 @@ std::string NWebDownloadHandlerDelegate::GenerateSuggestedFilename(
   std::string content_disposition = GetContentDisposition(download_item,
                                                           default_charset);
   GURL gurl = GURL();
-  if (!(download_item->GetURL().size() > url::kMaxURLChars)) {
+  if (!(IsDataScheme(download_item->GetURL()) &&
+        download_item->GetURL().size() > content::kMaxLengthOfDataURLString)) {
     gurl = GURL(download_item->GetURL().ToString());
-  } 
+  }
+
   base::FilePath generated_filename = net::GenerateFileName(
       gurl, content_disposition, default_charset,
       suggested_filename, sniffed_mime_type, default_filename);
