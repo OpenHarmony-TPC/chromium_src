eefc4bdd77109b5235123d94f253cd98368fc582
huangwei236 <huangwei236@h-partners.com>
2026-03-02 20:49:40 +0800

    update: 更新文件 document_loader_impl.cc 解决pdf加载过程crash问题
    Signed-off-by: huangwei236 <huangwei236@h-partners.com>

--- pdf/loader/document_loader_impl.cc
+++ pdf/loader/document_loader_impl.cc
@@ -122,7 +122,7 @@ bool DocumentLoaderImpl::Init(std::unique_ptr<URLLoaderWrapper> loader,
     chunk_stream_.set_eof_pos(std::max(0, loader_->GetContentLength()));
 
 #if BUILDFLAG(ARKWEB_PDF)
-  int contentLength = loader->GetContentLength();
+  int contentLength = loader_->GetContentLength();
   if (contentLength > 0 && static_cast<size_t>(contentLength) > kReadLargeBufferSize) {
     LOG(DEBUG) << "pdf loader use large buffer, content length: " << loader_->GetContentLength();
     buffer_.resize(kReadLargeBufferSize);
