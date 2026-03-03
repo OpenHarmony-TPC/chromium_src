96961a551b4c0a07152e4ff61e9f01764cc631f5
guzhaoyang <guzhaoyang@h-partners.com>
2026-03-03 16:29:39 +0800

    test: update content_settings_default_provider_for_include_unittest.cc
    Signed-off-by: guzhaoyang <guzhaoyang@h-partners.com>

--- arkweb/chromium_ext/components/content_settings/core/browser/content_settings_default_provider_for_include_unittest.cc
+++ arkweb/chromium_ext/components/content_settings/core/browser/content_settings_default_provider_for_include_unittest.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,35 +17,87 @@
 #include "base/test/scoped_command_line.h"
 #include "content/public/common/content_switches.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/content_settings/core/browser/content_settings_default_provider_for_include.cc"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content_settings {
 
-TEST(ContentSettingsDefaultProviderForIncludeTest, ShouldSkipSettingForContentTypeExt_EnableNwebExExceptionList) {
+class ContentSettingsDefaultProviderForIncludeTest : public ::testing::Test {
+ protected:
+   void SetUp() override {
+     base::CommandLine::Init(0, nullptr);
+     base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
+     if (cmd_line->HasSwitch(switches::kEnableNwebExExceptionList)) {
+        cmd_line->RemoveSwitch(switches::kEnableNwebExExceptionList);
+     }
+   }
+   void TearDown() override {}
+};
+
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest001) {
+    base::test::ScopedCommandLine scoped_command_line;
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+
+    ASSERT_NE(command_line, nullptr);
+
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::IMAGES);
+    EXPECT_TRUE(is_skipped);
+}
+
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest002) {
+    base::test::ScopedCommandLine scoped_command_line;
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+
+    ASSERT_NE(command_line, nullptr);
+
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::COOKIES);
+    EXPECT_TRUE(is_skipped);
+}
+
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest003) {
+    base::test::ScopedCommandLine scoped_command_line;
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+
+    ASSERT_NE(command_line, nullptr);
+
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::JAVASCRIPT);
+    EXPECT_TRUE(is_skipped);
+}
+
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest004) {
     base::test::ScopedCommandLine scoped_command_line;
     auto* command_line = base::CommandLine::ForCurrentProcess();
 
     ASSERT_NE(command_line, nullptr);
     command_line->AppendSwitch(switches::kEnableNwebExExceptionList);
 
-    EXPECT_FALSE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::JAVASCRIPT));
-    EXPECT_FALSE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::COOKIES));
-
-    EXPECT_TRUE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::IMAGES));
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::IMAGES);
+    EXPECT_TRUE(is_skipped);
 }
 
-TEST(ContentSettingsDefaultProviderForIncludeTest, ShouldSkipSettingForContentTypeExt_DisableNwebExExceptionList) {
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest005) {
     base::test::ScopedCommandLine scoped_command_line;
     auto* command_line = base::CommandLine::ForCurrentProcess();
 
     ASSERT_NE(command_line, nullptr);
+    command_line->AppendSwitch(switches::kEnableNwebExExceptionList);
+
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::COOKIES);
+    EXPECT_TRUE(is_skipped);
+}
+
+TEST_F(ContentSettingsDefaultProviderForIncludeTest, ContentSettingsDefaultProviderForIncludeTest006) {
+    base::test::ScopedCommandLine scoped_command_line;
+    auto* command_line = base::CommandLine::ForCurrentProcess();
+
+    ASSERT_NE(command_line, nullptr);
+    command_line->AppendSwitch(switches::kEnableNwebExExceptionList);
+
+    bool is_skipped = ShouldSkipSettingForContentTypeExt(ContentSettingsType::JAVASCRIPT);
+    EXPECT_TRUE(is_skipped);
+
     command_line->RemoveSwitch(switches::kEnableNwebExExceptionList);
-
-    EXPECT_TRUE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::JAVASCRIPT));
-    EXPECT_TRUE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::COOKIES));
-
-    EXPECT_TRUE(ShouldSkipSettingForContentTypeExt(ContentSettingsType::IMAGES));
 }
 
 } // namespace content_settings
