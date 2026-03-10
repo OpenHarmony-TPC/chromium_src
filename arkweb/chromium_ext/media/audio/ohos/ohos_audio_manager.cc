97bbdaacb64b87880988e322f0f5fe4a11d0d3a0
a2637 <huangmeichen@h-partners.com>
2026-03-10 15:15:13 +0800

    update: 更新文件 ohos_audio_manager.cc
    Signed-off-by: a2637 <huangmeichen@h-partners.com>

--- arkweb/chromium_ext/media/audio/ohos/ohos_audio_manager.cc
+++ arkweb/chromium_ext/media/audio/ohos/ohos_audio_manager.cc
@@ -217,7 +217,7 @@ AudioInputStream* OHOSAudioManager::MakeLowLatencyInputStream(
   LOG(INFO) << "OHOSAudioManager::MakeLowLatencyInputStream";
   isCommunication_ = true;
   SelectAudioDevice(device_id, true);
-  if (!params.effects()) {
+  if (!(params.effects() & media::AudioParameters::ECHO_CANCELLER)) {
     isCommunication_ = false;
   }
   return new OHOSAudioInputStream(this, params);
@@ -245,9 +245,6 @@ AudioParameters OHOSAudioManager::GetPreferredInputStreamParameters(
       AudioParameters(AudioParameters::AUDIO_PCM_LOW_LATENCY,
                       ChannelLayoutConfig::Guess(kDefaultChannelCount),
                       kDefaultSampleRate, kMinimumInputBufferSize);
-  params.set_effects(AudioParameters::ECHO_CANCELLER |
-                     AudioParameters::NOISE_SUPPRESSION |
-                     AudioParameters::AUTOMATIC_GAIN_CONTROL);
   return params;
 }
 
