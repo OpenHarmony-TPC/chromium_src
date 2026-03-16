8d953bc0e288974277733e5358e6c8d2e6a83698
x397186324 <xiaye1@h-partners.com>
2026-03-16 16:58:26 +0800

    [CVE-2016-3919][444176961][Extensions Bindings] Make ContextInvalidationListeners Checked
    Signed-off-by: x397186324 <xiaye1@h-partners.com>

--- extensions/renderer/bindings/api_binding_util.cc
+++ extensions/renderer/bindings/api_binding_util.cc
@@ -50,8 +50,7 @@ class ContextInvalidationData : public base::SupportsUserData::Data {
 
  private:
   bool is_context_valid_ = true;
-  base::ObserverList<ContextInvalidationListener>::Unchecked
-      invalidation_listeners_;
+  base::ObserverList<ContextInvalidationListener> invalidation_listeners_;
 };
 
 constexpr char ContextInvalidationData::kPerContextDataKey[];
@@ -64,19 +63,18 @@ ContextInvalidationData::~ContextInvalidationData() {
 
 void ContextInvalidationData::AddListener(
     ContextInvalidationListener* listener) {
-  DCHECK(is_context_valid_);
+  CHECK(is_context_valid_);
   invalidation_listeners_.AddObserver(listener);
 }
 
 void ContextInvalidationData::RemoveListener(
     ContextInvalidationListener* listener) {
-  DCHECK(is_context_valid_);
-  DCHECK(invalidation_listeners_.HasObserver(listener));
+  CHECK(invalidation_listeners_.HasObserver(listener));
   invalidation_listeners_.RemoveObserver(listener);
 }
 
 void ContextInvalidationData::Invalidate() {
-  DCHECK(is_context_valid_);
+  CHECK(is_context_valid_);
   is_context_valid_ = false;
 
   for (ContextInvalidationListener& listener : invalidation_listeners_)
@@ -88,13 +86,13 @@ bool IsContextValid(v8::Local<v8::Context> context) {
   if (!per_context_data)
     return false;
 
-  auto* invalidation_data =
-      static_cast<ContextInvalidationData*>(per_context_data->GetUserData(
-          ContextInvalidationData::kPerContextDataKey));
-  // The context is valid if we've never created invalidation data for it, or if
-  // we have and it hasn't been marked as invalid.
+  auto* invalidation_data = GetPerContextData<ContextInvalidationData>(
+      context, CreatePerContextData::kDontCreateIfMissing);
+
+  // The context is valid as long as the invalidation data is present and not
+  // marked invalid.
   bool is_context_valid =
-      !invalidation_data || invalidation_data->is_context_valid();
+      invalidation_data && invalidation_data->is_context_valid();
 
   if (is_context_valid) {
     // As long as the context is valid, there should be an associated
@@ -114,6 +112,22 @@ bool IsContextValidOrThrowError(v8::Local<v8::Context> context) {
   isolate->ThrowException(v8::Exception::Error(
       gin::StringToV8(isolate, "Extension context invalidated.")));
   return false;
+}
+
+void InitializeContext(v8::Local<v8::Context> context) {
+  gin::PerContextData* per_context_data = gin::PerContextData::From(context);
+  CHECK(per_context_data);
+
+  // It would be nice to CHECK() that the invalidation data does *not* yet exist
+  // (since this means we're calling InitializeContext() twice), but a number of
+  // tests do this as part of their setup. It's also fairly harmless.
+  if (per_context_data->GetUserData(
+          ContextInvalidationData::kPerContextDataKey)) {
+    return;
+  }
+
+  per_context_data->SetUserData(ContextInvalidationData::kPerContextDataKey,
+                                std::make_unique<ContextInvalidationData>());
 }
 
 void InvalidateContext(v8::Local<v8::Context> context) {
@@ -159,16 +173,23 @@ ContextInvalidationListener::ContextInvalidationListener(
 }
 
 ContextInvalidationListener::~ContextInvalidationListener() {
-  if (!on_invalidated_)
-    return;  // Context was invalidated.
-
-  DCHECK(context_invalidation_data_);
-  context_invalidation_data_->RemoveListener(this);
+  // We may have already removed ourselves as a listener (in OnInvalidated())
+  // if the context was invalidated previously. Check the context first.
+  if (context_invalidation_data_) {
+    context_invalidation_data_->RemoveListener(this);
+  }
 }
 
 void ContextInvalidationListener::OnInvalidated() {
-  DCHECK(on_invalidated_);
+  DCHECK(on_invalidated_) << "OnInvalidated() called twice!";
+  DCHECK(context_invalidation_data_);
+
+  // The ContextInvalidationData will be cleaned up soon, so we can't store a
+  // reference to it. We also remove ourselves as an observer proactively to
+  // avoid leaving a dangling pointer in ContextInvalidationData.
+  context_invalidation_data_->RemoveListener(this);
   context_invalidation_data_ = nullptr;
+
   std::move(on_invalidated_).Run();
 }
 
