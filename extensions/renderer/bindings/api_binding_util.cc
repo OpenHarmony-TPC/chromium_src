3281d2f88e1649e77b84fdcd272700600ad0ee04
x397186324 <xiaye1@h-partners.com>
2026-03-17 14:08:57 +0800

    去除多余空格
    Signed-off-by: x397186324 <xiaye1@h-partners.com>

--- extensions/renderer/bindings/api_binding_util.cc
+++ extensions/renderer/bindings/api_binding_util.cc
@@ -118,9 +118,9 @@ bool IsContextValidOrThrowError(v8::Local<v8::Context> context) {
 
 void InvalidateContext(v8::Local<v8::Context> context) {
   ContextInvalidationData* data =
-      GetPerContextData<ContextInvalidationData>(context, kCreateIfMissing);	 
-   if (!data)	 
-     return;
+      GetPerContextData<ContextInvalidationData>(context, kCreateIfMissing);
+  if (!data)
+    return;
 
   data->Invalidate();
 }
