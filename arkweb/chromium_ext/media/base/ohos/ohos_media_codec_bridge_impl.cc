1eaf747d6722e58aef4b66ab5c2fb64c55d85298
GrowUpEveryday <yangjiawei24@huawei.com>
2026-03-19 21:16:56 +0800

    update: 更新文件 ohos_media_codec_bridge_impl.cc
    Signed-off-by: GrowUpEveryday <yangjiawei24@huawei.com>

--- arkweb/chromium_ext/media/base/ohos/ohos_media_codec_bridge_impl.cc
+++ arkweb/chromium_ext/media/base/ohos/ohos_media_codec_bridge_impl.cc
@@ -85,15 +85,12 @@ CodecCodeAdapter CheckYUVMemcpyData(const YUVMemcpyData &data, scoped_refptr<Vid
     LOG(ERROR) << "frame or shm_region is nullptr";
     return CodecCodeAdapter::ERROR;
   }
-  uint64_t real_all_plane_space = frame->shm_region()->Map().size();
-  uint64_t y_required_plane_space =
-      static_cast<uint64_t>(frame->layout().planes()[VideoFrame::kYPlane].offset) + data.src_size[VideoFrame::kYPlane];
-  uint64_t u_required_plane_space =
-      static_cast<uint64_t>(frame->layout().planes()[VideoFrame::kUPlane].offset) + data.src_size[VideoFrame::kUPlane];
-  uint64_t v_required_plane_space =
-      static_cast<uint64_t>(frame->layout().planes()[VideoFrame::kVPlane].offset) + data.src_size[VideoFrame::kVPlane];
-  if (y_required_plane_space > real_all_plane_space || u_required_plane_space > real_all_plane_space ||
-      v_required_plane_space > real_all_plane_space) {
+
+  uint64_t y_space_till_real_all_plane = frame->shm_region()->Map().size() - frame->layout().planes()[VideoFrame::kYPlane].offset;
+  uint64_t u_space_till_real_all_plane = frame->shm_region()->Map().size() - frame->layout().planes()[VideoFrame::kUPlane].offset;
+  uint64_t v_space_till_real_all_plane = frame->shm_region()->Map().size() - frame->layout().planes()[VideoFrame::kVPlane].offset;
+  if (y_space_till_real_all_plane < data.src_size[VideoFrame::kYPlane] || u_space_till_real_all_plane < data.src_size[VideoFrame::kUPlane] ||
+      v_space_till_real_all_plane < data.src_size[VideoFrame::kVPlane]) {
     LOG(ERROR) << "plane size error, required plane space > real plane space!";
     return CodecCodeAdapter::ERROR;
   }
