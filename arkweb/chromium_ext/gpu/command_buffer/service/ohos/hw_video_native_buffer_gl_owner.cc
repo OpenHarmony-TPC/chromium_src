3854aea041c82de8489c9dcbc1d8f18b21637fd1
wangshengwen333 <wangshengwen4@h-partners.com>
2026-03-22 15:58:38 +0800

    GetNativeBuffer问题部分遗漏的代码 Co-Authored-By: Agent
    Signed-off-by: wangshengwen333 <wangshengwen4@h-partners.com>

--- arkweb/chromium_ext/gpu/command_buffer/service/ohos/hw_video_native_buffer_gl_owner.cc
+++ arkweb/chromium_ext/gpu/command_buffer/service/ohos/hw_video_native_buffer_gl_owner.cc
@@ -168,12 +168,13 @@ HwVideoNativeBufferGLOwner::GetNativeBuffer() {
   TRACE_EVENT0("gpu", "HwVideoNativeBufferGLOwner::GetNativeBuffer");
 
   base::AutoLock auto_lock(lock_);
-  if (!current_image_ref_) {
+  if (!current_image_ref_ || !loader_) {
     return nullptr;
   }
   OHOSNativeBuffer buffer;
-  loader_->GetNativeBuffer(current_image_ref_->image()->rawbuffer, &buffer);
-  if (!buffer) {
+  int32_t return_code = 0;
+  return_code = loader_->GetNativeBuffer(current_image_ref_->image()->rawbuffer, &buffer);
+  if (return_code != 0 || !buffer) {
     LOG(ERROR) << "GetNativeBuffer returned nullptr: ";
     return nullptr;
   }
