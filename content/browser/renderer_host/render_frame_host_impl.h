1819ca8c9caa95f275e15ce4404c053a97c27c75
x397186324 <xiaye1@h-partners.com>
2026-03-30 17:26:17 +0800

    [CVE-2026-4451][487768779]Validate ResourceRequestBody in CreateNewWindowParams.
    Signed-off-by: x397186324 <xiaye1@h-partners.com>

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -3447,6 +3447,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
                            AttemptDuplicateRenderWidgetHost);
   FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
                            BindToWebUIFromWebViaMojo);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           CreateNewWindowWithInaccessibleFile);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
                            RenderViewHostIsNotReusedAfterDelayedUnloadACK);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
