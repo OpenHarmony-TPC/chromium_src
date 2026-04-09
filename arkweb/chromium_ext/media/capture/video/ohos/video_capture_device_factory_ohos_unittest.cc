fcf4ffe79623f566088d6023707252f6ebe95c6d
qq_42700029 <wuqiwen7@huawei.com>
2026-04-09 21:20:17 +0800

    修复webview新增虚函数导致编译失败的问题
    Signed-off-by: qq_42700029 <wuqiwen7@huawei.com>

--- arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_factory_ohos_unittest.cc
+++ arkweb/chromium_ext/media/capture/video/ohos/video_capture_device_factory_ohos_unittest.cc
@@ -99,6 +99,10 @@ class CameraManagerAdapterMock : public CameraManagerAdapter {
   MOCK_METHOD(void, SetForegroundFlag, (bool), (override));
   MOCK_METHOD(void, SetCameraStatus, (CameraStatusAdapter), (override));
   MOCK_METHOD(std::string, GetCurrentDeviceId, (), (override));
+  MOCK_METHOD(int32_t, GetSupportedFlashModes, (std::vector<FlashModeAdapter>& flashModesAdapter), (override));
+  MOCK_METHOD(bool, HasFlash, (), (override));
+  MOCK_METHOD(flashModeAdapter, GetFlashMode, (), (override));
+  MOCK_METHOD(int32_t, SetFlashMode, (FlashModeAdapter flashMode), (override));
 };
 
 class VideoDeviceDescriptorAdapterMock : public VideoDeviceDescriptorAdapter {
